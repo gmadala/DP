fa16d025be062f03f4e696b2df306ee460758927
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-07-15 16:48:15 -0400

    Renamed Navigation directive to navBar.

---
+++ app/scripts/directives/navBar/navBar.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('nextgearWebApp')
+  .directive('navBar', function() {
+      return {
+          restrict: 'A',
+          templateUrl: "scripts/directives/navBar/navBar.html",
+          controller: 'NavBarCtrl'
+      };
+  })
+
+  .controller('NavBarCtrl', function($scope) {
+
+  });
