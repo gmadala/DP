755dbe7d76064448804c962e72b18053bf9b3810
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-07-15 16:50:57 -0400

    Updated tests to refer to renamed controllers/directives.

--- test/spec/controllers/home.js
+++ test/spec/controllers/home.js
@@ -5,13 +5,13 @@ describe('Controller: HomeCtrl', function () {
   // load the controller's module
   beforeEach(module('nextgearWebApp'));
 
-  var MainCtrl,
+  var HomeCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('HomeCtrl', {
+      HomeCtrl = $controller('HomeCtrl', {
       $scope: scope
     });
   }));
---
+++ test/spec/directives/navBar.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Directive: navBar', function () {
+  beforeEach(module('nextgearWebApp'));
+
+  var element;
+
+  
+});
