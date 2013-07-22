459d28a7b72cbf43d5aedf06af7e8345b997ff11
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-07-22 16:08:08 -0400

    Adds skeleton html and dummy data for home views: floorplan, payments, receipts, & scheduled payments.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,6 +2,7 @@
 
 angular.module('nextgearWebApp')
   .controller('HomeCtrl', function($scope) {
+      // dummy data
       $scope.payments = [
           {vin: "CH224157",
               vehicle: "2008 Toyota Sequoia Limited Tan",
@@ -27,4 +28,43 @@ angular.module('nextgearWebApp')
       ];
 
       $scope.isCollapsed = true;
+
+      $scope.schPayments = [
+          {vin: "CH224157",
+              vehicle: "2008 Toyota Sequoia Limited Tan",
+              stockNo: 1064,
+              status: "Pending",
+              scheduledDate: "5/15/2013",
+              payoff: 20864.24,
+              curtailment: 274,
+              setup: "5/10/2013",
+              scheduledBy: "Michael Bluth"}
+      ];
+
+      $scope.receipts = [
+          {vin: "CH224157",
+              vehicle: "2008 Toyota Sequoia Limited Tan",
+              stockNo: "1064",
+              status: "Processed",
+              payDate: "5/10/2013",
+              applied: 20864.24,
+              provided: 3181.60,
+              receiptNo: 2146838,
+              payment: "ACH",
+              checkNo: "",
+              payee: ""}
+      ];
+
+      $scope.floorplan = [
+          {vin: "CH224157",
+              vehicle: "2008 Toyota Sequoia Limited Tan",
+              stockNo: "1064",
+              status: "Processed",
+              purchased: "5/10/2013",
+              lastPay: "6/1/2013",
+              titleRelease: "5/15/2013",
+              releasedTo: "Buyer",
+              floored: "5/10/2013",
+              seller: "MAFS Pennsylvania"}
+      ];
   });
