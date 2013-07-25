0a55dc52bc4f64418443a508ba18b037a77b1183
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-07-25 17:30:09 -0400

    Adds forgotten username/password html structure to login.html

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,32 +3,34 @@
 angular.module('nextgearWebApp')
   .controller('LoginCtrl', function($rootScope, $scope, $http, $location, Base64) {
 
-      $scope.credentials = {
-          username: "",
-          password: ""
-      };
+    $scope.credentials = {
+        username: "",
+        password: ""
+    };
 
-      $scope.authenticate = function() {
-          console.log("authenticating with: " + $scope.credentials.username + "/" + $scope.credentials.password);
-          $http.post(
-            '/UserAccount/Authenticate/',
-            {
-                Authorization: "CT " +
-                  Base64.encode($scope.credentials.username + ":" + $scope.credentials.password)
-            })
-            .success(function(response) {
-                if (response.Success) {
-                    console.log(["Login success"]);
-                    $rootScope.authToken = response.Data;
-                    $location.path('/home');
-                }
-                else {
-                    $rootScope.authToken = null;
-                }
-            })
-            .error(function(response) {
-                console.error(["Login error"]);
-            });
-      }
+    $scope.forgot = false;
+
+    $scope.authenticate = function() {
+      console.log("authenticating with: " + $scope.credentials.username + "/" + $scope.credentials.password);
+      $http.post(
+        '/UserAccount/Authenticate/',
+        {
+            Authorization: "CT " +
+              Base64.encode($scope.credentials.username + ":" + $scope.credentials.password)
+        })
+        .success(function(response) {
+          if (response.Success) {
+            console.log(["Login success"]);
+            $rootScope.authToken = response.Data;
+            $location.path('/home');
+          }
+          else {
+            $rootScope.authToken = null;
+          }
+        })
+        .error(function(response) {
+            console.error(["Login error"]);
+        });
+    }
   }
 );
