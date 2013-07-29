fbcb6acbee3033eb84af895a4f2bb9c390c4de59
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-07-29 17:10:02 -0400

    Adds toggle button to test dealer and auction specific nav, ref #10.

--- app/scripts/directives/navBar/navBar.js
+++ app/scripts/directives/navBar/navBar.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .directive('navBar', function() {
-      return {
-          restrict: 'A',
-          templateUrl: "scripts/directives/navBar/navBar.html",
-          controller: 'NavBarCtrl'
-      };
-  })
+    .directive('navBar', function() {
+        return {
+            restrict: 'A',
+            templateUrl: "scripts/directives/navBar/navBar.html",
+            controller: 'NavBarCtrl'
+    };
+})
 
-  .controller('NavBarCtrl', function($scope) {
-
-  });
+.controller('NavBarCtrl', function($scope) {
+    $scope.isDealer = true;
+});
