fa0735be78aff1de9ac506f6d5288eaf65d28d61
Daniel McOrmond <daniel.mcormond@effectiveui.com>
2013-07-29 10:09:34 -0400

    Updated Karma E2E config syntax.

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,51 +1,55 @@
 // Karma E2E configuration
+module.exports = function(karma) {
+	karma.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+		// base path, that will be used to resolve files and exclude
+		basePath: 'app',
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js'
-];
+		frameworks: ['ng-scenario'],
 
-// list of files to exclude
-exclude = [];
+		// list of files / patterns to load in the browser
+		files: [
+		  '../test/e2e/**/*.js'
+		],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+		// list of files to exclude
+		exclude: [],
 
-// web server port
-port = 8080;
+		// test results reporter to use
+		// possible values: dots || progress || growl
+		reporters: ['progress'],
 
-// cli runner port
-runnerPort = 9100;
+		// web server port
+		port: 8080,
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+		// cli runner port
+		runnerPort: 9100,
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+		// level of logging
+		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+		logLevel: karma.LOG_INFO,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['Chrome'],
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 5000,
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false
+	});
+};
