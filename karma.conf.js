3d0177a0f2242ad88df872bca2293ccb81c0a204
Daniel McOrmond <daniel.mcormond@effectiveui.com>
2013-07-29 09:32:30 -0400

    Updated Karma configuration to latest syntax.

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,81 @@
 // Karma configuration
+module.exports = function(karma) {
+	karma.set({
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+		// base path, that will be used to resolve files and exclude
+		basePath: 'app',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+		frameworks: ['jasmine'],
 
-// list of files to exclude
-exclude = [];
+		plugins: [
+      'karma-jasmine',
+      'karma-coverage',
+      'karma-ng-html2js-preprocessor',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-script-launcher'
+		],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+		// list of files / patterns to load in the browser
+		files: [
+		  'components/angular/angular.js',
+		  'components/angular-mocks/angular-mocks.js',
+			'components/angular-strap/dist/angular-strap.js',
+			'components/ui-router/release/angular-ui-router.js',
+			'components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+		  'scripts/*.js',
+		  'scripts/**/*.js',
+		  '../test/spec/**/*.js'
+		],
 
-// web server port
-port = 8080;
+		preprocessors: {
+			'**/*.html': 'html2js',
+			'scripts/**/*.js': 'coverage'
+		},
 
-// cli runner port
-runnerPort = 9100;
+		// list of files to exclude
+		exclude: [],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+		// test results reporter to use
+		// possible values: dots || progress || growl
+		reporters: ['dots', 'coverage'],
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+		coverageReporter: {
+			type: 'html',
+			dir: '../test/coverage/'
+		},
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+		// web server port
+		port: 9876,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+		// cli runner port
+		runnerPort: 9100,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+		// level of logging
+		// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+		logLevel: karma.LOG_INFO,
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
+
+		// Start these browsers, currently available:
+		// - Chrome
+		// - ChromeCanary
+		// - Firefox
+		// - Opera
+		// - Safari (only Mac)
+		// - PhantomJS
+		// - IE (only Windows)
+		browsers: ['Chrome'],
+
+		// If browser does not capture in given timeout [ms], kill it
+		captureTimeout: 5000,
+
+		// Continuous Integration mode
+		// if true, it capture browsers, run tests and exit
+		singleRun: false
+	});
+};
