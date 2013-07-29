2f7bffcaa6b218a88f886b52166c49d1c391f895
Daniel McOrmond <daniel.mcormond@effectiveui.com>
2013-07-29 09:51:05 -0400

    Fixed failing test in IE8.

--- test/spec/directives/nxgInput.js
+++ test/spec/directives/nxgInput.js
@@ -8,6 +8,6 @@ describe('Directive: nxgInput', function () {
   it('should make hidden element visible', inject(function ($rootScope, $compile) {
     element = angular.element('<div nxg-input></div>');
     element = $compile(element)($rootScope);
-    expect(element.text()).toBe(' {{ model }}');
+    expect(element.text().trim()).toBe('{{ model }}');
   }));
 });
