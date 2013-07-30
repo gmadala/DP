5961a8c747dc2f9d7a758eae2007afed51e4e424
Ryan Bell <ryan.bell@effectiveui.com>
2013-07-30 14:02:39 -0400

    Increase timeout for test launch

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function(karma) {
 		browsers: ['Chrome'],
 
 		// If browser does not capture in given timeout [ms], kill it
-		captureTimeout: 5000,
+		captureTimeout: 10000,
 
 		// Continuous Integration mode
 		// if true, it capture browsers, run tests and exit
