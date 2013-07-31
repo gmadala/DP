29b037dbcbc67a29d0278738866990e096bc1608
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-07-31 16:45:48 -0400

    Fixes request payout dialog markup & functionality, re #58.

--- app/scripts/controllers/home/dashboard.js
+++ app/scripts/controllers/home/dashboard.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('DashboardCtrl', function($scope) {
+  .controller('DashboardCtrl', function($scope, $dialog) {
     $scope.viewMode = 'week';
+
+    $scope.openRequestPayout = function() {
+      var dialogOptions = {
+        backdrop: true,
+        keyboard: true,
+        backdropClick: true,
+        templateUrl: 'views/modals/requestPayout.html',
+        controller: 'PayoutCtrl'
+      };
+
+      $dialog.dialog(dialogOptions).open();
+      // TODO: Add MVC integration so that user gets a confirmation message when successful.
+    };
+  })
+  .controller('PayoutCtrl', function($scope, dialog) {
+    $scope.close = function() {
+      // actually send data for payout here.
+      dialog.close();
+    };
   });
