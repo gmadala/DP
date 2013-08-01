56155b23a6da5a32a0042cf556f6a6a8343e2d0c
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-08-01 17:07:20 -0400

    Updating code style.

--- app/scripts/directives/navBar/navBar.js
+++ app/scripts/directives/navBar/navBar.js
@@ -2,23 +2,23 @@
 
 angular.module('nextgearWebApp')
   .directive('navBar', function() {
-      return {
-          restrict: 'A',
-          templateUrl: "scripts/directives/navBar/navBar.html",
-          controller: 'NavBarCtrl'
-      };
+    return {
+      restrict: 'A',
+      templateUrl: "scripts/directives/navBar/navBar.html",
+      controller: 'NavBarCtrl'
+    };
   })
 
   .controller('NavBarCtrl', function($scope, User) {
-      $scope.isDealer = true;
-      $scope.showSettings = false;
-      $scope.showNavbar = false;
+    $scope.isDealer = true;
+    $scope.showSettings = false;
+    $scope.showNavbar = false;
 
-      // fetch the dealer info every time there's a new session (user could have changed)
-      $scope.$on("AuthenticationSuccess", function() {
-          User.refreshInfo().then(function(results) {
-              $scope.dealerInfo = results;
-          })
-          $scope.showNavbar = true;
-      });
+    // fetch the dealer info every time there's a new session (user could have changed)
+    $scope.$on("AuthenticationSuccess", function() {
+      User.refreshInfo().then(function(results) {
+        $scope.dealerInfo = results;
+      })
+      $scope.showNavbar = true;
+    });
   });
