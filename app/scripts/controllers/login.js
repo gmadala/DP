732132137ede87449a227abcfeb06a09b416c57d
Daniel McOrmond <daniel.mcormond@effectiveui.com>
2013-08-01 10:56:26 -0400

    Fixed JSHint syntax errors.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,42 +3,42 @@
 angular.module('nextgearWebApp')
   .controller('LoginCtrl', function($rootScope, $scope, $http, $location, Base64, nxgConfig, User) {
 
-      $scope.credentials = {
-          username: '',
-          password: ''
-      };
+    $scope.credentials = {
+      username: '',
+      password: ''
+    };
 
-      $scope.identity = {
-          securityQuestion: 'Where did you attend high school?',
-          securityAnswer: 'chsn',
-          showQuestion: false
-      };
+    $scope.identity = {
+      securityQuestion: 'Where did you attend high school?',
+      securityAnswer: 'chsn',
+      showQuestion: false
+    };
 
-      // TODO: Replace these mock variables with actual ones.
-      $scope.forgot = false;
+    // TODO: Replace these mock variables with actual ones.
+    $scope.forgot = false;
 
-      $scope.authenticate = function() {
-          console.log('authenticating with: ' + $scope.credentials.username + '/' + $scope.credentials.password);
+    $scope.authenticate = function() {
+      console.log('authenticating with: ' + $scope.credentials.username + '/' + $scope.credentials.password);
 
-          User.authenticate($scope.credentials.username, $scope.credentials.password)
-            .then(function(data) {
-                console.log(['Login success']);
-                User.isLogged = true;
-                $rootScope.$broadcast("AuthenticationSuccess");
-                // set a default Authorization header with the authentication token
-                $http.defaults.headers.common.Authorization = "CT " + data;
-                $location.path('/home');
-            }, function(error) {
-                console.error('Login error', error);
-            });
-      };
+      User.authenticate($scope.credentials.username, $scope.credentials.password)
+        .then(function(data) {
+          console.log(['Login success']);
+          User.isLogged = true;
+          $rootScope.$broadcast('AuthenticationSuccess');
+          // set a default Authorization header with the authentication token
+          $http.defaults.headers.common.Authorization = 'CT ' + data;
+          $location.path('/home');
+        }, function(error) {
+          console.error('Login error', error);
+        });
+    };
 
-      $scope.onForgotPasswordSubmit = function() {
-          // TODO: Grab security question and answer data
-          $scope.identity.showQuestion = true;
+    $scope.onForgotPasswordSubmit = function() {
+      // TODO: Grab security question and answer data
+      $scope.identity.showQuestion = true;
 
-          // TODO: Determine if it's submitting the username or the
-          // question/answer, and handle accordingly.
-      };
+      // TODO: Determine if it's submitting the username or the
+      // question/answer, and handle accordingly.
+    };
   }
 );
