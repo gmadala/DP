07d8b0beedfc272a6dae3aaac7563705065c2c0f
Daniel McOrmond <daniel.mcormond@effectiveui.com>
2013-08-02 14:33:27 -0400

    Wired up the receipt search criteria. #117

--- app/scripts/controllers/home/receipts.js
+++ app/scripts/controllers/home/receipts.js
@@ -2,8 +2,35 @@
 
 angular.module('nextgearWebApp')
   .controller('ReceiptsCtrl', function($scope, $log, Receipts) {
-    Receipts.search().then(
-      function(results) { $scope.receipts = results.Receipts; },
-      function(error) { $log.error(error); }
-    );
+
+    // This is a generic piece of data that maps to multiple API params
+    $scope.keyword = null;
+
+    // These are the query-string params the API accepts
+    $scope.criteria = {
+      ReceiptNumber:    null,
+      Vin:              null,
+      StartDate:        null,
+      EndDate:          null,
+      PaymentMethod:    null,
+      CheckNumber:      null,
+      PayeeDescription: null,
+      StockNumber:      null,
+      TransactionType:  null
+    };
+
+    // Run the search
+    $scope.search = function() {
+
+      // The search field could be any one of these:
+      $scope.criteria.ReceiptNumber = $scope.keyword;
+      $scope.criteria.StockNumber   = $scope.keyword;
+      $scope.criteria.Vin           = $scope.keyword;
+      $scope.criteria.CheckNumber   = $scope.keyword;
+
+      Receipts.search($scope.criteria).then(
+        function(results) { $scope.receipts = results.Receipts; },
+        function(error) { $log.error(error); }
+      );
+    };
   });
--- test/spec/controllers/home/receipts.js
+++ test/spec/controllers/home/receipts.js
@@ -12,7 +12,7 @@ describe('Controller: ReceiptsCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     httpBackend = $httpBackend;
-    httpBackend.when('GET', '/receipt/search').respond({
+    httpBackend.when('GET', '/receipt/search?').respond({
       'Success': true,
       'Message': null,
       'Data': {
@@ -25,7 +25,20 @@ describe('Controller: ReceiptsCtrl', function () {
     });
   }));
 
+  it('should attach keyword to the scope', function () {
+    expect(scope.keyword).toBeDefined();
+  });
+
+  it('should attach criteria to the scope', function () {
+    expect(scope.criteria).toBeDefined();
+  });
+
+  it('should attach search to the scope', function () {
+    expect(typeof scope.search).toBe('function');
+  });
+
   it('should attach receipts to the scope', function () {
+    scope.search();
     httpBackend.flush();
     expect(scope.receipts.length).toBe(1);
   });
