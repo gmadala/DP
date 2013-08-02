5307430488c508fa578a402a342669f208a18eb2
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-02 14:04:14 -0400

    Add code to convert API data to calendar data re #33 re #179

--- app/scripts/models/payments.js
+++ app/scripts/models/payments.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .factory('Payments', function($q, api) {
+  .factory('Payments', function($q, $filter, api) {
     return {
       fetchUpcomingCalendar: function(startDate, endDate) {
         // convert to UNIX epoch datetime values
@@ -12,33 +12,89 @@ angular.module('nextgearWebApp')
             api.request('GET','/payment/search', {DueDateStart: startDate, DueDateEnd: endDate}),
             api.request('GET','/payment/searchscheduled', {StartDate: startDate, EndDate: endDate}),
             api.request('GET','/payment/possiblePaymentDates/'+startDate+'/'+endDate)
-          ]).then(function(/*responses*/) {
-            // TODO: Transform response data into daily summary events & open dates hash table
+          ]).then(function(responses) {
+            var dueRaw = responses[0].SearchResults || [],
+              scheduledRaw = responses[1].SearchResults || [],
+              possibleRaw = responses[2] || [],
+              dueEvents = [],
+              scheduledEvents = [],
+              eventsByDate = {},
+              openDates = {},
+              formatDate = $filter('date'),
+              formatMoney = $filter('currency'),
+              iso = 'yyyy-MM-dd',
+              dateMap,
+              date,
+              dateKey,
+              list;
+
+            // aggregate due payments list into a set of calendar events, max 1 per day, summarizing payments due that day
+            dateMap = {};
+            dueRaw.forEach(function (payment) {
+              date = new Date(payment.DueDate * 1000);
+              dateKey = formatDate(date, iso);
+              list = dateMap[dateKey] || [];
+              list.push(payment);
+              dateMap[dateKey] = list;
+            });
+            for (dateKey in dateMap) {
+              list = dateMap[dateKey];
+              date = list.reduce(function (summary, payment) {
+                summary.count += 1;
+                summary.total += payment.AmountDue;
+                return summary;
+              }, {count: 0, total: 0});
+              date = {
+                title: date.count + (date.count === 1 ? ' Payment Due' : ' Payments Due'),
+                subTitle: formatMoney(date.total),
+                start: dateKey
+              };
+              dueEvents.push(date);
+              list = eventsByDate[dateKey] || [];
+              list.push(date);
+              eventsByDate[dateKey] = list;
+            }
+
+            // aggregate scheduled payments list into a set of calendar events, max 1 per day, summarizing payments scheduled that day
+            dateMap = {};
+            scheduledRaw.forEach(function (payment) {
+              date = new Date(payment.ScheduledPaymentDate * 1000);
+              dateKey = formatDate(date, iso);
+              list = dateMap[dateKey] || [];
+              list.push(payment);
+              dateMap[dateKey] = list;
+            });
+            for (dateKey in dateMap) {
+              list = dateMap[dateKey];
+              date = list.reduce(function (summary, payment) {
+                summary.count += 1;
+                // is this the right way to find the scheduled payment amount?
+                summary.total += payment.IsPayoff ? payment.CurrentPayoff : payment.AmountDue;
+                return summary;
+              }, {count: 0, total: 0});
+              date = {
+                title: date.count + ' Scheduled',
+                subTitle: formatMoney(date.total),
+                start: dateKey
+              };
+              scheduledEvents.push(date);
+              list = eventsByDate[dateKey] || [];
+              list.push(date);
+              eventsByDate[dateKey] = list;
+            }
+
+            // convert possiblePaymentDates into a map of 'yyyy-MM-dd': true
+            possibleRaw.forEach(function (dateVal) {
+              date = new Date(dateVal * 1000);
+              dateKey = formatDate(date, iso);
+              openDates[dateKey] = true;
+            });
+
             return {
-              dueEvents: [
-                {title: '2 Payments Due', subTitle: '$5,000', start: '2013-08-01'},
-                {title: '3 Payments Due', subTitle: '$8,000', start: '2013-08-07'}
-              ],
-              scheduledEvents: [
-                {title: '2 Scheduled', subTitle: '$5,000', start: '2013-08-01'}
-              ],
-              eventsByDate: {
-                '2013-08-01': [
-                  {title: '2 Payments Due', subTitle: '$5,000', start: '2013-08-01'},
-                  {title: '2 Scheduled', subTitle: '$5,000', start: '2013-08-01'}
-                ],
-                '2013-08-07': [
-                  {title: '3 Payments Due', subTitle: '$8,000', start: '2013-08-07'}
-                ]
-              },
-              openDates: {
-                '2013-08-01': true,
-                '2013-08-05': true,
-                '2013-08-06': true,
-                '2013-08-07': true,
-                '2013-08-08': true,
-                '2013-08-09': true
-              }
+              dueEvents: dueEvents,
+              scheduledEvents: scheduledEvents,
+              eventsByDate: eventsByDate,
+              openDates: openDates
             };
           });
       },
---
+++ test/spec/models/payments.js
@@ -0,0 +1,131 @@
+'use strict';
+
+describe("Model: Payments", function () {
+
+  beforeEach(module('nextgearWebApp'));
+
+  var payments,
+    httpBackend;
+
+  beforeEach(inject(function ($httpBackend, Payments) {
+    payments = Payments;
+    httpBackend = $httpBackend;
+  }));
+
+  describe('fetchUpcomingCalendar method', function () {
+    var resultData = null;
+
+
+    // abbreviate to just the data needed for the tests, to keep things cleaner
+    beforeEach(function () {
+      httpBackend.whenGET(/^\/payment\/search\?(DueDateStart=|DueDateEnd=)\d+&(DueDateStart=|DueDateEnd=)\d+$/).respond({
+        "Success": true,
+        "Message": null,
+        "Data": {
+          "SearchResults": [{
+            "AmountDue": 2200.00,
+            "DueDate": 1375329600 // 2013-08-01T00:00:00 EDT
+          }, {
+            "AmountDue": 2800.00,
+            "DueDate": 1375329600 // 2013-08-01T00:00:00 EDT
+          }, {
+            "AmountDue": 8000.00,
+            "DueDate": 1375848000 // 2013-08-07T00:00:00 EDT
+          }],
+          "AccountFees": []
+        }
+      });
+
+      httpBackend.whenGET(/^\/payment\/searchscheduled\?(StartDate=|EndDate=)\d+&(StartDate=|EndDate=)\d+$/).respond({
+        "Success": true,
+        "Message": null,
+        "Data": {
+          "SearchResults": [{
+            "CurrentPayoff": 34098.00,
+            "AmountDue": 2200.00,
+            "IsPayoff": false,
+            "ScheduledPaymentDate": 1375329600 // 2013-08-01T00:00:00 EDT
+          }, {
+            "CurrentPayoff": 34098.00,
+            "AmountDue": 2800.00,
+            "IsPayoff": false,
+            "ScheduledPaymentDate": 1375329600 // 2013-08-01T00:00:00 EDT
+          }],
+          "AccountFees": []
+        }
+      });
+
+      httpBackend.whenGET(/^\/payment\/possiblePaymentDates\/\d+\/\d+$/).respond({
+        "Success": true,
+        "Message": null,
+        "Data": [
+          1375329600, // 2013-08-01T00:00:00 EDT
+          1375675200, // 2013-08-05T00:00:00 EDT
+          1375761600, // 2013-08-06T00:00:00 EDT
+          1375848000, // 2013-08-07T00:00:00 EDT
+          1375934400, // 2013-08-08T00:00:00 EDT
+          1376020800  // 2013-08-09T00:00:00 EDT
+        ]
+      });
+
+      payments.fetchUpcomingCalendar(new Date(), new Date()).then(function (results) {
+        resultData = results;
+      }, function (error) {
+        throw new Error('request failed');
+      });
+      httpBackend.flush();
+
+    });
+
+    it('should create a data object with the expected properties', function () {
+      expect(resultData).not.toBe(null);
+      expect(resultData.dueEvents).toBeDefined();
+      expect(resultData.scheduledEvents).toBeDefined();
+      expect(resultData.eventsByDate).toBeDefined();
+      expect(resultData.openDates).toBeDefined();
+    });
+
+    it('should create the expected dueEvents', function () {
+      var expected = [
+        {title: '2 Payments Due', subTitle: '$5,000.00', start: '2013-08-01'},
+        {title: '1 Payment Due', subTitle: '$8,000.00', start: '2013-08-07'}
+      ];
+      expect(angular.equals(resultData.dueEvents, expected)).toBe(true);
+    });
+
+    it('should create the expected scheduledEvents', function () {
+      var expected = [
+        {title: '2 Scheduled', subTitle: '$5,000.00', start: '2013-08-01'}
+      ];
+      expect(angular.equals(resultData.scheduledEvents, expected)).toBe(true);
+    });
+
+    it('should create the expected eventsByDate', function () {
+      var expected = {
+        '2013-08-01': [
+          {title: '2 Payments Due', subTitle: '$5,000.00', start: '2013-08-01'},
+          {title: '2 Scheduled', subTitle: '$5,000.00', start: '2013-08-01'}
+        ],
+        '2013-08-07': [
+          {title: '1 Payment Due', subTitle: '$8,000.00', start: '2013-08-07'}
+        ]
+      };
+      expect(angular.equals(resultData.eventsByDate, expected)).toBe(true);
+    });
+
+    it('should create the expected openDates', function () {
+      var expected = {
+        '2013-08-01': true,
+        '2013-08-05': true,
+        '2013-08-06': true,
+        '2013-08-07': true,
+        '2013-08-08': true,
+        '2013-08-09': true
+      };
+      expect(angular.equals(resultData.openDates, expected)).toBe(true);
+    });
+
+  });
+
+
+});
