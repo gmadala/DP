37ece76cd5a238faffc627cc4e386c6635610e49
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-06 15:43:17 -0400

    basic tests for receipts model

--- test/spec/models/receipts.js
+++ test/spec/models/receipts.js
@@ -6,13 +6,58 @@ describe('Service: Receipts', function () {
   beforeEach(module('nextgearWebApp'));
 
   // instantiate service
-  var receipts;
-  beforeEach(inject(function (_Receipts_) {
+  var receipts,
+    httpBackend;
+
+  beforeEach(inject(function ($httpBackend,_Receipts_) {
+    httpBackend = $httpBackend;
     receipts = _Receipts_;
   }));
 
-  it('should do something', function () {
+  it('should exist', function () {
     expect(!!receipts).toBe(true);
+  });
+
+  describe('search method', function () {
+
+    beforeEach(function () {
+      httpBackend.expectGET(/receipt\/search.*/)
+        .respond({Success: true, Data: {Receipts: []}});
+    });
+
+    it('should make a GET request to the expected endpoint', function () {
+      receipts.search({});
+      httpBackend.flush();
+    });
+
+    it('should return a promise', function () {
+      var resolveFn = jasmine.createSpy('success');
+      receipts.search({}).then(resolveFn);
+      httpBackend.flush();
+      expect(resolveFn).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('fetchRecent method', function () {
+
+    beforeEach(function () {
+      httpBackend.expectGET(/receipt\/search.*/)
+        .respond({Success: true, Data: {Receipts: []}});
+    });
+
+    it('should make a GET request to the expected endpoint', function () {
+      receipts.fetchRecent();
+      httpBackend.flush();
+    });
+
+    it('should return a promise', function () {
+      var resolveFn = jasmine.createSpy('success');
+      receipts.fetchRecent().then(resolveFn);
+      httpBackend.flush();
+      expect(resolveFn).toHaveBeenCalled();
+    });
+
   });
 
 });
