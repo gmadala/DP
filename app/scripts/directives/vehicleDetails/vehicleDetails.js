f9b2527c6f30acfd483d38207de714cb9d8685a4
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-08-08 14:07:50 -0400

    Created a vehicle-details directive to be included everywhere vehicle details needs to be shown. It takes a stock number as parameter. Also added a VehicleDetails service that knows how to get and process the vehicle data. refs #69

---
+++ app/scripts/directives/vehicleDetails/vehicleDetails.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('nextgearWebApp')
+  .directive('vehicleDetails', function($timeout) {
+    return {
+      restrict: 'A',
+      templateUrl: 'scripts/directives/vehicleDetails/vehicleDetails.html',
+      scope: {
+        stockNumber: '='
+      },
+      controller: function($scope, VehicleDetails) {
+        $scope.$watch('stockNumber', function() {
+          VehicleDetails.getDetails($scope.stockNumber).then(
+            function(results) {
+              $scope.vehicleDetails = results;
+            }
+          );
+
+          // TODO: Waiting on service endpoint spec
+          /*VehicleDetails.getCurtailmentSchedule($scope.stockNumber).then(
+            function(results) {
+              $scope.curtailmentSchedule = results;
+            }
+          );*/
+        });
+      }
+    };
+  });
---
+++ app/scripts/models/vehicleDetails.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('nextgearWebApp')
+  .factory('VehicleDetails', function($q, api) {
+
+    return {
+      getDetails: function(stockNum) {
+        return api.request('GET', 'floorplan/' + stockNum).then(function(r) {
+          return {
+            vin: r.UnitVin,
+            make: r.UnitMake,
+            model: r.UnitModel,
+            style: r.UnitStyle,
+            color: r.Color,
+            year: r.UnitYear,
+            currentMileage: r.UnitMileageAtApproval,
+            salvage: r.Salvage,
+            status: r.FloorplanStatusName,
+            stockNumber: r.StockNumber,
+            purchaseDate: r.UnitPurchaseDate,
+            dateSubmitted: r.FlooringDate,
+            financedAmount: r.AmountFinanced,
+            purchaseAmount: r.PurchaseAmount,
+            lastPayment: null, // TODO: This has yet to be added to the contract
+            lineOfCredit: null, // TODO: This has yet to be added to the contract
+            termPlanName: null, // TODO: This has yet to be added to the contract
+            productType: r.ProductType,
+            seller: r.SellerName,
+            titleLocation: r.TitleLocation,
+            titleState: r.TitleState,
+            trackingNumber: null, // TODO: This has yet to be added to the contract
+            disbursementDate: r.DisbursementDate
+          };
+        });
+      },
+      getCurtailmentSchedule: function(stockNum) {
+        // TODO: Yet to be defined in the service
+        /*return api.request('GET', 'someServiceURL/' + stockNum).then(function(r) {
+         return {
+
+         };
+         })*/
+        return $q;
+      }
+    };
+  });
