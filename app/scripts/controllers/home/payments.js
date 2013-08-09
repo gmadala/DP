96c4758ea5a715c31662773086d6d5dfb4e420a9
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-08-09 16:22:43 -0400

    Adds payments queue sidebar to payments tab, re #76.

--- app/scripts/controllers/home/payments.js
+++ app/scripts/controllers/home/payments.js
@@ -30,4 +30,29 @@ angular.module('nextgearWebApp')
       highlight: ''
     }];
 
+    $scope.queue = [{
+      vin: 'CH224157',
+      make: 'Toyota',
+      model: 'Corolla',
+      payment: 3544.49,
+      year: 2013
+    },
+    {
+      vin: 'CH224157',
+      make: 'Toyota',
+      model: 'Corolla',
+      payment: 3544.49,
+      year: 2013
+    },
+    {
+      vin: 'CH224157',
+      make: 'Toyota',
+      model: 'Corolla',
+      payment: 3544.49,
+      year: 2013
+    }];
+
+    $scope.total = 3544.49*3;
+    $scope.fees=[{ type: 'Collateral Audit', payment: 150}];
+
   });
