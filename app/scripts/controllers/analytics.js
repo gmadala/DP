df2f8821e6471f46f6921229f79c520f3ed5c95b
Mikey McOrmond <mikey.mcormond@effectiveui.com>
2013-08-23 14:29:42 -0400

    Adds charts to analytics page. See ticket #429

--- app/scripts/controllers/analytics.js
+++ app/scripts/controllers/analytics.js
@@ -2,6 +2,38 @@
 
 angular.module('nextgearWebApp')
   .controller('AnalyticsCtrl', function ($scope, $dialog) {
+    $scope.averageTurn = {
+      labels : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", ],
+      datasets : [
+        {
+          data : [65,59,90,81,56,55]
+        }
+      ]
+    };
+    $scope.bestMovers = {
+      labels : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", ],
+      datasets : [
+        {
+          data : [65,59,90,81,56,55]
+        }
+      ]
+    };
+    $scope.worstMovers = {
+      labels : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", ],
+      datasets : [
+        {
+          data : [65,59,90,81,56,55]
+        }
+      ]
+    };
+    $scope.top10auctions = {
+      labels : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", ],
+      datasets : [
+        {
+          data : [65,59,90,81,56,55]
+        }
+      ]
+    };
     $scope.openTopAuctions = function() {
       var dialogOptions = {
         backdrop: true,
