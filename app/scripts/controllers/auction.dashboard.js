330ae8beecf5cbfd00918d9f4d1e8aea8693c9ce
Mikey McOrmond <mikey.mcormond@effectiveui.com>
2013-08-26 16:27:34 -0400

    Adds Auction Dashboard HTML, Chart HTML, Floorplan Groups HTML, Disbursement Groups HTML, Recent Disbursements HTML. See #246, #258, #277, #271, #252.

--- app/scripts/controllers/auction.dashboard.js
+++ app/scripts/controllers/auction.dashboard.js
@@ -3,4 +3,12 @@
 angular.module('nextgearWebApp')
   .controller('AuctionDashboardCtrl', function($scope) {
     $scope.foo = '';
+  $scope.volume = {
+    labels : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', ],
+    datasets : [
+        {
+          data : [65,59,90,81,56,55]
+        }
+      ]
+    };
   });
