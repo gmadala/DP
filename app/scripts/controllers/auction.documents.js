ee842c36bdc3fa00b5a6880b9778a6ff77418223
Mikey McOrmond <mikey.mcormond@effectiveui.com>
2013-08-26 09:40:58 -0400

    Update documents page for both dealer & auction. See #283, #234.

--- app/scripts/controllers/auction.documents.js
+++ app/scripts/controllers/auction.documents.js
@@ -3,4 +3,12 @@
 angular.module('nextgearWebApp')
   .controller('AuctionDocumentsCtrl', function($scope) {
     $scope.foo = '';
+    $scope.documents = [
+          { 'title': 'Welcome Packet(PDF)',
+            'url': 'path/to/link'
+          },
+          { 'title': 'Instructions for Sellers (PDF)',
+            'url': 'path/to/link'
+          }
+        ];
   });
