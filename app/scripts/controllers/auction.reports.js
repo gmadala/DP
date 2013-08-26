217477f1dc4a63b8e92e03990a4ec6f9eaf505cf
Mikey McOrmond <mikey.mcormond@effectiveui.com>
2013-08-26 16:30:58 -0400

    Adds Auction Reports HTML. See #172.

--- app/scripts/controllers/auction.reports.js
+++ app/scripts/controllers/auction.reports.js
@@ -2,5 +2,12 @@
 
 angular.module('nextgearWebApp')
   .controller('AuctionReportsCtrl', function($scope) {
-    $scope.foo = '';
+    $scope.documents = [
+      { 'title': 'Credit Availability Query History (PDF)',
+        'url': 'path/to/link'
+      },
+      { 'title': 'Receivable Detail (PDF)',
+        'url': 'path/to/link'
+      }
+    ];
   });
