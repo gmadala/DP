7cdf83e0c936cc9e983a5229e34f6e64e39473f8
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-27 12:08:15 -0400

    fix jshint errors

--- app/scripts/controllers/auction.dashboard.js
+++ app/scripts/controllers/auction.dashboard.js
@@ -3,9 +3,9 @@
 angular.module('nextgearWebApp')
   .controller('AuctionDashboardCtrl', function($scope) {
     $scope.foo = '';
-  $scope.volume = {
-    labels : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', ],
-    datasets : [
+    $scope.volume = {
+      labels : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', ],
+      datasets : [
         {
           data : [65,59,90,81,56,55]
         }
