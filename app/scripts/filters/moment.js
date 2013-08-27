7c6079494ce83466437b4f7e872054e4413e015e
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-27 15:58:34 -0400

    Add a version of moment filter that will give you the UTC behavior

--- app/scripts/filters/moment.js
+++ app/scripts/filters/moment.js
@@ -3,6 +3,9 @@
 /**
  * Pipe a date/time value through the moment.js library's parse and format functionality
  *
+ * There are two versions of this filter: moment will act on the incoming value in terms of local time,
+ * whereas momentUTC will act on the incoming value in terms of UTC.
+ *
  * @param {String} outputFormat the output format you want; see http://momentjs.com/docs/#/displaying/format/
  * @param {String} [inputFormat] if the input value is a non-ISO 8601 string, the format to read it as
  */
@@ -10,5 +13,10 @@ angular.module('nextgearWebApp')
   .filter('moment', function (moment) {
     return function (input, outputFormat, inputFormat) {
       return moment(input, inputFormat).format(outputFormat);
+    };
+  })
+  .filter('momentUTC', function (moment) {
+    return function (input, outputFormat, inputFormat) {
+      return moment.utc(input, inputFormat).format(outputFormat);
     };
   });
--- test/spec/filters/moment.js
+++ test/spec/filters/moment.js
@@ -33,4 +33,35 @@ describe('Filter: moment', function () {
     expect(moment(text, usFormat, 'dddd, MMMM D, YYYY')).toBe('8/19/2013');
   });
 
+  describe('utc version', function () {
+
+    var momentUTC;
+    beforeEach(inject(function ($filter) {
+      momentUTC = $filter('momentUTC');
+    }));
+
+    it('should work identically to non-utc version for short ISO dates with no time information"', function () {
+      // technically if you include the timezone in your format string, this wouldn't hold true, but it's an unlikely case
+      var text = '2013-08-07';
+      expect(momentUTC(text, usFormat)).toBe('8/7/2013');
+
+      text = '2013-12-25';
+      expect(momentUTC(text, usFormat)).toBe('12/25/2013');
+    });
+
+    it('should format JavaScript dates to a US date format based on UTC"', function () {
+      var date = new Date(2013, 7, 7);
+      date.setUTCFullYear(2013);
+      date.setUTCMonth(7);
+      date.setUTCDate(7);
+      expect(momentUTC(date, usFormat)).toBe('8/7/2013');
+    });
+
+    it('should read and reformat randomly formatted dates with an input format hint"', function () {
+      var text = 'Monday, August 19, 2013';
+      expect(momentUTC(text, usFormat, 'dddd, MMMM D, YYYY')).toBe('8/19/2013');
+    });
+
+  });
+
 });
