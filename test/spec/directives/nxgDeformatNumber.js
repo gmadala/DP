c62d14472f0749ca3e9469d6955cd707024bff33
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-27 17:24:53 -0400

    add directive to strip number formatting re #131

---
+++ test/spec/directives/nxgDeformatNumber.js
@@ -0,0 +1,44 @@
+'use strict';
+
+describe('Directive: nxgDeformatNumber', function () {
+  beforeEach(module('nextgearWebApp'));
+
+  var element,
+    scope,
+    form;
+
+  beforeEach(inject(function ($rootScope, $compile) {
+    scope = $rootScope.$new();
+    scope.model = {
+      data: undefined
+    };
+
+    element = angular.element(
+      '<form name="form">' +
+        '<input type="text" name="input" ng-model="model.data" ' +
+        'ng-pattern="/^[0-9]{1,3}(?:,?[0-9]{3})*(\\.[0-9]{2})?$/" nxg-deformat-number></input>' +
+      '</form>'
+    );
+    element = $compile(element)(scope);
+    scope.$digest();
+    form = scope.form;
+  }));
+
+  it('should remove any non-digit and non-decimal-point characters', function () {
+    form.input.$setViewValue('123');
+    expect(scope.model.data).toBe('123');
+
+    form.input.$setViewValue('123.34');
+    expect(scope.model.data).toBe('123.34');
+
+    form.input.$setViewValue('123,234');
+    expect(scope.model.data).toBe('123234');
+  });
+
+  it('should defer to earlier, and likely more sensitive, validators', function () {
+    form.input.$setViewValue('1pq3');
+    expect(scope.model.data).toBe(undefined);
+    expect(form.input.$error.pattern).toBe(true);
+  });
+
+});
