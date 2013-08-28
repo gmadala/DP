e024cfd272ee0fa4dad3bc5488d68f30a33eb3e3
Ryan Bell <ryan.bell@effectiveui.com>
2013-08-28 17:23:14 -0400

    tests for multiple vehicles dialog controller

--- test/spec/controllers/multipleVehicles.js
+++ test/spec/controllers/multipleVehicles.js
@@ -6,17 +6,44 @@ describe('Controller: MultipleVehiclesCtrl', function () {
   beforeEach(module('nextgearWebApp'));
 
   var MultipleVehiclesCtrl,
-    scope;
+    scope,
+    dialogMock,
+    matchListMock;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
+    dialogMock = {
+      close: angular.noop
+    };
+
+    matchListMock = [
+      {someValue: 'foo'},
+      {someValue: 'bar'},
+    ];
+
     scope = $rootScope.$new();
     MultipleVehiclesCtrl = $controller('MultipleVehiclesCtrl', {
-      $scope: scope
+      $scope: scope,
+      dialog: dialogMock,
+      matchList: matchListMock
     });
   }));
 
-  xit('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach the match list to the scope', function () {
+    expect(scope.matches).toBe(matchListMock);
+  });
+
+  it('should expose a select function that closes the dialog with the provided value', function () {
+    spyOn(dialogMock, 'close');
+    expect(typeof scope.select).toBe('function');
+    scope.select('foo');
+    expect(dialogMock.close).toHaveBeenCalledWith('foo');
+  });
+
+  it('should expose a close function that closes the dialog with no value', function () {
+    spyOn(dialogMock, 'close');
+    expect(typeof scope.close).toBe('function');
+    scope.close();
+    expect(dialogMock.close).toHaveBeenCalledWith();
   });
 });
