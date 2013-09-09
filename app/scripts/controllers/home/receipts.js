566aceca3b334f652f1e531baed3f84e468f671d
Ryan Bell <ryan.bell@effectiveui.com>
2013-09-09 15:10:35 -0400

    Integrate search directive into receipts view re #300
    Note: This replaces some old controller code for this view that appeared to be invalid as a result of API changes

--- app/scripts/controllers/home/receipts.js
+++ app/scripts/controllers/home/receipts.js
@@ -5,45 +5,51 @@ angular.module('nextgearWebApp')
 
     $scope.isCollapsed = true;
 
-    // This is a generic piece of data that maps to multiple API params
-    $scope.keyword = null;
+    $scope.filterOptions = [
+      {
+        label: 'View All',
+        value: 'all'
+      },
+      {
+        label: 'Auction Check',
+        value: 'ach'
+      },
+      {
+        label: 'Certified Funds',
+        value: 'certified'
+      },
+      {
+        label: 'Check',
+        value: 'check'
+      },
+      {
+        label: 'CPP Claim',
+        value: 'cpp'
+      },
+      {
+        label: 'Non Dealer Check',
+        value: 'nondealerCheck'
+      },
+      {
+        label: 'Proceeds',
+        value: 'proceeds'
+      }
+    ];
 
-    // These are the query-string params the API accepts
-    $scope.criteria = {
-      ReceiptNumber:    null,
-      Vin:              null,
-      StartDate:        null,
-      EndDate:          null,
-      PaymentMethod:    null,
-      CheckNumber:      null,
-      PayeeDescription: null,
-      StockNumber:      null,
-      TransactionType:  null
-    };
-
-    // Run the search
     $scope.search = function() {
-
-      // The search field could be any one of these:
-      $scope.criteria.ReceiptNumber = $scope.keyword;
-      $scope.criteria.StockNumber   = $scope.keyword;
-      $scope.criteria.Vin           = $scope.keyword;
-      $scope.criteria.CheckNumber   = $scope.keyword;
-
-      Receipts.search($scope.criteria).then(
-        function(results) { $scope.receipts = results.Receipts; },
-        function(error) { $log.error(error); }
-      );
+      $scope.results = Receipts.search($scope.searchCriteria);
     };
 
-    // handle deep-link search param on URL
-    if ($stateParams.search) {
-      $scope.keyword = $stateParams.search;
-      angular.extend($scope.criteria, {
-        StartDate: null,
-        EndDate: null,
-        PaymentMethod: null
-      });
+    $scope.resetSearch = function (initialKeyword) {
+      $scope.searchCriteria = {
+        query: initialKeyword || null,
+        startDate: null,
+        endDate: null,
+        filter: 'all'
+      };
       $scope.search();
-    }
+    };
+
+    $scope.resetSearch($stateParams.search);
+
   });
--- test/spec/controllers/home/receipts.js
+++ test/spec/controllers/home/receipts.js
@@ -7,59 +7,92 @@ describe('Controller: ReceiptsCtrl', function () {
 
   var ReceiptsCtrl,
       scope,
-      httpBackend;
+      receiptsMock;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
-    httpBackend = $httpBackend;
-    httpBackend.when('GET', '/receipt/search?').respond({
-      'Success': true,
-      'Message': null,
-      'Data': {
-        'Receipts': [{}]
+  beforeEach(inject(function ($controller, $rootScope, $q) {
+    receiptsMock = {
+      search: function () {
+        var deferred = $q.defer();
+        deferred.resolve({
+          'Success': true,
+          'Message': null,
+          'Data': {
+            'Receipts': [{}]
+          }
+        });
+        $rootScope.$apply();
+        return deferred.promise;
       }
-    });
+    };
+
     scope = $rootScope.$new();
     ReceiptsCtrl = $controller('ReceiptsCtrl', {
-      $scope: scope
+      $scope: scope,
+      Receipts: receiptsMock
     });
   }));
 
-  it('should attach keyword to the scope', function () {
-    expect(scope.keyword).toBeDefined();
+  it('should attach filterOptions to the scope', function () {
+    expect(angular.isArray(scope.filterOptions)).toBe(true);
   });
 
-  it('should attach criteria to the scope', function () {
-    expect(scope.criteria).toBeDefined();
-  });
-
-  it('should attach search to the scope', function () {
+  it('should attach search function to the scope', function () {
     expect(typeof scope.search).toBe('function');
   });
 
-  it('should attach receipts to the scope upon search', function () {
-    scope.search();
-    httpBackend.flush();
-    expect(scope.receipts.length).toBe(1);
+  describe('search function', function () {
+
+    it('should call receipts model search function with criteria', function () {
+      spyOn(receiptsMock, 'search').andCallThrough();
+      scope.searchCriteria = {
+        query: 'foo'
+      };
+
+      scope.search();
+      expect(receiptsMock.search).toHaveBeenCalledWith(scope.searchCriteria);
+    });
+
+    it('should attach results promise to the scope', function () {
+      scope.search();
+      expect(scope.results).toBeDefined();
+      expect(typeof scope.results.then).toBe('function');
+    });
+
   });
 
-  it('should auto-search with a provided search param on the URL', inject(function ($controller, $rootScope) {
-    httpBackend.expectGET(/\/receipt\/search\?.+/).respond({
-      'Success': true,
-      'Message': null,
-      'Data': {
-        'Receipts': [{}]
-      }
+  it('should attach resetSearch function to the scope', function () {
+    expect(typeof scope.resetSearch).toBe('function');
+  });
+
+  describe('resetSearch function', function () {
+
+    it('should set appropriate search defaults', function () {
+      scope.resetSearch();
+      expect(scope.searchCriteria).toBeDefined();
+      expect(scope.searchCriteria.query).toBe(null);
+      expect(scope.searchCriteria.startDate).toBe(null);
+      expect(scope.searchCriteria.endDate).toBe(null);
+      expect(scope.searchCriteria.filter).toBe('all');
     });
 
-    var scope2 = $rootScope.$new(),
-    ReceiptsCtrl2 = $controller('ReceiptsCtrl', {
+    it('should call search method', function () {
+      spyOn(scope, 'search');
+      scope.resetSearch();
+      expect(scope.search).toHaveBeenCalled();
+    });
+
+  });
+
+  it('should honor a provided search keyword from the URL params', inject(function ($controller) {
+    spyOn(receiptsMock, 'search').andCallThrough();
+    $controller('ReceiptsCtrl', {
       $scope: scope,
-      $stateParams: {search: '123'}
+      $stateParams: {search: '123'},
+      Receipts: receiptsMock
     });
 
-    httpBackend.flush();
-    expect(scope.receipts.length).toBe(1);
+    expect(receiptsMock.search.mostRecentCall.args[0].query).toBe('123');
   }));
 
 });
