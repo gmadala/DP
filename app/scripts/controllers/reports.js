fae042faad8800dfd9a772c71dbe15e47c162e03
jcheng <jim.cheng@effectiveui.com>
2013-09-09 10:02:43 -0600

    Updated routing for loading mocked PDF reports. #135, #137

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -1,12 +1,9 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('ReportsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ReportsCtrl', function($scope, api) {
+
+    $scope.data = null;
 
     $scope.currentReports = [
       { 'title': 'Credit Availability Report (PDF)',
@@ -15,25 +12,56 @@ angular.module('nextgearWebApp')
       },
       { 'title': 'Receivable Detail (PDF)',
         'date': 'MMDDYYYY',
-      'url': 'path/to/link'
+        'url': 'path/to/link'
       },
       { 'title': 'Upcoming Curtailment / Payoff Quote (PDF)',
         'date': 'MMDDYYYY',
         'url': 'path/to/link'
       }
     ];
-	
-	$scope.viewDealerStatement = function() {
 
-		console.log('viewDealerStatement');
+    $scope.viewDealerStatement = function() {
 
-        if (!$scope.stmtForm.$valid) {
-			console.log('form not valid');
-          return false;
-        }
-		
-		console.log('form valid');
+          // take a snapshot of form state -- view can bind to this for submit-time update of validation display
+          $scope.stmtFormValidity = angular.copy($scope.stmtForm);
 
-	};
-  
+          if (!$scope.stmtForm.$valid) {
+            return false;
+          }
+
+          var startDate = api.toShortISODate($scope.data.stmtStartDate);
+          var endDate = api.toShortISODate($scope.data.stmtEndDate);
+
+          var strUrl = 'report/dealerstatement/' + startDate + '/' + endDate;
+
+          // append the vin filter string if one was provided, encoding it for safe transit in a GET query
+          if ($scope.data.stmtVinFilter) {
+            strUrl += '/' + encodeURIComponent($scope.data.stmtVinFilter);
+          }
+
+          window.open(
+            strUrl,
+            '_blank'  // open a new window every time
+          );
+
+        };
+
+    $scope.viewDisbursementDetail = function() {
+
+          // take a snapshot of form state -- view can bind to this for submit-time update of validation display
+          $scope.disFormValidity = angular.copy($scope.disForm);
+
+          if (!$scope.disForm.$valid) {
+            return false;
+          }
+
+          var date = api.toShortISODate($scope.data.disDate);
+          var strUrl = 'report/disbursementdetail/' + date;
+
+          window.open(
+            strUrl,
+            '_blank'  // open a new window every time
+          );
+
+        };
   });
--- app/scripts/directives/nxgStockNumbersInput/nxgStockNumbersInput.js
+++ app/scripts/directives/nxgStockNumbersInput/nxgStockNumbersInput.js
@@ -6,9 +6,44 @@ angular.module('nextgearWebApp')
       templateUrl: 'scripts/directives/nxgStockNumbersInput/nxgStockNumbersInput.html',
       replace: true,
       scope: {},
-      controller: 'StockNumbersInputCtrl'
-    };
-  })
-  .controller('StockNumbersInputCtrl', function ($scope) {
-	  
-  });
+      controller: function($scope) {
+
+				$scope.rangeStart = '';
+				$scope.rangeEnd = '';
+				$scope.stockNos = '';
+				$scope.isRange = false;
+				$scope.isSpecific = false;
+
+				$scope.selectData = {
+					'type' : 'select',
+					'value' : 'None',
+					'values' : [ 'None', 'Range of Stock #\'s', 'Specific Stock #\'s']
+				};
+
+				/* Handle changing the stock number filter select */
+				$scope.selectChange = function() {
+
+					/* Show the empty range entry fields as needed */
+					if ($scope.selectData.value === $scope.selectData.values[1]) {
+						$scope.isRange = true;
+						$scope.isSpecific = false;
+						$scope.rangeStart = '';
+						$scope.rangeEnd = '';
+						return;
+					}
+
+					/* Show the specific range field as needed */
+					if ($scope.selectData.value === $scope.selectData.values[2]) {
+						$scope.isRange = false;
+						$scope.isSpecific = true;
+						$scope.stockNos = '';
+						return;
+					}
+
+					/* Hide additional fields if "None" was selected */
+					$scope.isRange = false;
+					$scope.isSpecific = false;
+				};
+			}
+		};
+	});
