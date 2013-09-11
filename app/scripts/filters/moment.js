e576d2701cf7308667afee2ce75b926cb350be34
Ryan Bell <ryan.bell@effectiveui.com>
2013-09-11 12:21:14 -0400

    Add a default date format for moment filter

--- app/scripts/filters/moment.js
+++ app/scripts/filters/moment.js
@@ -6,12 +6,14 @@
  * There are two versions of this filter: moment will act on the incoming value in terms of local time,
  * whereas momentUTC will act on the incoming value in terms of UTC.
  *
- * @param {String} outputFormat the output format you want; see http://momentjs.com/docs/#/displaying/format/
+ * @param {String} [outputFormat] the output format you want; see http://momentjs.com/docs/#/displaying/format/
+ *  if not specified, outputFormat will default to a typical short US date format like M/D/YYYY
  * @param {String} [inputFormat] if the input value is a non-ISO 8601 string, the format to read it as
  */
 angular.module('nextgearWebApp')
   .filter('moment', function (moment) {
     return function (input, outputFormat, inputFormat) {
+      outputFormat = outputFormat || 'M/D/YYYY';
       if (input === null || !angular.isDefined(input)) {
         return input;
       }
@@ -20,6 +22,7 @@ angular.module('nextgearWebApp')
   })
   .filter('momentUTC', function (moment) {
     return function (input, outputFormat, inputFormat) {
+      outputFormat = outputFormat || 'M/D/YYYY';
       if (input === null || !angular.isDefined(input)) {
         return input;
       }
