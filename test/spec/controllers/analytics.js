706557663121d5ab00d17d8c6e08f925b7dabd20
jcheng <jim.cheng@effectiveui.com>
2013-09-20 10:02:49 -0600

    Fixed broken mockery in test spec for analytics controller

--- test/spec/controllers/analytics.js
+++ test/spec/controllers/analytics.js
@@ -6,15 +6,38 @@ describe('Controller: AnalyticsCtrl', function () {
   beforeEach(module('nextgearWebApp'));
 
   var AnalyticsCtrl,
-    scope;
+    scope, httpBackend,
+    analyticsMock, q;
+
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend, $q) {
+  
+    httpBackend = $httpBackend;
+    httpBackend.when('GET', /\/dealer\/summary?.+/).respond({});
+  
     scope = $rootScope.$new();
+    
+    analyticsMock = {
+      create: function () {
+        return $q.resolved(createResult);
+      },
+      fetchBusinessSummary : function() {},
+      fetchAnalytics: function() {},
+      fetchMovers: function() {}
+    };
+
     AnalyticsCtrl = $controller('AnalyticsCtrl', {
-      $scope: scope
+     $scope: scope,
+     $dialog: {
+        close: function() {}
+      },
+      Analytics: analyticsMock
     });
+  
   }));
+
 
   it('should exist', function () {
     expect(scope.openTopAuctions).toBeDefined;
