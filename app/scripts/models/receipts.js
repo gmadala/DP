6c97a70c50a06a60eed45d2660755c5c970c0847
Ryan Bell <ryan.bell@effectiveui.com>
2013-09-26 13:04:31 -0400

    Re-implement receipt search in model re #744

--- app/scripts/models/receipts.js
+++ app/scripts/models/receipts.js
@@ -1,10 +1,28 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .factory('Receipts', function receipts($q, api) {
+  .factory('Receipts', function receipts($q, api, Paginate) {
     return {
-      search: function(params) {
-        return api.request('GET', '/receipt/search', params);
+      search: function (criteria, paginator) {
+        var self = this,
+          params = {
+            Keyword: criteria.query || undefined,
+            PaymentMethodIds: criteria.filter,
+            StartDate: api.toShortISODate(criteria.startDate) || undefined,
+            EndDate: api.toShortISODate(criteria.endDate) || undefined,
+            OrderBy: 'CreateDate',
+            OrderByDirection: 'DESC',
+            PageNumber: paginator ? paginator.nextPage() : Paginate.firstPage(),
+            PageSize: Paginate.PAGE_SIZE_MEDIUM
+          };
+        return api.request('GET', '/receipt/search', params).then(
+          function (results) {
+            angular.forEach(results.Receipts, function (receipt) {
+              receipt.$receiptURL = self.getReceiptUrl(receipt.FinancialTransactionId);
+            });
+            return Paginate.addPaginator(results, results.ReceiptRowCount, params.PageNumber, params.PageSize);
+          }
+        );
       },
       getReceiptUrl: function (transactionId) {
         return api.contentLink('/receipt/view/' + transactionId);
--- test/spec/models/receipts.js
+++ test/spec/models/receipts.js
@@ -14,27 +14,146 @@ describe('Service: Receipts', function () {
     receipts = _Receipts_;
   }));
 
-  it('should exist', function () {
-    expect(!!receipts).toBe(true);
-  });
-
   describe('search method', function () {
 
-    beforeEach(function () {
-      httpBackend.expectGET(/receipt\/search.*/)
-        .respond({Success: true, Data: {Receipts: []}});
-    });
+    var paginate,
+      defaultCriteria = {
+        query: '',
+        startDate: null,
+        endDate: null,
+        filter: ''
+      },
+      searchResults = [],
+      callParams,
+      extractParams = function(method, url) {
+        // http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values
+        var match,
+          pl     = /\+/g,  // Regex for replacing addition symbol with a space
+          search = /([^&=]+)=?([^&]*)/g,
+          decode = function (s) { return decodeURIComponent(s.replace(pl, ' ')); },
+          query = url.substring(url.indexOf('?') + 1);
+
+        callParams = {};
+
+        while (match = search.exec(query)) {
+          callParams[decode(match[1])] = decode(match[2]);
+        }
+
+        return [200, {
+          Success: true,
+          Data: {
+            ReceiptRowCount: 20,
+            Receipts: searchResults
+          }
+        }, {}];
+      };
+
+    beforeEach(inject(function (Paginate) {
+      paginate = Paginate;
+      httpBackend.whenGET(/\/receipt\/search.*/).respond(extractParams);
+    }));
 
     it('should make a GET request to the expected endpoint', function () {
-      receipts.search({});
-      httpBackend.flush();
+      httpBackend.expectGET(/\/receipt\/search.*/);
+      receipts.search(defaultCriteria);
+      expect(httpBackend.flush).not.toThrow();
     });
 
-    it('should return a promise', function () {
-      var resolveFn = jasmine.createSpy('success');
-      receipts.search({}).then(resolveFn);
+    it('should ask for items sorted by most recent first', function () {
+      receipts.search(defaultCriteria);
       httpBackend.flush();
-      expect(resolveFn).toHaveBeenCalled();
+      expect(callParams.OrderBy).toBe('CreateDate');
+      expect(callParams.OrderByDirection).toBe('DESC');
+    });
+
+    it('should provide a page size', function () {
+      receipts.search(defaultCriteria);
+      httpBackend.flush();
+      expect(isNaN(parseInt(callParams.PageSize, 10))).toBe(false);
+    });
+
+    it('should start on the first page if a paginator is not provided', function () {
+      receipts.search(defaultCriteria);
+      httpBackend.flush();
+      expect(callParams.PageNumber).toBe(paginate.firstPage().toString());
+    });
+
+    it('should start on the next page if a paginator is provided', function () {
+      receipts.search(defaultCriteria, {
+        nextPage: function () {
+          return 11;
+        }
+      });
+      httpBackend.flush();
+      expect(callParams.PageNumber).toBe('11');
+    });
+
+    it('should add a paginator to the results', function () {
+      var output = {};
+      receipts.search(defaultCriteria).then(function (results) {
+        output = results;
+      });
+      httpBackend.flush();
+      expect(output.$paginator).toBeDefined();
+      expect(output.$paginator.nextPage()).toBe(2);
+    });
+
+    it('should add the appropriate $receiptURL to receipts', function () {
+      var output = {};
+      searchResults = [
+        {
+          FinancialTransactionId: 'foo123'
+        }
+      ];
+      receipts.search(defaultCriteria).then(function (results) { output = results; });
+      httpBackend.flush();
+      expect(output.Receipts[0].$receiptURL).toBe('/receipt/view/foo123');
+    });
+
+    it('should NOT send a Keyword if search term is empty/null', function () {
+      receipts.search(defaultCriteria);
+      httpBackend.flush();
+      expect(callParams.Keyword).not.toBeDefined();
+    });
+
+    it('should send the search term as Keyword, if present', function () {
+      receipts.search(angular.extend({}, defaultCriteria, {query: 'foo'}));
+      httpBackend.flush();
+      expect(callParams.Keyword).toBe('foo');
+    });
+
+    it('should not send startDate and endDate if not provided', function () {
+      receipts.search(defaultCriteria);
+      httpBackend.flush();
+      expect(callParams.StartDate).not.toBeDefined();
+      expect(callParams.EndDate).not.toBeDefined();
+    });
+
+    it('should send startDate and endDate if provided', function () {
+      receipts.search(angular.extend({}, defaultCriteria, {
+        startDate: moment([2013, 1, 1]).toDate(),
+        endDate: moment([2013, 2, 1]).toDate()
+      }));
+      httpBackend.flush();
+      expect(callParams.StartDate).toBe('2013-02-01');
+      expect(callParams.EndDate).toBe('2013-03-01');
+    });
+
+    it('should send the filter as PaymentMethodIds', function () {
+      receipts.search(angular.extend({}, defaultCriteria, {
+        filter: '1,2,3,4'
+      }));
+      httpBackend.flush();
+      expect(callParams.PaymentMethodIds).toBe('1,2,3,4');
+    });
+
+  });
+
+  describe('getReceiptUrl method', function () {
+
+    it('should append the provided transaction id to the correct path', function () {
+      var result = receipts.getReceiptUrl('foo');
+      expect(result).toBe('/receipt/view/foo');
     });
 
   });
