fe26a9d4c3908883434f9e383ed3d70b09b2d241
Ryan Bell <ryan.bell@effectiveui.com>
2013-09-27 16:48:01 -0400

    Add half second show delay for busy indicator to reduce blippy visual noise for fast operations re #294

--- app/scripts/directives/nxgActivity/nxgActivity.js
+++ app/scripts/directives/nxgActivity/nxgActivity.js
@@ -7,9 +7,27 @@ angular.module('nextgearWebApp')
       replace: true,
       restrict: 'A',
       scope: true,
-      controller: function ($scope, $http, status) {
+      controller: function ($scope, $http, $timeout, status) {
+        var showing,
+          pendingShow;
+
         $scope.showIndicator = function () {
-          return status.isShown() && $http.pendingRequests.length > 0;
+          var canShow = status.isShown() && $http.pendingRequests.length > 0;
+
+          if (!showing && !pendingShow && canShow) {
+            pendingShow = $timeout(function () {
+              showing = true;
+              pendingShow = null;
+            }, 500);
+          } else if (!canShow) {
+            showing = false;
+            if (pendingShow) {
+              $timeout.cancel(pendingShow);
+              pendingShow = null;
+            }
+          }
+
+          return showing;
         };
       }
     };
--- test/spec/directives/nxgActivity.js
+++ test/spec/directives/nxgActivity.js
@@ -7,13 +7,15 @@ describe('Directive: nxgActivity', function () {
     scope,
     status,
     httpBackend,
-    http;
+    http,
+    timeout;
 
-  beforeEach(inject(function ($rootScope, $compile, $http, $httpBackend, _status_) {
+  beforeEach(inject(function ($rootScope, $compile, $http, $httpBackend, $timeout, _status_) {
     status = _status_;
     httpBackend = $httpBackend;
     http = $http;
     scope = $rootScope;
+    timeout = $timeout;
 
     element = angular.element('<div nxg-activity></div>');
     element = $compile(element)($rootScope);
@@ -24,10 +26,14 @@ describe('Directive: nxgActivity', function () {
     expect(element.css('display')).toBe('none');
   });
 
-  it('should display while there are in-progress http requests, and it has not been suppressed', function () {
+  it('should display after delay while there are in-progress http requests, and it has not been suppressed', function () {
     httpBackend.whenGET('/foo').respond('OK');
 
     http.get('/foo');
+    scope.$apply();
+    expect(element.css('display')).toBe('none');
+
+    timeout.flush();
     scope.$apply();
     expect(element.css('display')).not.toBe('none');
 
@@ -46,6 +52,7 @@ describe('Directive: nxgActivity', function () {
 
     status.show();
     scope.$apply();
+    timeout.flush();
     expect(element.css('display')).not.toBe('none');
 
     httpBackend.flush();
