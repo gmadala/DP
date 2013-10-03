88325ca5d35c2521aabd2a841267e5460fdae7c1
Ryan Bell <ryan.bell@effectiveui.com>
2013-10-03 19:01:04 -0400

    tests for business search controller re #822

--- test/spec/controllers/businessSearch.js
+++ test/spec/controllers/businessSearch.js
@@ -6,28 +6,185 @@ describe('Controller: BusinessSearchCtrl', function () {
   beforeEach(module('nextgearWebApp'));
 
   var BusinessSearchCtrl,
-      scope,
-      httpBackend;
+    scope,
+    model,
+    searchResult,
+    dialog;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
-    httpBackend = $httpBackend;
-    httpBackend.when('GET', /\/dealer\/search\/seller\/?.+/).respond({});
+  beforeEach(inject(function ($controller, $rootScope, BusinessSearch, $q) {
+
+    searchResult = {
+      data: {
+        $paginator: {
+          hasMore: function () { return true; }
+        },
+        SearchResults: []
+      }
+    };
+
+    model = BusinessSearch;
+    spyOn(model, 'search').andReturn($q.when(searchResult.data));
+
+    dialog = {
+      close: angular.noop
+    };
+
     scope = $rootScope.$new();
     BusinessSearchCtrl = $controller('BusinessSearchCtrl', {
       $scope: scope,
-      dialog: {
-        close: function() {}
-      },
-      initialQuery: '',
+      dialog: dialog,
+      initialQuery: 'x',
       searchBuyersMode: false
     });
   }));
 
-  // TODO: test this controller properly
+  it('should attach a data model object to the scope with expected defaults', function () {
+    var expected = {
+      searchBuyersMode: false,
+      query: 'x',
+      results: [],
+      loading: true, // a load is started automatically
+      paginator: null,
+      sortBy: 'BusinessName',
+      sortDescending: false
+    };
 
-  it('should attach a close function to the scope', function () {
-    expect(scope.close).toBeDefined();
-    scope.close();
+    expect(angular.equals(scope.data, expected)).toBe(true);
   });
+
+  describe('search function', function () {
+
+    it('should clear any prior results', function () {
+      scope.data.results = ['foo', 'bar'];
+      scope.search();
+      expect(scope.data.results.length).toBe(0);
+    });
+
+    it('should call for data with no paginator to start at beginning', function () {
+      expect(model.search).toHaveBeenCalled();
+      expect(model.search.mostRecentCall.args[4]).toBe(null);
+    });
+
+  });
+
+  describe('fetchNextResults function', function () {
+
+    it('should not call for data if the paginator indicates it is already at the end', function () {
+      var originalCallCount = model.search.calls.length;
+
+      scope.data.paginator = {
+        hasMore: function () {
+          return false;
+        }
+      };
+
+      scope.fetchNextResults();
+      expect(model.search.calls.length).toBe(originalCallCount);
+    });
+
+    it('should call the search method with the expected parameters', function () {
+      scope.data = {
+        searchBuyersMode: true,
+        query: 'xyz',
+        results: [],
+        loading: false,
+        paginator: null,
+        sortBy: 'SomeCrazyField',
+        sortDescending: true
+      };
+      scope.fetchNextResults();
+      expect(model.search).toHaveBeenCalledWith(true, 'xyz', 'SomeCrazyField', true, null);
+    });
+
+    it('should set loading to true while waiting for results', function () {
+      scope.fetchNextResults();
+      expect(scope.data.loading).toBe(true);
+    });
+
+    it('should set loading to false on success', function () {
+      scope.fetchNextResults();
+      scope.$apply();
+      expect(scope.data.loading).toBe(false);
+    });
+
+    it('should set loading to false on error', inject(function ($q) {
+      searchResult.data = $q.reject('oops!');
+      scope.fetchNextResults();
+      scope.$apply();
+      expect(scope.data.loading).toBe(false);
+    }));
+
+    it('should pass back the paginator from previous calls on subsequent ones', function () {
+      var p = {
+        hasMore: function () {
+          return true;
+        }
+      };
+      searchResult.data.$paginator = p;
+      scope.fetchNextResults();
+      scope.$apply();
+      scope.fetchNextResults();
+      expect(model.search.mostRecentCall.args[4]).toBe(p);
+    });
+
+    it('should append new results to the results array', function () {
+      scope.$apply(); // flush initial load promise
+      scope.data.results = ['one', 'two'];
+      searchResult.data.SearchResults = ['three', 'four'];
+      scope.fetchNextResults();
+      scope.$apply();
+      expect(angular.equals(scope.data.results, ['one', 'two', 'three', 'four'])).toBe(true);
+    });
+
+  });
+
+  describe('sortBy function', function () {
+
+    it('should set the sortBy field and default to ascending if non-current-sort field is passed', function () {
+      scope.data.sortDescending = true;
+      scope.data.sortBy = 'bar';
+      scope.sortBy('foo');
+      expect(scope.data.sortBy).toBe('foo');
+      expect(scope.data.sortDescending).toBe(false);
+    });
+
+    it('should just toggle the direction if current-sort field is passed', function () {
+      scope.data.sortDescending = true;
+      scope.data.sortBy = 'bar';
+
+      scope.sortBy('bar');
+      expect(scope.data.sortBy).toBe('bar');
+      expect(scope.data.sortDescending).toBe(false);
+
+      scope.sortBy('bar');
+      expect(scope.data.sortBy).toBe('bar');
+      expect(scope.data.sortDescending).toBe(true);
+    });
+
+    it('should restart the search', function () {
+      spyOn(scope, 'search');
+      scope.sortBy('foo');
+      expect(scope.search).toHaveBeenCalled();
+    });
+
+  });
+
+  it('should have a close function that closes the dialog', function () {
+    spyOn(dialog, 'close');
+    scope.close();
+    expect(dialog.close).toHaveBeenCalledWith();
+  });
+
+  it('should have a select function that closes the dialog with the passed item', function () {
+    var item = {}
+    spyOn(dialog, 'close');
+    scope.select(item);
+    expect(dialog.close).toHaveBeenCalledWith(item);
+  });
+
+  it('should kick off a search', function () {
+    expect(model.search).toHaveBeenCalled();
+  });
+
 });
