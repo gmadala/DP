ee1e6c3bcd8efd10da7ecc805f8b83e6e6c943e6
Joseph (Jos) Smith <joseph.smith@effectiveui.com>
2013-10-04 08:24:01 -0600

    removing orphaned dep

--- app/scripts/models/user.js
+++ app/scripts/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .factory('User', function($q, $location, api, Base64, messages) {
+  .factory('User', function($q, api, Base64, messages) {
     // Private
     var info = null,
       statics = null,
