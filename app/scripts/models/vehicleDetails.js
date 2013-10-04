daac78c281dbd354c082308a8fd9642eea9d3aae
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-10-04 12:55:22 -0400

    Updates vehicle details model to reflect actual api response data.

--- app/scripts/models/vehicleDetails.js
+++ app/scripts/models/vehicleDetails.js
@@ -30,7 +30,7 @@ angular.module('nextgearWebApp')
             titleState: r.TitleState,
             trackingNumber: r.TrackingNumber,
             disbursementDate: r.DisbursementDate,
-            curtailments: r.Curtailments
+            curtailments: r.OriginalCurtailmentSchedule
           };
         });
       }
