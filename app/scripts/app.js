93505c5919afc7dd2a7a7b4aafd8bda5d230c8a8
Joseph (Jos) Smith <joseph.smith@effectiveui.com>
2013-10-07 08:13:31 -0600

    killing the interceptor, lets do this through the api service, if possible

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -136,30 +136,6 @@ angular.module('nextgearWebApp', ['ui.state', 'ui.bootstrap', '$strap.directives
       })
     ;
 
-    // response interceptors are deprecated as of 1.1.4 in favor of more generalized
-    // interceptors which may be pushed onto the $httpProvider.interceptors array
-    var interceptor = ['$rootScope', '$q', function($rootScope, $q) {
-      function success(response) {
-        return response;
-      }
-
-      function error(response) {
-        var status = response.status;
-
-        if (status === 401) {
-          $rootScope.$broadcast('event:requiresAuth');
-        }
-
-        return $q.reject(response);
-      }
-
-      return function (promise) {
-        return promise.then(success, error);
-      };
-    }];
-
-    $httpProvider.responseInterceptors.push(interceptor);
-
   })
   .run(function($rootScope, $location, User) {
 
