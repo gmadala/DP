19caf664bb40d236309109248db6a738aab6463c
Ryan Bell <ryan.bell@effectiveui.com>
2013-10-09 16:02:15 -0400

    improvements to top auctions re #144
    - eliminate duplicate retrieval of data
    - label cleanup

--- app/scripts/controllers/topAuctions.js
+++ app/scripts/controllers/topAuctions.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('TopAuctionsCtrl', function ($scope, dialog, Analytics) {
+  .controller('TopAuctionsCtrl', function ($scope, dialog, auctions) {
 
-	$scope.data = Analytics.fetchTopAuctions();
+    $scope.data = auctions;
 
     // Allow the dialog to close itself using the "Close" button.
     // The current `dialog` is magically injected thanks to AngularUI.
-	$scope.close = function() {
+    $scope.close = function() {
       dialog.close();
     };
-});
+  });
