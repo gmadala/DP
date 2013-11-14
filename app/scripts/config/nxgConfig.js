b2f987be1515a0df61d927cde08cf1d9bcf913f1
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-11-14 18:17:18 -0500

    Added commented out line with production service URL for easy switching.

--- app/scripts/config/nxgConfig.js
+++ app/scripts/config/nxgConfig.js
@@ -2,7 +2,8 @@
 
 angular.module('nextgearWebApp')
   .value('nxgConfig', {
-    apiBase: 'http://test.discoverdsc.com/MobileService/api',
+    apiBase: 'https://test.discoverdsc.com/MobileService/api',
+    //apiBase: 'https://customer.nextgearcapital.com/MobileService/api',  // Production
     segmentIoKey: 'sb06a2jbvj', // production: '9eaffv4cbe', test: 'sb06a2jbvj'
     infiniteScrollingMax: 500,
     showReloadWarning: true
