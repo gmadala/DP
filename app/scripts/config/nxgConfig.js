7a84cbee8c3a62bcf6090ef1870a46ebe08a64a4
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-11-14 17:26:56 -0500

    Added test key to comments for clarity.

--- app/scripts/config/nxgConfig.js
+++ app/scripts/config/nxgConfig.js
@@ -3,7 +3,7 @@
 angular.module('nextgearWebApp')
   .value('nxgConfig', {
     apiBase: 'http://test.discoverdsc.com/MobileService/api',
-    segmentIoKey: 'sb06a2jbvj', // production: '9eaffv4cbe',
+    segmentIoKey: 'sb06a2jbvj', // production: '9eaffv4cbe', test: 'sb06a2jbvj'
     infiniteScrollingMax: 500,
     showReloadWarning: true
   });
