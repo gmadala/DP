263b86fd75587dee0e64a43cbb44b5baeb704f50
Juan Ortiz <juan.ortiz@effectiveui.com>
2013-11-18 19:14:08 -0500

    Setting test URL and keys"

--- app/scripts/config/nxgConfig.js
+++ app/scripts/config/nxgConfig.js
@@ -2,7 +2,8 @@
 
 angular.module('nextgearWebApp')
   .value('nxgConfig', {
-    apiBase: 'https://customer.nextgearcapital.com/MobileService/api',
+    apiBase: 'https://test.discoverdsc.com/MobileService/api',
+    //apiBase: 'https://customer.nextgearcapital.com/MobileService/api',  // Production
     segmentIoKey: '9eaffv4cbe',
     infiniteScrollingMax: 500,
     showReloadWarning: true
