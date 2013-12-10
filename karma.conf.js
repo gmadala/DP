40adff82d3a2d7e5c5caad3e31fd9a265bb16e53
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-12-10 15:22:46 -0500

    Adds polyfill directive to support placeholders in IE <10. Ref. #1214.

--- karma.conf.js
+++ karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(karma) {
       'app/components/fullcalendar/fullcalendar.js',
       'app/components/angular-ui-calendar/src/calendar.js',
       'app/components/moment/moment.js',
+      'app/components/modernizr/modernizr.js',
 	    'app/private-components/chartjs/Chart.js',
 	    'app/private-components/angular-segmentio/angular-segmentio.js',
 	    'app/components/sinon/lib/sinon/util/fake_timers.js',
