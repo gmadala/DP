91c7189fcd85053b31bf514dae34b217d83ac85e
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2013-12-18 14:50:01 -0500

    Adds some custom CSS through user voice interface; moves feedback link to settings dropdown. Refs VO-485.

--- app/scripts/directives/nxgUserVoice/nxgUserVoice.js
+++ app/scripts/directives/nxgUserVoice/nxgUserVoice.js
@@ -15,12 +15,12 @@ angular.module('nextgearWebApp')
     $scope.showClassicWidget = function() {
       UserVoice.push(['showLightbox', 'classic_widget', {
         'mode': 'full',
-        'primary_color': '#cc6d00',
-        'link_color': '#007dbf',
+        'primary_color': '#135889', // $brand
+        'link_color': '#1864A1', // $brand-text
         'default_mode': 'support',
         'forum_id': 233296,
+        'custom_template_id': 21815, // custom stylesheet editable on nextgearcapital.uservoice.com/admin
         'support_tab_name': 'Technical Support'
       }]);
     };
-
   });
