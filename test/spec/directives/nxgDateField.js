32b5cd4f23609aa554c7c9a070bb4853fb01112a
Josh Kramer <josh.kramer@effectiveui.com>
2013-12-21 23:42:45 -0500

    Add support to nxgDateField for disallowing past or future dates, for benefit of VO-522

--- test/spec/directives/nxgDateField.js
+++ test/spec/directives/nxgDateField.js
@@ -32,7 +32,7 @@ describe('Directive: nxgDateField', function () {
   });
 
   describe('notFutureDates functionality', function () {
-    
+
     it('should say the following dates are not in the future', function() {
 
       // Today
@@ -51,11 +51,52 @@ describe('Directive: nxgDateField', function () {
     });
 
     it('should say the following dates are in the future', function() {
-      
+
       // Tomorrow
       var tomorrow = new Date();
       tomorrow.setDate(tomorrow.getDate()+1);
       expect(scope.notFutureDates(tomorrow)).toBeFalsy();
+    });
+
+    it('should say null and NaN dates are in the past', function() {
+
+      expect(scope.notFutureDates(null)).toBeTruthy();
+      expect(scope.notFutureDates(NaN)).toBeTruthy();
+    });
+
+  });
+
+  describe('notPastDates functionality', function () {
+
+    it('should say the following dates are not in the past', function() {
+
+      // Today
+      expect(scope.notPastDates(new Date())).toBeTruthy();
+
+      // Tomorrow
+      var tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate()+1);
+      expect(scope.notPastDates(tomorrow)).toBeTruthy();
+
+      // Today at 11:59pm
+      var today = new Date();
+      today.setHours(23, 59, 59, 999);
+      expect(scope.notPastDates(today)).toBeTruthy();
+
+    });
+
+    it('should say the following dates are in the past', function() {
+
+      // Yesterday
+      var yesterday = new Date();
+      yesterday.setDate(yesterday.getDate()-1);
+      expect(scope.notPastDates(yesterday)).toBeFalsy();
+    });
+
+    it('should say null and NaN dates are in the future', function() {
+
+      expect(scope.notPastDates(null)).toBeTruthy();
+      expect(scope.notPastDates(NaN)).toBeTruthy();
     });
 
   });
@@ -76,6 +117,62 @@ describe('Directive: nxgDateField', function () {
     it('should cause the before-show-day function to be called with date(s) being rendered', function () {
       expect(scope.onDayRender).toHaveBeenCalled();
       expect(angular.isDate(scope.onDayRender.mostRecentCall.args[0])).toBe(true);
+    });
+
+  });
+
+  describe('allow past and future date restriction support', function(){
+    var dateAllow = function(pastOrFuture) {
+      inject(function ($rootScope, $compile) {
+        scope = $rootScope;
+        scope.bar = new Date();
+        scope.onDayRender = jasmine.createSpy('onDayRender');
+        element = angular.element('<form name="form">' +
+          '<input name="dateInput" id="foo" ng-model="bar" nxg-date-field date-allow="'+pastOrFuture+'" />' +
+          '</form>');
+        element = $compile(element)(scope);
+        scope.$digest();
+      });
+    };
+
+    describe('should only allow past values', function() {
+      beforeEach(function(){
+        dateAllow('past');
+      });
+
+      it('with past value', function(){
+        spyOn(scope, 'notFutureDates').andReturn(true); //have the date be a past value
+        scope.bar = new Date();
+        scope.$digest();
+        expect(scope.form.dateInput.$error.past).toBeFalsy();
+      });
+
+      it('with future value', function(){
+        spyOn(scope, 'notFutureDates').andReturn(false); //have the date be a future value
+        scope.bar = new Date();
+        scope.$digest();
+        expect(scope.form.dateInput.$error.past).toBeTruthy();
+      });
+    });
+
+    describe('should only allow future values', function() {
+      beforeEach(function(){
+        dateAllow('future');
+      });
+
+      it('with past value', function(){
+        spyOn(scope, 'notPastDates').andReturn(true); //have the date be a past value
+        scope.bar = new Date();
+        scope.$digest();
+        expect(scope.form.dateInput.$error.future).toBeFalsy();
+      });
+
+      it('with future value', function(){
+        spyOn(scope, 'notPastDates').andReturn(false); //have the date be a future value
+        scope.bar = new Date();
+        scope.$digest();
+        expect(scope.form.dateInput.$error.future).toBeTruthy();
+      });
     });
 
   });
