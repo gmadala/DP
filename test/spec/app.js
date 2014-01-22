2557bcaa352bda4f437a365e20aa874f8553e56a
Josh Kramer <josh.kramer@effectiveui.com>
2014-01-22 18:15:04 -0500

    Added test for app/scripts/app.js

---
+++ test/spec/app.js
@@ -0,0 +1,183 @@
+'use strict';
+
+describe('app.js', function () {
+
+  // load the service's module
+  beforeEach(module('nextgearWebApp'));
+
+  var dealer, user, state, lastState;
+
+  // instantiate service
+  var api, http, rootScope, location, cookieStore;
+  beforeEach(inject(function ($http, $rootScope, User, $state, LastState, $cookieStore, $location) {
+    rootScope = $rootScope;
+    user = User;
+    state = $state;
+    lastState = LastState;
+    location = $location;
+    cookieStore = $cookieStore;
+    User.isDealer = function(){
+      return dealer;
+    };
+
+    User.reloadSession = function(authToken){
+      return {
+        then: function(callback){
+          callback();
+        }
+      };
+    };
+
+    User.initSession = function(savedAuth){
+      return {
+        then: function(callback){
+          callback();
+        }
+      };
+    }
+
+  }));
+
+
+  describe('event:userAuthenticated', function () {
+    it('should restore a previous state', function() {
+
+      spyOn(lastState, 'getUserState').andReturn('userstate');
+      spyOn(lastState, 'clearUserState');
+      spyOn(state, 'transitionTo');
+
+      rootScope.$broadcast('event:userAuthenticated');
+      expect(lastState.getUserState.calls.length).toEqual(3);
+      expect(lastState.clearUserState.calls.length).toEqual(1);
+      expect(state.transitionTo).toHaveBeenCalledWith('userstate');
+
+    });
+
+    it('should go to dealer home if no saved state', function() {
+
+      spyOn(lastState, 'getUserState').andReturn('');
+      spyOn(lastState, 'clearUserState');
+      spyOn(location, 'path');
+      dealer = true;
+
+      rootScope.$broadcast('event:userAuthenticated');
+      expect(lastState.getUserState).toHaveBeenCalled();
+      expect(lastState.clearUserState).not.toHaveBeenCalled();
+      expect(location.path).toHaveBeenCalledWith('/home');
+
+    });
+
+    it('should go to auction home if no saved state', function() {
+
+      spyOn(lastState, 'getUserState').andReturn('');
+      spyOn(lastState, 'clearUserState');
+      spyOn(location, 'path');
+      dealer = false;
+
+      rootScope.$broadcast('event:userAuthenticated');
+      expect(lastState.getUserState).toHaveBeenCalled();
+      expect(lastState.clearUserState).not.toHaveBeenCalled();
+      expect(location.path).toHaveBeenCalledWith('/act/home');
+
+    });
+
+    it('should go to pending state if one exists', function() {
+
+      var toStateObject = {allowAnonymous: false, name: 'myfancystring'};
+
+      // Must do this part to set up "pendingState" variable.
+      spyOn(user, 'isLoggedIn').andReturn(false);
+      spyOn(location, 'path');
+      spyOn(cookieStore, 'get').andReturn(false);
+      rootScope.$broadcast('$stateChangeStart', toStateObject);
+      expect(location.path).toHaveBeenCalledWith('/login');
+
+      spyOn(state, 'transitionTo');
+      rootScope.$broadcast('event:userAuthenticated');
+      expect(state.transitionTo).toHaveBeenCalledWith(toStateObject.name);
+
+    });
+
+
+  });
+
+  describe('event:switchState', function() {
+
+    it('should switch the location path', function() {
+      spyOn(location, 'path');
+      rootScope.$broadcast('event:switchState', {url: 'bar'});
+      expect(location.path).toHaveBeenCalledWith('bar');
+    })
+
+  });
+
+  describe('$stateChangeStart', function() {
+
+    it('should not check type of user if state allows anonymous users', function() {
+      spyOn(user, 'isDealer');
+      rootScope.$broadcast('$stateChangeStart', {allowAnonymous: true});
+      expect(user.isDealer).not.toHaveBeenCalled();
+    });
+
+    it('should restore session from saved auth token', function() {
+      spyOn(user, 'isLoggedIn').andReturn(false);
+      spyOn(cookieStore, 'get').andReturn('savedAuthString');
+      spyOn(user, 'initSession').andCallThrough();
+      spyOn(state, 'transitionTo');
+      var toState = {allowAnonymous: false};
+      rootScope.$broadcast('$stateChangeStart', toState);
+
+      expect(user.initSession).toHaveBeenCalledWith('savedAuthString');
+      expect(state.transitionTo).toHaveBeenCalledWith(toState)
+    });
+
+    it('should redirect to login if user not logged in', function() {
+      spyOn(user, 'isDealer').andCallThrough();
+      spyOn(user, 'isLoggedIn').andReturn(false);
+      spyOn(location, 'path');
+      dealer = true;
+      spyOn(cookieStore, 'get').andReturn(false);
+
+      rootScope.$broadcast('$stateChangeStart', {allowAnonymous: false});
+
+      expect(user.isDealer).toHaveBeenCalled();
+      expect(user.isLoggedIn).toHaveBeenCalled();
+      expect(location.path).toHaveBeenCalledWith('/login');
+    });
+
+    it('should redirect to update security questions', function() {
+      spyOn(user, 'isDealer').andCallThrough();
+      spyOn(user, 'isLoggedIn').andReturn(true);
+      spyOn(user, 'isUserInitRequired').andReturn(true);
+      spyOn(location, 'path');
+      dealer = true;
+
+      rootScope.$broadcast('$stateChangeStart', {allowAnonymous: false});
+
+      expect(location.path).toHaveBeenCalledWith('/login/updateSecurity');
+    });
+
+    it('should redirect auction user if trying to access page for dealer user', function() {
+      spyOn(user, 'isLoggedIn').andReturn(true);
+      spyOn(user, 'isUserInitRequired').andReturn(false);
+      spyOn(location, 'path');
+      dealer = false;
+
+      rootScope.$broadcast('$stateChangeStart', {allowAnonymous: false, isAuctionState: false});
+
+      expect(location.path).toHaveBeenCalledWith('/act/home');
+    });
+
+    it('should redirect dealer user if trying to access page for auction user', function() {
+      spyOn(user, 'isLoggedIn').andReturn(true);
+      spyOn(user, 'isUserInitRequired').andReturn(false);
+      spyOn(location, 'path');
+      dealer = true;
+
+      rootScope.$broadcast('$stateChangeStart', {allowAnonymous: false, isAuctionState: true});
+
+      expect(location.path).toHaveBeenCalledWith('/home');
+    });
+
+  });
+});
