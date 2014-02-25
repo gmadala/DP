95493e0ccea1addf91ebc372af6cfd8899821621
Josh Kramer <josh.kramer@effectiveui.com>
2014-02-25 15:53:27 -0500

    Moved Profile Settings to a separate controller/model from the generic settings controller.
    VO-1216

---
+++ app/scripts/controllers/profileSettings.js
@@ -0,0 +1,282 @@
+'use strict';
+
+angular.module('nextgearWebApp')
+  .controller('ProfileSettingsCtrl', function($scope, $dialog, ProfileSettings, segmentio, metric) {
+    segmentio.track(metric.VIEW_SETTINGS);
+
+    $scope.loading = false;
+
+    // Valid pattern examples: 5552301520, (555)230-1520, 555-230-1520, 1-555-230-1520
+    $scope.phonePattern = /^((\d)-?)?(\((\d{3})\)|(\d{3})-?)(\d{3})-?(\d{4})$/;
+    $scope.cleanPhoneReplacementPattern = '$2$4$5$6$7'; // this matches all the digit groups in the phonePattern regex
+
+    // Password must be at least 8 characters long and contain 3 of the following 4 groups: uppercase, lowercase, number, special characters
+    var prv = {
+      passwordPattern: {
+        digits: /(?=.*\d)/,
+        lowercase: /(?=.*[a-z])/,
+        uppercase: /(?=.*[A-Z])/,
+        special: /(?=.*[!@#$%\^\&\*\(\)\-_\+={}\[\]\\\|;:'"<>,.\?\/`~])/
+      },
+      edit: function() {
+        this.dirtyData = angular.copy(this.data);
+        this.editable = true;
+      },
+      cancel: function() {
+        this.dirtyData = this.validation = null;
+        this.editable = false;
+      },
+      save: function() {
+        if (!this.validate()) {
+          return false;
+        }
+        if (!this.isDirty()) {
+          this.cancel();
+          return false;
+        }
+        segmentio.track(metric.CHANGE_SETTINGS);
+        return true;
+      },
+      saveSuccess: function() {
+        this.data = this.dirtyData;
+        this.dirtyData = this.validation = null;
+        this.editable = false;
+      },
+      /**
+       * We get a notification object for every notification/delivery_method combination. This function
+       * bundles them up so we get a notification that has a list of delivery methods.
+       */
+      bundleNotifications: function(notifications) {
+
+        var hash = {},
+          bundled = [];
+
+        angular.forEach(notifications, function(n) {
+          var deliveryMethod = {
+            DeliveryMethodId: n.DeliveryMethodId,
+            DeliveryMethodName: n.DeliveryMethodName
+          };
+
+          if (hash[n.NotificationId]) {
+            hash[n.NotificationId].DeliveryMethods.push(deliveryMethod);
+          }
+          else {
+            bundled.push(
+              hash[n.NotificationId] = {
+                NotificationId: n.NotificationId,
+                NotificationName: n.NotificationName,
+                DeliveryMethods: [deliveryMethod]
+              }
+            );
+          }
+        });
+        return bundled;
+      },
+      /**
+       * This does the opposite of bundleNotifications. It flattens bundled notification
+       * objects to be consumed by the model.
+       */
+      flattenNotifications: function(notifications) {
+        var flatList = [];
+        angular.forEach(notifications, function(n) {
+          angular.forEach(n.DeliveryMethods, function(d) {
+            flatList.push({
+              NotificationId: n.NotificationId,
+              DeliveryMethodId: d.DeliveryMethodId,
+              Enabled: !!d.Enabled
+            });
+          });
+        });
+        return flatList;
+      }
+    };
+
+    ProfileSettings.get().then(function(results) {
+        $scope.loading = true;
+
+        /** PROFILE **/
+        $scope.profile = {
+          data: {
+            username: results.Username,
+            email: results.Email,
+            phone: results.CellPhone,
+            questions: results.SecurityQuestions,
+            allQuestions: results.AllSecurityQuestions
+          },
+          dirtyData: null, // a copy of the data for editing (lazily built)
+          editable: false,
+          edit: function() {
+            prv.edit.apply(this);
+          },
+          cancel: function() {
+            prv.cancel.apply(this);
+          },
+          save: function() {
+            if (prv.save.apply(this)) {
+              var d = this.dirtyData,
+                cleanPhone = d.phone.replace($scope.phonePattern, '$2$4$5$6$7'); // sanitize phone format
+
+              this.updateQuestionText(d.questions);
+
+              ProfileSettings.saveProfile(d.username, d.password, d.email, cleanPhone, d.questions).then(
+                prv.saveSuccess.bind(this)
+              );
+            }
+          },
+          updateQuestionText: function(questions) {
+            for (var i = 0; i < questions.length; i++) {
+              var q = questions[i];
+              q.QuestionText = this.getQuestionText(q.SecurityQuestionId);
+            }
+          },
+          getQuestionText: function(id) {
+            for (var i = 0; i < this.data.allQuestions.length; i++) {
+              var q = this.data.allQuestions[i];
+              if (id === q.QuestionId) {
+                return q.QuestionText;
+              }
+            }
+            return '';
+          },
+          isDirty: function() {
+            return $scope.userSettings.$dirty;
+          },
+          validate: function() {
+            var profile = $scope.profile;
+            profile.validation = angular.copy($scope.userSettings);
+
+            if (profile.dirtyData.password || profile.dirtyData.passwordConfirm) {
+              // check matching passwords
+              if (profile.dirtyData.password !== profile.dirtyData.passwordConfirm) {
+                profile.validation.setPassword.$error.nomatch = true;
+                profile.validation.$valid = false;
+              }
+              // check valid password pattern
+              else if (profile.validation.setPassword.$dirty && !this.validatePasswordPattern(profile.dirtyData.password)) {
+                profile.validation.setPassword.$error.pattern = true;
+                profile.validation.$valid = false;
+              }
+            }
+            // check all questions have answers && are <= 100 chars long
+            for (var i = 0; i < profile.dirtyData.questions.length; i++) {
+              var q = profile.dirtyData.questions[i];
+              delete q.$error;
+              if (!q.Answer) {
+                q.$error = { required: true };
+                profile.validation.$valid = false;
+              }
+              else if (q.Answer.length > 100) {
+                q.$error = { maxLength: true };
+                profile.validation.$valid = false;
+              }
+            }
+            return profile.validation.$valid;
+          },
+          validatePasswordPattern: function(pwd) {
+            var groupCount = 0;
+            if (prv.passwordPattern.digits.test(pwd)) {
+              groupCount++;
+            }
+            if (prv.passwordPattern.lowercase.test(pwd)) {
+              groupCount++;
+            }
+            if (prv.passwordPattern.uppercase.test(pwd)) {
+              groupCount++;
+            }
+            if (prv.passwordPattern.special.test(pwd)) {
+              groupCount++;
+            }
+            return groupCount >= 3 && pwd.length >= 8;
+          }
+        };
+
+
+        /** NOTIFICATIONS **/
+        $scope.notifications = {
+          data: {
+            selected: prv.bundleNotifications(results.Notifications),
+            available: results.AvailableNotifications
+          },
+          dirtyData: null, // a copy of the data for editing (lazily built)
+          editable: false,
+          edit: function() {
+            prv.edit.apply(this);
+            this.updateAvailableNotification(this.dirtyData);
+          },
+          cancel: function() {
+            prv.cancel.apply(this);
+          },
+          save: function() {
+            if (prv.save.apply(this)) {
+              this.updateSelectedNotifications(this.dirtyData);
+              ProfileSettings.saveNotifications(prv.flattenNotifications(this.dirtyData.available)).then(
+                prv.saveSuccess.bind(this)
+              );
+            }
+          },
+          isDirty: function() {
+            return $scope.notificationSettings.$dirty;
+          },
+          validate: function() {
+            return true;
+            /*nothing can be invalid input*/
+          },
+          updateSelectedNotifications: function(data) {
+            if (data && data.available && data.selected) {
+              var selectedNotifications = [];
+              angular.forEach(data.available, function(notification) {
+                var selectedMethods = [];
+                angular.forEach(notification.DeliveryMethods, function(method) {
+                  if (method.Enabled) {
+                    selectedMethods.push({
+                      DeliveryMethodId: method.DeliveryMethodId,
+                      DeliveryMethodName: method.Name
+                    });
+                  }
+                });
+                if (selectedMethods.length > 0) {
+                  selectedNotifications.push({
+                    NotificationId: notification.NotificationId,
+                    NotificationName: notification.Name,
+                    DeliveryMethods: selectedMethods
+                  });
+                }
+              });
+              data.selected = selectedNotifications;
+            }
+          },
+          updateAvailableNotification: function(data) {
+            var selectionHash = {};
+            // to avoid a performance hit of O(n^4) we extract a hash of all the selected notifications/methods
+            angular.forEach(data.selected, function(selectedNotification) {
+              angular.forEach(selectedNotification.DeliveryMethods, function(selectedMethod) {
+                selectionHash[selectedNotification.NotificationId + '-' + selectedMethod.DeliveryMethodId] = true;
+              });
+            });
+
+            if (data && data.available && data.selected) {
+              angular.forEach(data.available, function(notification) {
+                angular.forEach(notification.DeliveryMethods, function(method) {
+                  method.Enabled = selectionHash[notification.NotificationId + '-' + method.DeliveryMethodId];
+                });
+              });
+            }
+          }
+        };
+      },
+      function(/*reason*/) {
+        $scope.loading = false;
+      });
+
+    $scope.formatPhonenumber = function(item) {
+      if (typeof item === 'string' && item.length === 10) {
+        return '(' + item.substr(0, 3) + ') ' + item.substr(3, 3) + '-' + item.substr(6, 4);
+      }
+      else if (item) {
+        return item.toString();
+      }
+      else {
+        return '';
+      }
+    };
+  });
--- test/spec/controllers/profileSettings.js
+++ test/spec/controllers/profileSettings.js
@@ -1,14 +1,14 @@
 'use strict';
 
-describe('Controller: SettingsCtrl', function () {
+describe('Controller: ProfileSettingsCtrl', function () {
 
   // load the controller's module
   beforeEach(module('nextgearWebApp'));
 
-  var SettingsCtrl,
+  var ProfileSettingsCtrl,
     scope,
     settingsData,
-    SettingsMock;
+    ProfileSettingsMock;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -29,21 +29,7 @@ describe('Controller: SettingsCtrl', function () {
       ],
       Email: 'diana.guarin@manheim.com',
       CellPhone: '2143301800',
-      BusinessEmail: 'diana.guarin@manheim.com',
       EnhancedRegistrationEnabled: false,
-      Addresses: [
-        {
-          BusinessAddressId: 'be9b22cb-5896-4356-86a0-e932293faa6a',
-          City: 'Dallas',
-          Fax: '2143399361',
-          IsTitleReleaseAddress: true,
-          Line1: '5333 West Kiest Blvd',
-          Line2: null,
-          State: 'TX',
-          Zip: '75236',
-          Phone: '2143301800'
-        }
-      ],
       Notifications: [
         {
           NotificationId: 'e4b6c079-3ae0-4ac4-84bc-0474d56bb2f5',
@@ -69,7 +55,7 @@ describe('Controller: SettingsCtrl', function () {
       ]
     };
 
-    SettingsMock = {
+    ProfileSettingsMock = {
       get: function() {
         return {
           then: function(success) {
@@ -80,9 +66,9 @@ describe('Controller: SettingsCtrl', function () {
     }
 
     scope = $rootScope.$new();
-    SettingsCtrl = $controller('SettingsCtrl', {
+    ProfileSettingsCtrl = $controller('ProfileSettingsCtrl', {
       $scope: scope,
-      Settings: SettingsMock
+      ProfileSettings: ProfileSettingsMock
     });
   }));
 
@@ -124,7 +110,7 @@ describe('Controller: SettingsCtrl', function () {
         });
         spyOn(scope.profile, 'isDirty').andReturn(true);
 
-        SettingsMock.saveProfile = function(username, password, email, cleanPhone, questions) {
+        ProfileSettingsMock.saveProfile = function(username, password, email, cleanPhone, questions) {
           savingProfile = {
             username: username,
             password: password,
@@ -186,11 +172,11 @@ describe('Controller: SettingsCtrl', function () {
       it('should not change if not in editing mode', function() {
         validateResult = false;
 
-        spyOn(SettingsMock, 'saveProfile');
+        spyOn(ProfileSettingsMock, 'saveProfile');
 
         scope.profile.save();
 
-        expect(SettingsMock.saveProfile).not.toHaveBeenCalled();
+        expect(ProfileSettingsMock.saveProfile).not.toHaveBeenCalled();
       });
 
     });
@@ -208,155 +194,6 @@ describe('Controller: SettingsCtrl', function () {
 
       it('should require 8 characters', function() {
         expect(scope.profile.validatePasswordPattern('abC7&eu')).toBe(false);
-      });
-
-    });
-
-  });
-
-  describe('business', function() {
-    it('business should exist on scope', function () {
-      expect(scope.business.data).toBeDefined();
-    });
-
-    it('edit function should be defined for business', function() {
-      expect(scope.business.edit).toBeDefined();
-    });
-
-    it('cancel function should be defined for business', function() {
-      expect(scope.business.cancel).toBeDefined();
-    });
-
-    it('save function should be defined for business', function() {
-      expect(scope.business.save).toBeDefined();
-    });
-
-    it('calling edit() on the business should set the business\'s editable property to true', function() {
-      scope.business.edit();
-      expect(scope.business.dirtyData).toEqual(scope.business.data);
-      expect(scope.business.editable).toBe(true);
-    });
-
-    it('calling cancel() on the business should set the business\'s editable property to false', function() {
-      scope.business.cancel();
-      expect(scope.business.dirtyData).toBe(null);
-      expect(scope.business.editable).toBe(false);
-    });
-
-    describe('save()', function(){
-      var savingBusiness, validateResult;
-      beforeEach(function() {
-        validateResult = true;
-        spyOn(scope.business, 'validate').andCallFake(function() {
-          return validateResult;
-        });
-        spyOn(scope.business, 'isDirty').andReturn(true);
-
-        SettingsMock.saveBusiness = function(email, enhancedRegistrationEnabled, enhancedRegistrationPin) {
-          savingBusiness = {
-            email: email,
-            enhancedRegistrationPin: enhancedRegistrationPin,
-            enhancedRegistrationEnabled: enhancedRegistrationEnabled
-          };
-          return {
-            then: function(callback){callback()}
-          };
-        };
-        scope.business.edit();
-      });
-
-      it('should define dirtyData and set editable to false', function() {
-        scope.business.save();
-
-        expect(scope.business.dirtyData).toBeDefined();
-        expect(scope.business.editable).toBe(false);
-      });
-
-      it('should not change if not in editing mode', function() {
-        validateResult = false;
-
-        spyOn(SettingsMock, 'saveBusiness');
-
-        scope.business.save();
-
-        expect(SettingsMock.saveBusiness).not.toHaveBeenCalled();
-      });
-    });
-  });
-
-  describe('title', function() {
-    it('title should exist on scope', function () {
-      expect(scope.title.data).toBeDefined();
-    });
-
-    it('edit function should be defined for title', function() {
-      expect(scope.title.edit).toBeDefined();
-    });
-
-    it('cancel function should be defined for title', function() {
-      expect(scope.title.cancel).toBeDefined();
-    });
-
-    it('save function should be defined for title', function() {
-      expect(scope.title.save).toBeDefined();
-    });
-
-    it('calling edit() on the title should set the title\'s editable property to true', function() {
-      spyOn(scope.title, 'updateAddressSelection');
-      scope.title.edit();
-      expect(scope.title.updateAddressSelection).toHaveBeenCalled();
-      expect(scope.title.dirtyData).toEqual(scope.title.data);
-      expect(scope.title.editable).toBe(true);
-    });
-
-    it('calling cancel() on the title should set the title\'s editable property to false', function() {
-      scope.title.cancel();
-      expect(scope.title.dirtyData).toBe(null);
-      expect(scope.title.editable).toBe(false);
-    });
-
-    describe('save()', function(){
-      var savingTitle, validateResult;
-      beforeEach(function() {
-        validateResult = true;
-        spyOn(scope.title, 'validate').andCallFake(function() {
-          return validateResult;
-        });
-        spyOn(scope.title, 'isDirty').andReturn(true);
-        spyOn(scope.title, 'updateAddressSelection');
-
-        SettingsMock.saveTitleAddress = function(titleAddressId) {
-          savingTitle = {
-            titleAddressId: titleAddressId
-          };
-          return {
-            then: function(callback){callback()}
-          };
-        };
-
-        scope.title.data.titleAddress = {
-          BusinessAddressId: 1
-        };
-
-        scope.title.edit();
-      });
-
-      it('should define dirtyData and set editable to false and call updateAddressSelection', function() {
-        scope.title.save();
-
-        expect(scope.title.updateAddressSelection).toHaveBeenCalled();
-        expect(scope.title.dirtyData).toBeDefined();
-        expect(scope.title.editable).toBe(false);
-      });
-
-      it('should not change if not in editing mode', function() {
-        validateResult = false;
-
-        spyOn(SettingsMock, 'saveTitleAddress');
-
-        scope.title.save();
-
-        expect(SettingsMock.saveTitleAddress).not.toHaveBeenCalled();
       });
 
     });
