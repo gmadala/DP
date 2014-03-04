cd584ebe06e7df711b66080c911164b9dd7ffe04
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2014-03-04 13:40:54 -0500

    Adds interest and fees information to vehicle details, updates mock data. Refs. VO-966, VO-967, VO-968, VO-969, VO-971, VO-811.

--- app/scripts/models/vehicleDetails.js
+++ app/scripts/models/vehicleDetails.js
@@ -35,7 +35,11 @@ angular.module('nextgearWebApp')
             titleState: r.TitleState,
             trackingNumber: r.TrackingNumber,
             disbursementDate: r.DisbursementDate,
-            curtailments: r.Curtailments
+            curtailments: r.Curtailments,
+            interestAccrued: r.interestAccrued,
+            interestApplied: r.interestApplied,
+            feesAccrued: r.feesAccrued,
+            feesApplied: r.feesApplied
           };
         });
       }
