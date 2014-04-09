f306c0a386a4b1ba8f3331106c69b99eed751643
Juan Ortiz <juan.ortiz@effectiveui.com>
2014-04-09 18:15:22 -0400

    Added custom property user_is_dealer to enable conditional showing of the survey (just dealers).

--- app/scripts/services/qualarooSurvey.js
+++ app/scripts/services/qualarooSurvey.js
@@ -1,16 +1,24 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .factory('QualarooSurvey', function () {
+  .factory('QualarooSurvey', function ($window) {
 
     return {
-      init: function(apiKey) {
+      init: function(apiKey, isDealer) {
         var q = document.createElement('script');
         q.type = 'text/javascript';
         q.async = true;
         q.src = '//s3.amazonaws.com/ki.js/' + apiKey + '/boa.js';
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(q, s);
+
+        var api = this.getAPI();
+
+        api.push(['set', { 'user_is_dealer': isDealer ? 'yes' : 'no'}]);
+      },
+      getAPI: function() {
+        $window._kiq = $window._kiq || [];
+        return $window._kiq;
       }
     };
   });
