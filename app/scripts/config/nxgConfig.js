43a04915a9bc589abd090af71636f867e87cd2ae
Juan Ortiz <juan.ortiz@effectiveui.com>
2014-04-29 17:17:57 -0400

    Surveys target specific domains. We have three survey domains setup in Qualaroo (localhost, test, production) and each has its own domainCode which is used to point to the correct script. Added a way to configure which domain code a build should use. Fixes VO-1523

--- app/scripts/config/nxgConfig.js
+++ app/scripts/config/nxgConfig.js
@@ -3,20 +3,27 @@
 angular.module('nextgearWebApp')
   .factory('nxgConfig', function(){
 
+
     // Test
     var apiDomain = 'https://test.discoverdsc.com';
     var segmentIoKey = 'sb06a2jbvj';
     var timeoutMs = 3600000; // 60 minutes
+    var qualarooDomainCode = 'brC';
+
+    // Local
+//    var qualarooDomainCode = 'boa'; // for the survey shown for localhost
 
     // Training (Hotfix Testbed)
 //    var apiDomain = 'https://training.discoverdsc.com';
 //    var segmentIoKey = 'sb06a2jbvj';
 //    var timeoutMs = 900000; // 15 minutes
+//    var qualarooDomainCode = 'brC';
 
     // Production
 //    var apiDomain = 'https://customer.nextgearcapital.com';
 //    var segmentIoKey = '9eaffv4cbe';
 //    var timeoutMs = 900000; // 15 minutes
+//    var qualarooDomainCode = 'bmV';
 
     return {
       apiBase: apiDomain + '/MobileService/api',
@@ -31,7 +38,8 @@ angular.module('nextgearWebApp')
         auctionCustomTemplateId: 23042
       },
       qualarooSurvey: {
-        apiKey: 52803
+        apiKey: 52803,
+        domainCode: qualarooDomainCode
       },
       infiniteScrollingMax: 500,
       sessionTimeoutMs: timeoutMs
