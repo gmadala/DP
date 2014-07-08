6e33275e7436f254f4de18f402b4e48e3e320250
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2014-07-08 14:20:01 -0400

    VO-2580 Auto-select first line of credit in lines array if it only contains 1 line of credit.

--- test/spec/controllers/requestCreditIncrease.js
+++ test/spec/controllers/requestCreditIncrease.js
@@ -26,6 +26,7 @@ describe('Controller: RequestCreditIncreaseCtrl', function () {
       type: 'retail',
       amount: 123.45
     }];
+
     creditIncreaseMock = {
       getActiveLinesOfCredit: function(){
         return $q.when(linesOfCredit);
@@ -66,6 +67,55 @@ describe('Controller: RequestCreditIncreaseCtrl', function () {
     rootScope.$digest();
     expect(scope.selector.linesOfCredit).toEqual(linesOfCredit);
   });
+
+  it('should set the selected line of credit if there is only 1 active line of credit', function() {
+    rootScope.$digest();
+    expect(scope.selector.selectedLineOfCredit).toEqual(linesOfCredit[0]);
+  });
+
+  describe('multiple lines of credit scenario', function() {
+    var scope,
+    RequestCreditIncreaseCtrl,
+    creditIncreaseMock,
+    rootScope,
+    q,
+    linesOfCreditMultiple;
+
+    beforeEach(inject(function($rootScope, $controller, $q){
+      scope = $rootScope.$new();
+      rootScope = $rootScope;
+      q = $q;
+      linesOfCreditMultiple = [{
+        id: 'id1',
+        type: 'retail',
+        amount: 123.45
+      },
+      {
+        id: 'id2',
+        type: 'current',
+        amount: 1987.60
+      }];
+
+      creditIncreaseMock = {
+        getActiveLinesOfCredit: function(){
+          return $q.when(linesOfCreditMultiple);
+        }
+      };
+
+      RequestCreditIncreaseCtrl = $controller('RequestCreditIncreaseCtrl', {
+        $scope: scope,
+        dialog: dialogMock,
+        $dialog: $dialogMock,
+        CreditIncrease: creditIncreaseMock
+      });
+    }));
+
+    it('should not set the selected line of credit an option if there are multiple lines of credit', inject(function($q) {
+      rootScope.$digest();
+      expect(scope.selector.selectedLineOfCredit).toBe(null);
+    }));
+  });
+
 
   it('should not submit the request if the form is invalid', function () {
     scope.requestCreditIncreaseForm = {
