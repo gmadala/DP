b79697a24e78bf1c62f9ca74abb888d8c0d01f33
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2014-07-09 15:13:43 -0400

    VO-2190, VO-2593 Removes all references to defunct vehicleDetails directive and adds temporary link to vehicle details where directive used to be.

--- app/scripts/models/vehicleDetails.js
+++ app/scripts/models/vehicleDetails.js
@@ -4,46 +4,6 @@ angular.module('nextgearWebApp')
   .factory('VehicleDetails', function(api) {
 
     return {
-      // getDetails: function(stockNum) {
-      //   return api.request('GET', '/floorplan/' + stockNum).then(function(r) {
-      //     return {
-      //       vin: r.UnitVin,
-      //       make: r.UnitMake,
-      //       model: r.UnitModel,
-      //       style: r.UnitStyle,
-      //       color: r.UnitColor,
-      //       year: r.UnitYear,
-      //       currentMileage: r.UnitCurrentMileage,
-      //       salvage: r.Salvage,
-      //       status: r.FloorplanStatusName,
-      //       stockNumber: r.StockNumber,
-      //       purchaseDate: r.UnitPurchaseDate,
-      //       dateSubmitted: r.FlooringDate,
-      //       financedAmount: r.AmountFinanced,
-      //       purchaseAmount: r.PurchaseAmount,
-      //       lastPayment: r.LastPaymentDate,
-      //       lineOfCredit: r.LineOfCredit,
-      //       termPlanName: r.TermPlanName,
-      //       productType: r.ProductType,
-      //       seller: r.SellerName,
-      //       sellerAddressLine1: r.SellerAddressLine1,
-      //       sellerAddressLine2: r.SellerAddressLine2,
-      //       sellerAddressCity: r.SellerAddressCity,
-      //       sellerAddressState: r.SellerAddressState,
-      //       sellerAddressZip: r.SellerAddressZip,
-      //       titleLocation: r.TitleLocation,
-      //       titleState: r.TitleState,
-      //       trackingNumber: r.TrackingNumber,
-      //       disbursementDate: r.DisbursementDate,
-      //       interestAccrued: r.InterestAccrued,
-      //       interestApplied: r.InterestApplied,
-      //       feesAccrued: r.FeesAccrued,
-      //       feesApplied: r.FeesApplied,
-      //       inventoryLocation: r.InventoryLocationDescription
-      //     };
-      //   });
-      // }
-
       getLanding: function(stockNumber) {
         return api.request('GET', '/floorplan/mobiledetails/landing/' + stockNumber);
       },
@@ -77,6 +37,5 @@ angular.module('nextgearWebApp')
       getFeeDetails: function(stockNumber, id) {
         return api.request('GET', '/floorplan/mobiledetails/financialsummary/fee/' + stockNumber + '/' + id);
       }
-
     };
   });
