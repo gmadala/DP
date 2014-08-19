102650665086a51da9d670db33c1da362161a817
Juan Ortiz <Juan.Ortiz@effectiveui.com>
2014-08-19 16:34:37 -0400

    VO-2253/VO-2819/VO-2818 - Auction with manufacturing subsidiaries now get a drop down on the Disbursement Details report to get subsidiary reports.

--- app/scripts/controllers/auction.reports.js
+++ app/scripts/controllers/auction.reports.js
@@ -1,13 +1,11 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('AuctionReportsCtrl', function($scope, api, segmentio, metric) {
-
+  .controller('AuctionReportsCtrl', function ($scope, api, segmentio, metric, User) {
     /***
      * The last URI route param of the report endpoints is used so browsers can get it as a default filename
      * when saving the report PDF.
      */
-
     $scope.metric = metric; // make metric names available to template
 
     $scope.data = null;
@@ -23,7 +21,11 @@ angular.module('nextgearWebApp')
       }
     ];
 
-    $scope.viewDisbursementDetail = function() {
+    // set the subsidiary options
+    $scope.subsidiaries = User.getInfo().ManufacturerSubsidiaries || [];
+    $scope.selectedSubsidiary = $scope.subsidiaries.length > 0 ? $scope.subsidiaries[0] : null;
+
+    $scope.viewDisbursementDetail = function () {
 
       // take a snapshot of form state -- view can bind to this for submit-time update of validation display
       $scope.disFormValidity = angular.copy($scope.disForm);
@@ -32,13 +34,25 @@ angular.module('nextgearWebApp')
         return false;
       }
 
-      var date = api.toShortISODate($scope.data.disDate);
-      var strUrl = api.contentLink('/report/disbursementdetail/' + date + ('/Disbursements-' + date /*filename*/), {});
+      var businessId, businessName;
+      var reportDate = api.toShortISODate($scope.data.disDate);
 
-      window.open(
-        strUrl,
-        '_blank'  // open a new window every time
-      );
+      if ($scope.selectedSubsidiary) {
+        businessId = $scope.selectedSubsidiary.BusinessId;
+        businessName = '-' + $scope.selectedSubsidiary.BusinessName.replace(/\W+/g, ''); // remove non-alphanumeric
+      }
+      else {
+        businessId = User.getInfo().BusinessId;
+        businessName = '';
+      }
+
+      var strUrl =
+        api.contentLink('/report/disbursementdetail/v1_1/' +
+          reportDate + '/' +
+          businessId +
+          ('/Disbursements-' + reportDate + businessName /*filename*/), {});
+
+      window.open(strUrl, '_blank'); // open a new window every time
 
       segmentio.track(metric.VIEW_HISTORICAL_REPORT, {
         reportName: 'Disbursement Detail'
--- test/spec/controllers/auction.reports.js
+++ test/spec/controllers/auction.reports.js
@@ -7,14 +7,14 @@ describe('Controller: AuctionReportsCtrl', function() {
 
   var AuctionReportsCtrl,
     scope,
-    dashboard,
     api,
-    formDataMock;
+    formDataMock,
+    mfgSubsidiaries,
+    UserMock;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function($controller, $rootScope, api) {
     scope = $rootScope.$new();
-    api = api;
     formDataMock = {
       $valid: false,
       disDate: {
@@ -22,8 +22,18 @@ describe('Controller: AuctionReportsCtrl', function() {
       }
     };
 
+    UserMock = {
+      getInfo: function() {
+        return {
+          BusinessId: "1234",
+          ManufacturerSubsidiaries: []
+        };
+      }
+    };
+
     AuctionReportsCtrl = $controller('AuctionReportsCtrl', {
-      $scope: scope
+      $scope: scope,
+      User: UserMock
     });
 
     scope.disForm = formDataMock;
@@ -55,9 +65,38 @@ describe('Controller: AuctionReportsCtrl', function() {
       scope.disForm.$valid = true;
 
       scope.viewDisbursementDetail();
-      var expectedStr = '/report/disbursementdetail/2014-01-20/Disbursements-2014-01-20';
+      var expectedStr = '/report/disbursementdetail/v1_1/2014-01-20/1234/Disbursements-2014-01-20';
       expect(window.open).toHaveBeenCalledWith(expectedStr, '_blank');
     });
+
+    it('should create an api link that includes the selected subsidiary\'s id and open the report in a new tab if the form is valid', function() {
+      scope.selectedSubsidiary = {
+        BusinessId: 'subsidiaryId',
+        BusinessName: 'subsidiaryName'
+      };
+
+      spyOn(window, 'open').andReturn();
+      scope.disForm.$valid = true;
+
+      scope.viewDisbursementDetail();
+      var expectedStr = '/report/disbursementdetail/v1_1/2014-01-20/subsidiaryId/Disbursements-2014-01-20-subsidiaryName';
+      expect(window.open).toHaveBeenCalledWith(expectedStr, '_blank');
+    });
+
+    it('should create an api link that includes the selected subsidiary\'s id and with the subsidiary\'s business name stripped of non-alphanumeric characters', function() {
+      scope.selectedSubsidiary = {
+        BusinessId: 'subsidiaryId',
+        BusinessName: 'Subsidiary, @$%^!@  #Num1=+'
+      };
+
+      spyOn(window, 'open').andReturn();
+      scope.disForm.$valid = true;
+
+      scope.viewDisbursementDetail();
+      var expectedStr = '/report/disbursementdetail/v1_1/2014-01-20/subsidiaryId/Disbursements-2014-01-20-SubsidiaryNum1';
+      expect(window.open).toHaveBeenCalledWith(expectedStr, '_blank');
+    });
+
   });
 
 
