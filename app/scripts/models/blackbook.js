b3581bdfe203d52bdd15a2634c4507ceedbe1e50
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2014-08-27 09:56:25 -0400

    VO-2807 Updates multiple VIN results modal.

--- app/scripts/models/blackbook.js
+++ app/scripts/models/blackbook.js
@@ -10,7 +10,7 @@ angular.module('nextgearWebApp')
         backdrop: true,
         keyboard: false,
         backdropClick: false,
-        dialogClass: 'modal modal-large',
+        dialogClass: 'modal modal-medium',
         templateUrl: 'views/modals/multipleVehicles.html',
         controller: 'MultipleVehiclesCtrl',
         resolve: {
