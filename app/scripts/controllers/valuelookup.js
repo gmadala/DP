73730d8b3f7a56106a8f6cdcd9f0cb4385107c45
Alexandra Atzl <alexandra.atzl@effectiveui.com>
2014-09-23 14:02:38 -0400

    VO-2742, VO-3051 Adds vin lookup form to value lookup page.

--- app/scripts/controllers/valuelookup.js
+++ app/scripts/controllers/valuelookup.js
@@ -1,9 +1,40 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('ValueLookupCtrl', function () {
+  .controller('ValueLookupCtrl', function ($scope) {
 
-    console.log('valueeeee');
+    $scope.vinLookup = {
+      vin: null,
+      mileage: null,
+      multipleResults: [],
+      searchInProgress: false,
+      validity: {},
+      resetSearch: function() {
+        $scope.vinLookup.vin = null;
+        $scope.vinLookup.mileage = null;
+      },
+      lookup: function() {
+
+        if (this.validate()) {
+          this.searchInProgress = true;
+          // search blackbook
+
+          // search mmr
+
+        } else {
+          // nothing; inline error labels should display.
+        }
+      },
+      validate: function() {
+        this.validity = angular.copy($scope.vinLookupForm);
+
+        if (!$scope.vinLookupForm.$valid) {
+          return false;
+        }
+
+        return true;
+      }
+    };
 
 
   });
