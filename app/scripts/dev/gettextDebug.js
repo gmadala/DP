f096ecd0da22c4335adb0e46a59b8028b9748de7
chad d <chad@cmfolio.com>
2014-09-25 11:27:29 -0600

    VO-2902 - Added translation debugger tool for local use
    You will now see a Translations panel on the left of the screen when debugging locally. This is to easily switch without the need of using the real switcher. There is also an "English (Debug)" mode which will highlight all the translated strings.
    
    Some of the Spanish translations have been added as well.

--- app/scripts/dev/gettextDebug.js
+++ app/scripts/dev/gettextDebug.js
@@ -3,4 +3,52 @@
 angular.module('nextgearWebApp')
     .run(function(gettextCatalog) {
         gettextCatalog.debug = true;
-    });
+
+        angular.element('body').prepend('\
+          <div id="translationDebugger" ng-controller="TranslationDebuggerCtrl">\
+            <div nxg-translation-debugger></div>\
+          </div>\
+        ');
+    })
+  .controller('TranslationDebuggerCtrl', function ($scope) {
+    $scope.languages = [
+      { key: 'en', name: 'English' },
+      { key: 'enDebug', name: 'English (Debug)' },
+      { key: 'fr_CA', name: 'French (CA)' },
+      { key: 'es', name: 'Spanish' }
+    ];
+  })
+  .directive('nxgTranslationDebugger', function (gettextCatalog) {
+      var template = '\
+        <strong>Translations</strong>\
+        <ul>\
+          <li ng-repeat="lang in languages"><a ng-click="switchLang(lang.key)" ng-class="{ active: lang.key == currentLanguage }">{{ lang.name }}</a></li>\
+        </ul>\
+        ';
+
+      angular.element('head').append('\
+      <style>\
+        #translationDebugger { position: fixed; top: 150px; left: 10px; background: #eee; border-radius: 4px; border: solid 1px #aaa; padding: .5em; opacity: .7 }\
+        #translationDebugger strong { display: block; text-align: center }\
+        #translationDebugger ul { margin: .5em 0 0 0 }\
+        #translationDebugger ul li { margin: 0 0 .5em 0 }\
+        #translationDebugger ul li a { display: block; padding: .5em 1em; border: solid 1px #aaa; border-radius: 4px; color: #333; cursor: pointer }\
+        #translationDebugger ul li a.active { background: white; color: blue; border-color: blue }\
+       </style>\
+      ');
+
+      return {
+        restrict: 'A',
+        template: template,
+        link: function (scope, element, attrs) {
+
+          scope.currentLanguage = gettextCatalog.currentLanguage;
+
+          scope.switchLang = function (lang) {
+            gettextCatalog.setCurrentLanguage(lang);
+            scope.currentLanguage = gettextCatalog.currentLanguage
+          };
+
+        }
+      }
+  });
