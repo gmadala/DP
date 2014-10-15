2079a50536941b46ec827306aaf23483683d50e1
chad d <chad@cmfolio.com>
2014-10-15 15:38:15 -0600

    Auction Documents: Fix for French language docs

--- app/scripts/controllers/auction.documents.js
+++ app/scripts/controllers/auction.documents.js
@@ -4,14 +4,23 @@ angular.module('nextgearWebApp')
   .controller('AuctionDocumentsCtrl', function($scope, metric, gettextCatalog) {
     $scope.metric = metric; // make metric names available to templates
 
+    var languagePrefix = '';
+
+    if (gettextCatalog.currentLanguage === 'fr_CA') {
+      languagePrefix = 'CAF_';
+    }
+    else if (false /* TODO: User is English Canadian */) {
+      languagePrefix = 'CAE_';
+    }
+
     $scope.documents = [
       {
         title: gettextCatalog.getString('Welcome Packet (PDF)'),
-        url: 'documents/NextGear%20Capital%20Welcome%20Packet.pdf'
+        url: 'documents/' + languagePrefix + 'NextGear%20Capital%20Welcome%20Packet.pdf'
       },
       {
         title: gettextCatalog.getString('Instructions for Sellers (PDF)'),
-        url: 'documents/NextGear%20Capital%20Website%20Guide%20-%20Sellers.pdf'
+        url: 'documents/' + languagePrefix + 'NextGear%20Capital%20Website%20Guide%20-%20Sellers.pdf'
       }
     ];
   });
