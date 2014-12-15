b167ff283c5ec827f6eed19fce740a3739682673
johndaley-me <john.daley@nextgearcapital.com>
2014-12-15 12:36:32 -0500

    Use ngen example to output junit xml for CI

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,6 +1,24 @@
-exports.config = {
-  allScriptsTimeout: 10000,
+function mkdir(path, root) {
 
+  var dirs = path.split('/'), dir = dirs.shift(), root = (root||'')+dir+'/';
+
+  try { fs.mkdirSync(root); }
+  catch (e) {
+    //dir wasn't made, something went wrong
+    if(!fs.statSync(root).isDirectory()) throw new Error(e);
+  }
+
+  return !dirs.length||mkdir(dirs.join('/'), root);
+}
+
+var fs = require('fs');
+mkdir('target/surefire-reports/');
+
+//var seleniumVersion = require('../node_modules/protractor/package.json').webdriverVersions.selenium;
+
+exports.config = {
+  //seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-' + seleniumVersion + '.jar',
+  allScriptsTimeout: 10000,
   specs: [
     'e2e-*.js'
   ],
@@ -11,11 +29,20 @@ exports.config = {
     'browserName': 'chrome'
   }],
 
-  baseUrl: 'https://test.discoverdsc.com/eui/',
-
   framework: 'jasmine',
-
+  baseUrl: 'https://test.discoverdsc.com/eui/',
+  allScriptsTimeout: 200000,
+  onPrepare: function() {
+    require('jasmine-reporters');
+    //require('protractor-linkuisref-locator')(protractor);
+    jasmine.getEnv().addReporter(
+      new jasmine.JUnitXmlReporter(null, true, true, './target/surefire-reports/')
+    );
+  },
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    isVerbose: true,
+    showColors: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 600000
   }
 };
