a1b6ce047ae10bb75d136654496e92f44954b568
johndaley-me <john.daley@nextgearcapital.com>
2014-12-19 09:23:24 -0500

    Delete unused harnesses. Existing tests already merged into new harness e2e

--- test/ProtractorTest/Config.js
+++
@@ -1,9 +0,0 @@
-
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['DealerPortalLogin.js'],
-  browserName: 'chrome',
-  onPrepare: function() {
-  	browser.driver.manage().window().maximize();
-  }
-};
