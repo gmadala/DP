8cf3ae6d19979c662b5eaea37f54f44ce0bd5897
gmadala <gayathri.madala@nextgearcapital.com>
2014-12-29 15:26:06 -0500

    WMT-54: Protractor test creation: Dealer Portal Floor Plan navigation

---
+++ e2e/framework/floorplan_page_object.js
@@ -0,0 +1,146 @@
+/**
+ * Created by gayathrimadala on 12/23/14.
+ */
+'use strict';
+
+var FloorPlanObject = function () {
+};
+
+FloorPlanObject.prototype = Object.create({}, {
+
+  floorPlanUrl:{ value: '#/floorplan'
+  },
+  //Locators
+  startDate: {
+    get: function () {
+      return browser.element(by.model('activeCriteria.startDate'));
+    }
+  },
+  endDate: {
+    get: function () {
+      return browser.element(by.model('activeCriteria.endDate'));
+    }
+  },
+  searchFloorPlan: {
+    get: function () {
+      return browser.element(by.model('activeCriteria.query'));
+    }
+  },
+  clearSearch: {
+    get: function () {
+      return browser.element(by.id('clearSearch'));
+    }
+  },
+  floorPlanLinks: {
+    get: function () {
+      return browser.element.all(by.css('.description-narrow a'));
+    }
+  },
+  floorPlanSearch:{
+    get: function () {
+      return browser.element(by.id('floorPlanSearch'));
+    }
+  },
+  invLocations: {
+    get: function() {
+      return browser.element.all(by.model('activeCriteria.inventoryLocation'));
+    }
+  },
+  invLocationsOptions: {
+    get: function() {
+      return browser.element.all(by.options('i.value as i.label for i in inventoryLocations'));
+    }
+  },
+  searchButton: {
+    get: function () {
+      return browser.element(by.css('button.btn-input'));
+    }
+  },
+  clearSearchButton: {
+    get: function(){
+      return browser.element(by.css('button.clear-search'));
+    }
+  },
+
+  //Setters
+  setStartDate :{
+    value: function (startdate) {
+      this.startDate.sendKeys(startdate);
+      browser.waitForAngular();
+    }
+  },
+  setEndDate :{
+    value: function (enddate) {
+      this.endDate.sendKeys(enddate);
+      browser.waitForAngular();
+    }
+  },
+  setSearchFloorPlan  :{
+    value: function (searchVIN) {
+      this.searchFloorPlan.sendKeys(searchVIN);
+      browser.waitForAngular();
+    }
+  },
+  //Doers
+  doStartDate: {
+    value: function (startdate) {
+      this.setStartDate(startdate);
+      browser.waitForAngular();
+    }
+  },
+  doEndDate: {
+    value: function (enddate) {
+      this.setEndDate(enddate);
+      browser.waitForAngular();
+    }
+  },
+  doSearchFloorPlan: {
+    value: function (searchVIN) {
+      this.setSearchFloorPlan(searchVIN);
+      browser.waitForAngular();
+    }
+  },
+  //Navigation
+  goToFlooringStatus: {
+    value: function(){
+      browser.element(by.model('activeCriteria.filter')).$('[value="0"]').click();
+      browser.element(by.model('activeCriteria.filter')).$('[value="1"]').click();
+      browser.element(by.model('activeCriteria.filter')).$('[value="2"]').click();
+      browser.element(by.model('activeCriteria.filter')).$('[value="3"]').click();
+      browser.element(by.model('activeCriteria.filter')).$('[value="4"]').click();
+    }
+  },
+
+  getInventoryLocation:{
+    value : function() {
+      var promise = protractor.promise.defer();
+      this.invLocations.each(function (inventoryLocation) {
+        inventoryLocation.isDisplayed().then(function (displayed) {
+          if (displayed) {
+            promise.fulfill(inventoryLocation);
+          }
+        });
+      });
+      return promise;
+    }
+  },
+
+  goToFloorPlanLinks: {
+    value: function(){
+      var promise = protractor.promise.defer();
+      this.floorPlanLinks.each(function (floorPlanLink) {
+        promise.fulfill(floorPlanLink);
+      });
+      return promise;
+    }
+  },
+
+  goToFloorPlanSearch: {
+    value: function(){
+      this.floorPlanSearch.click();
+    }
+  }
+
+});
+
+module.exports = FloorPlanObject;
---
+++ e2e/tests/floorplan_home_spec.js
@@ -0,0 +1,51 @@
+/**
+ * Created by gayathrimadala on 12/29/14.
+ */
+'use strict';
+
+var FloorPlanObject = require('../framework/floorplan_page_object.js');
+var CredentialsObject = require('../framework/credentials_page_object.js');
+
+var floorPlanPage = new FloorPlanObject();
+var credPage = new CredentialsObject();
+
+//Floor Plan Page
+describe('Dealer Portal Floor Plan Page', function(){
+
+  beforeEach(function(){
+    browser.ignoreSynchronization = true;
+    browser.get(floorPlanPage.floorPlanUrl);
+  });
+
+  it('should find a floor plan - search on VIN', function(){
+    floorPlanPage.doSearchFloorPlan(credPage.findFloorPlanVIN);
+    floorPlanPage.goToFloorPlanSearch();
+  });
+  it('should find a floor plan - search i/p fields', function(){
+    expect(browser.getCurrentUrl()).toContain(floorPlanPage.floorPlanUrl);
+    expect(floorPlanPage.floorPlanLinks.count()).toBeGreaterThan(0);
+    floorPlanPage.goToFlooringStatus();
+    floorPlanPage.doStartDate(credPage.floorPlanStartDate);
+    floorPlanPage.doEndDate(credPage.floorPlanEndDate);
+    //floorPlanPage.goToInvLocSelect();
+  });
+  it('should validate Floor Plan page object is accessing the correct fields.', function () {
+    expect(floorPlanPage.searchFloorPlan.isDisplayed()).toBeTruthy();
+    expect(floorPlanPage.getInventoryLocation()).toBeDefined();
+    expect(floorPlanPage.invLocationsOptions.count()).toBeGreaterThan(2);
+    floorPlanPage.getInventoryLocation().then(function(inventoryLocation) {
+      expect(inventoryLocation.isDisplayed()).toBeTruthy();
+    });
+    expect(floorPlanPage.searchButton.isDisplayed()).toBeTruthy();
+    expect(floorPlanPage.clearSearchButton.isDisplayed()).toBeTruthy();
+  });
+  it('should check for the Floor Plan Links', function(){
+    expect(browser.getCurrentUrl()).toContain(floorPlanPage.floorPlanUrl);
+    expect(floorPlanPage.floorPlanLinks.count()).toBeGreaterThan(0);
+    expect(floorPlanPage.goToFloorPlanLinks()).toBeDefined();
+    floorPlanPage.goToFloorPlanLinks().then(function(floorPlanLink){
+      floorPlanLink.click();
+    });
+  });
+
+});
