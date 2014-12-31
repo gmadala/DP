b2e0436c041e4cb103e85e445e4b72348f192fe0
Win Ribeka <win.ribeka@nextgearcapital.com>
2014-12-31 11:28:29 -0500

    Adding http interceptor in the mock to print request url.

--- app/scripts/config/nxgConfig.mock.js
+++ app/scripts/config/nxgConfig.mock.js
@@ -8,4 +8,19 @@ angular.module('nextgearWebApp')
         apiDomain: ''
       });
     }]);
+  })
+  .config(function ($httpProvider) {
+    $httpProvider.responseInterceptors.push('requestInterceptor');
+  })
+  .factory('requestInterceptor', function ($q) {
+    return function(promise) {
+      return promise.then(function(response) {
+        // do something on success
+        console.log(response.status, response.config.url);
+        return response;
+      }, function(response) {
+        // do something on error
+        return $q.reject(response);
+      });
+    };
   });
