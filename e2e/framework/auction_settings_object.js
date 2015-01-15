64635b043e2eec4fe2a4338062f66f203e342f19
Win Ribeka <win.ribeka@nextgearcapital.com>
2015-01-15 12:09:25 -0500

    WMT-81: Adding tests for each element in the settings page.

--- e2e/framework/auction_settings_object.js
+++ e2e/framework/auction_settings_object.js
@@ -9,8 +9,9 @@ var AuctionSettingsObject = function () {
   this.passwordField = browser.element(by.model('profile.dirtyData.password'));
   this.passwordConfirmField = browser.element(by.model('profile.dirtyData.passwordConfirm'));
   this.phoneField = browser.element(by.model('profile.dirtyData.phone'));
+  this.emailField = browser.element(by.model('profile.dirtyData.email'));
 
-  this.questions = browser.element(by.repeater('question in profile.dirtyData.questions'));
+  this.questions = browser.element.all(by.repeater('question in profile.dirtyData.questions'));
 
   this.userEditSettings = this.userProfileSection.element(by.cssContainingText('button', 'Edit Settings'));
   this.userCancelEdit = this.userProfileSection.element(by.cssContainingText('button', 'Cancel'));
@@ -19,20 +20,27 @@ var AuctionSettingsObject = function () {
   this.businessSettingsSection = browser.element(by.cssContainingText('section', 'Business Settings'));
   this.emailField = browser.element(by.model('business.dirtyData.email'));
   this.pinField = browser.element(by.model('business.dirtyData.enhancedRegistrationPin'));
+  this.enhancedRegistrationToggle = browser.element.all(by.model('business.dirtyData.enhancedRegistrationEnabled'));
 
   this.businessEditSettings = this.businessSettingsSection.element(by.cssContainingText('button', 'Edit Settings'));
   this.businessCancelEdit = this.businessSettingsSection.element(by.cssContainingText('button', 'Cancel'));
   this.businessSaveSettings = this.businessSettingsSection.element(by.cssContainingText('button', 'Save Settings'));
 
   this.titleSettingsSection = browser.element(by.cssContainingText('section', 'Title Settings'));
-  this.addressOptions = browser.element.all(by.options('addr | address:\'oneLineSelect\' for addr in title.data.addresses'))
+  this.addressSelection = browser.element(by.model('title.dirtyData.titleAddress'));
+  this.addressOptions = browser.element.all(by.options('addr | address:\'oneLineSelect\' for addr in title.data.addresses'));
+  this.defaultAddressesLine = this.titleSettingsSection.element(by.cssContainingText('.row-fluid', 'Default'));
+  this.additionalAddressesLine = this.titleSettingsSection.element(by.cssContainingText('.row-fluid', 'Additional'));
+  this.additionalAddressRepeater = this.titleSettingsSection.all(by.repeater('addr in title.data.addresses'));
+  this.addressHelpButtons = this.titleSettingsSection.all(by.css('.btn-help'));
 
   this.titleEditSettings = this.titleSettingsSection.element(by.cssContainingText('button', 'Edit Settings'));
   this.titleCancelEdit = this.titleSettingsSection.element(by.cssContainingText('button', 'Cancel'));
   this.titleSaveSettings = this.titleSettingsSection.element(by.cssContainingText('button', 'Save Settings'));
 
-
   this.notificationsSection = browser.element(by.cssContainingText('section', 'Notifications'));
+  this.notificationDeliveries = browser.element.all(by.repeater('n in notifications.data.selected'));
+  this.notificationAvailable = browser.element.all(by.repeater('n in notifications.dirtyData.available'));
 
   this.notificationsEditSettings = this.notificationsSection.element(by.cssContainingText('button', 'Edit Settings'));
   this.notificationsCancelEdit = this.notificationsSection.element(by.cssContainingText('button', 'Cancel'));
@@ -51,6 +59,18 @@ var AuctionSettingsObject = function () {
 
   this.elementWithCaption = function (section, caption) {
     return section.element(by.cssContainingText('.row-fluid', caption));
+  };
+
+  this.displayedAddressHelp = function () {
+    var promise = protractor.promise.defer();
+    this.addressHelpButtons.each(function (addressHelpButton) {
+      addressHelpButton.isDisplayed().then(function (displayed) {
+        if (displayed) {
+          promise.fulfill(addressHelpButton);
+        }
+      });
+    });
+    return promise;
   };
 
 };
--- e2e/tests/auction_settings_spec.js
+++ e2e/tests/auction_settings_spec.js
@@ -14,19 +14,154 @@ auctionHelper.describe('WMT-81', function () {
     });
 
     it('User profile contains username, password, security question, email and phone.', function () {
-      expect(auctionSettings.usernameField.isDisplayed()).toBeFalsy();
-      auctionSettings.userEditSettings.click();
-      expect(auctionSettings.usernameField.isDisplayed()).toBeTruthy();
+      var userSection = auctionSettings.userProfileSection;
+      expect(auctionSettings.elementWithCaption(userSection, 'User Name').isDisplayed()).toBeTruthy();
+      expect(auctionSettings.elementWithCaption(userSection, 'Email').isDisplayed()).toBeTruthy();
+      expect(auctionSettings.elementWithCaption(userSection, 'Phone').isDisplayed()).toBeTruthy();
 
-      auctionSettings.titleEditSettings.click();
-      auctionSettings.addressOptions.each(function (addressOption) {
-        addressOption.getTagName().then(function (tagName) {
-          console.log('Tag Name: ', tagName);
-        });
-        addressOption.getText().then(function (text) {
-          console.log('Text: ', text);
-        });
+      expect(auctionSettings.usernameField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.passwordField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.passwordConfirmField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.phoneField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.emailField.isDisplayed()).toBeFalsy();
+      auctionSettings.questions.each(function (question) {
+        expect(question.isDisplayed()).toBeFalsy();
       });
+
+      expect(auctionSettings.userSaveSettings.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.userCancelEdit.isDisplayed()).toBeFalsy();
+      auctionSettings.userEditSettings.click();
+      expect(auctionSettings.userSaveSettings.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.userCancelEdit.isDisplayed()).toBeTruthy();
+
+      expect(auctionSettings.usernameField.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.passwordField.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.passwordConfirmField.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.phoneField.isDisplayed()).toBeTruthy();
+      // the email field displayed still returning false here
+      // expect(auctionSettings.emailField.isDisplayed()).toBeTruthy();
+      auctionSettings.questions.each(function (question) {
+        expect(question.isDisplayed()).toBeTruthy();
+      });
+    });
+
+    it('Title settings should contains default address and additional addresses.', function () {
+      expect(auctionSettings.defaultAddressesLine.isDisplayed()).toBeTruthy();
+      auctionSettings.additionalAddressRepeater.count().then(function (count) {
+        if (count > 0) {
+          expect(auctionSettings.additionalAddressesLine.isDisplayed()).toBeTruthy();
+          expect(auctionSettings.displayedAddressHelp()).not.toBeDefined();
+          expect(auctionSettings.addressSelection.isDisplayed()).toBeFalsy();
+
+          expect(auctionSettings.titleSaveSettings.isDisplayed()).toBeFalsy();
+          expect(auctionSettings.titleCancelEdit.isDisplayed()).toBeFalsy();
+          auctionSettings.titleEditSettings.click();
+          expect(auctionSettings.titleSaveSettings.isDisplayed()).toBeTruthy();
+          expect(auctionSettings.titleCancelEdit.isDisplayed()).toBeTruthy();
+
+          expect(auctionSettings.displayedAddressHelp()).toBeDefined();
+          expect(auctionSettings.addressSelection.isDisplayed()).toBeTruthy();
+        }
+      });
+    });
+
+    it('Business settings should contains business email and enhanced registration toggle.', function () {
+      var businessSection = auctionSettings.businessSettingsSection;
+      expect(auctionSettings.elementWithCaption(businessSection, 'Email').isDisplayed()).toBeTruthy();
+      expect(auctionSettings.elementWithCaption(businessSection, 'Enabled').isDisplayed()).toBeTruthy();
+
+      expect(auctionSettings.emailField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.pinField.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.enhancedRegistrationToggle.isDisplayed()).toEqual([false, false]);
+
+      expect(auctionSettings.businessSaveSettings.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.businessCancelEdit.isDisplayed()).toBeFalsy();
+      auctionSettings.businessEditSettings.click();
+      expect(auctionSettings.businessSaveSettings.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.businessCancelEdit.isDisplayed()).toBeTruthy();
+
+      expect(auctionSettings.emailField.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.pinField.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.enhancedRegistrationToggle.isDisplayed()).toEqual([true, true]);
+    });
+
+    it('Notifications should contains notifications type selection.', function () {
+      var notificationSection = auctionSettings.notificationsSection;
+      expect(auctionSettings.elementWithCaption(notificationSection, 'Notifications').isDisplayed()).toBeTruthy();
+      auctionSettings.notificationDeliveries.count().then(function (count) {
+        if (count > 0) {
+          auctionSettings.notificationDeliveries.map(function (notificationDelivery) {
+            var deliveryName = notificationDelivery.evaluate('n.NotificationName');
+            var deliveryMethods = notificationDelivery.evaluate('n.DeliveryMethods');
+            // check the section to see if it contains the data from repeater element
+            notificationSection.getText().then(function (text) {
+              deliveryName.then(function (notificationName) {
+                expect(text).toContain(notificationName);
+              });
+              deliveryMethods.then(function (methodNames) {
+                methodNames.forEach(function (methodName) {
+                  expect(text).toContain(methodName.DeliveryMethodName);
+                });
+              });
+            });
+          });
+        }
+      });
+
+      expect(auctionSettings.notificationsSaveSettings.isDisplayed()).toBeFalsy();
+      expect(auctionSettings.notificationsCancelEdit.isDisplayed()).toBeFalsy();
+      auctionSettings.notificationsEditSettings.click();
+      expect(auctionSettings.notificationsSaveSettings.isDisplayed()).toBeTruthy();
+      expect(auctionSettings.notificationsCancelEdit.isDisplayed()).toBeTruthy();
+
+      auctionSettings.notificationAvailable.count().then(function (count) {
+        if (count > 0) {
+          auctionSettings.notificationAvailable.map(function (notificationAvailable) {
+            var availableName = notificationAvailable.evaluate('n.Name');
+            var availableMethods = notificationAvailable.evaluate('n.DeliveryMethods');
+            // check the section to see if it contains the data from repeater element
+            notificationSection.getText().then(function (text) {
+              availableName.then(function (notificationName) {
+                expect(text).toContain(notificationName);
+              });
+              availableMethods.then(function (methodNames) {
+                methodNames.forEach(function (methodName) {
+                  expect(text).toContain(methodName.Name);
+                });
+              });
+            });
+          });
+        }
+      });
+    });
+  });
+});
+
+auctionHelper.describe('WMT-80', function () {
+  describe('Auction Portal – User Dropdown Content', function () {
+    beforeEach(function () {
+      auctionSettings.openPage();
+      auctionSettings.waitForPage();
+    });
+
+    it('Contains the navigation links for Settings and Sign Out', function () {
+      var logoutButton = auctionHelper.logoutButton;
+      expect(auctionHelper.hasClass(auctionHelper.userInfoDropDown, 'expanded')).toBeFalsy();
+
+      auctionHelper.userInfoLink.click();
+      browser.driver.wait(function () {
+        return logoutButton.isDisplayed();
+      }, 3000);
+
+      expect(auctionHelper.hasClass(auctionHelper.userInfoDropDown, 'expanded')).toBeTruthy();
+      expect(auctionHelper.userInfoDropDown.getText()).toContain('SIGN OUT');
+      expect(auctionHelper.userInfoDropDown.getText()).toContain('SETTINGS');
+      expect(auctionHelper.logoutButton.isDisplayed()).toBeTruthy();
+      auctionHelper.getActiveSettingsButton().then(function (activeSettingsButton) {
+        expect(activeSettingsButton.isDisplayed()).toBeTruthy();
+      });
+
+      auctionHelper.userInfoLink.click();
     });
   });
 });
