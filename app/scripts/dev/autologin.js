e224c7b780c9f1c8597e3bfcb812073b969e1c3b
johndaley-me <john.daley@nextgearcapital.com>
2015-04-07 14:11:44 -0400

    Update credentials for autologin (will fix account lockout)

--- app/scripts/dev/autologin.js
+++ app/scripts/dev/autologin.js
@@ -5,7 +5,7 @@ angular.module('nextgearWebApp')
     var dest = $location.path();
     // List of valid credentials on URL below
     // https://effectiveui.basecamphq.com/projects/10178421-nextgear-desktop-and-mobile-internal/posts/77903221/comments
-    User.authenticate('72694DC', 'weLoveEffectiveUI123@')
+    User.authenticate('53190md', 'ngcpass!0')
       .then(function() {
         $location.path(dest || '/home');
       });
