58626864a27626024383b6c622dedb9adff42db5
Win Ribeka <win.ribeka@nextgearcapital.com>
2015-06-09 10:06:58 -0400

    MNGW-5457: Removing unused reference in scope.

--- app/scripts/controllers/maintenance.js
+++ app/scripts/controllers/maintenance.js
@@ -1,9 +1,6 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('MaintenanceCtrl', function ($scope) {
-    $scope.message = 'myNextGear is currently unavailable while we update it with additional features and benefits.' +
-      ' We apologize for the inconvenience and appreciate your patience while we actively work to improve our service' +
-      ' to you. Thank you for using myNextGear!';
+  .controller('MaintenanceCtrl', function () {
   }
 );
