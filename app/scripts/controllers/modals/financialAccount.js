b8462ed4035c8492bd27b88bc8748451df6fb17d
Win Ribeka <win.ribeka@nextgearcapital.com>
2015-06-16 11:42:02 -0400

    MNGW-3841: Triggering validation messages only when trying to save the account.

--- app/scripts/controllers/modals/financialAccount.js
+++ app/scripts/controllers/modals/financialAccount.js
@@ -5,11 +5,11 @@ angular.module('nextgearWebApp')
     $scope.account = options.account;
     $scope.defaultForBilling = options.defaultForBilling;
     $scope.defaultForDisbursement = options.defaultForDisbursement;
-    $scope.accountNumberDisplay = '******' + $scope.account.AchAccountNumberLast4;
+    $scope.accountNumberDisplay = $scope.account.AchAccountNumberLast4 ? '******' + $scope.account.AchAccountNumberLast4 : '';
     $scope.routingNumberDisplay = $scope.account.AchAbaNumber;
 
     $scope.confirmRequest = function() {
-      console.log($scope.financialAccountForm);
+      $scope.validity = angular.copy($scope.financialAccountForm);
     };
 
     $scope.close = dialog.close;
