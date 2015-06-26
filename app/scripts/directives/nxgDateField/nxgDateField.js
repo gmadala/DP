17acadcc4676093154a828ba0222ac230ad359ff
Win Ribeka <nyoman.ribeka@gmail.com>
2015-06-26 15:38:22 -0400

    MNGW-5600: Updating some unit tests and adding notes on upgrade.

--- app/scripts/directives/nxgDateField/nxgDateField.js
+++ app/scripts/directives/nxgDateField/nxgDateField.js
@@ -88,8 +88,14 @@ angular.module('nextgearWebApp')
 
               // IE9 has an issue with timezones - if date isn't set at midnight,
               // then the date will be wrong for timezones west of GMT. Correct for this
-              if(this.value && this.value.getHours() !== 0) {
-                this.value.setTime(this.value.getTime() + this.value.getTimezoneOffset()*60*1000);
+              // TODO: In 1.3.16, this value returns string value instead of date value.
+              var self = this;
+              if(self.value) {
+                var momentValue = moment(self.value);
+                if (momentValue.hour() !== 0) {
+                  momentValue = momentValue.add(moment().zone(), 'minutes');
+                }
+                self.value = momentValue.format();
               }
 
               // adding this validation here to update the past or future $error after the tz correction.
