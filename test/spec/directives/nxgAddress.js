235cc0217ba9b43cfacc1466b5eb1c6ba7488828
George Ruan <george.ruan@nextgearcapital.com>
2015-07-09 14:43:06 -0400

    MNGW-3840: Implement logic and tests of conversion between state name and state id.

--- test/spec/directives/nxgAddress.js
+++ test/spec/directives/nxgAddress.js
@@ -8,31 +8,57 @@ describe('Directive: nxgAddress', function () {
     scope,
     rootScope,
     compile,
-    mockAddressAccount;
+    user,
+    $q,
+    mockAddress,
+    mockAddressAccount,
+    mockStates;
 
-  beforeEach(inject(function ($rootScope, $compile) {
+  beforeEach(inject(function ($rootScope, $compile, User, _$q_) {
     scope = $rootScope.$new();
     rootScope = $rootScope;
     compile = $compile;
+    user = User;
+    $q = _$q_;
 
-    scope.info = {
+    scope.account = {
       "Line1": '450 W 15th St',
       "Line2": '#8',
-      "City": 'New YorK',
+      "City": 'New York',
       "State": 'New York',
       "Zip": '10011'
     };
 
-    mockAddressAccount = scope.info;
+    mockAddressAccount = scope.account;
+
+    mockStates = {
+      states: [
+        {
+          "StateId": "7c36afed-4941-4e83-b9a5-3a0bcfe44f10",
+          "StateName": "Alabama"
+        },
+        {
+          "StateId": "6a63763d-8c69-44ff-af14-29b0144a21ec",
+          "StateName": "Alaska"
+        },
+        {
+          "StateId": "b1ebdded-b72d-4a12-9f3a-076302e3124a",
+          "StateName": "Alberta"
+        }
+      ]
+    };
+
+    spyOn(user, 'getStatics').andReturn($q.when(mockStates));
 
     element = angular.element(
-      '<nxg-address city="true" info="info" line1="true" line2="true" state="true" validity="{}" zip="true"></nxg-address>'
+      '<nxg-address city="true" info="account" line1="true" line2="true" state="true" validity="{}" zip="true"></nxg-address>'
     );
     element = $compile(element)(scope);
     $rootScope.$digest();
 
     dScope = element.isolateScope();
   }));
+
 
   it('should not contain any fields', function () {
     element = angular.element(
@@ -57,5 +83,16 @@ describe('Directive: nxgAddress', function () {
     expect(dScope.info.City).toEqual(mockAddressAccount.City);
     expect(dScope.info.State).toEqual(mockAddressAccount.State);
     expect(dScope.info.Zip).toEqual(mockAddressAccount.Zip);
+  });
+
+  it('should modify the state of account to be a GUID', function() {
+    dScope.inputs.state = mockStates.states[0];
+    dScope.$digest();
+
+    expect(dScope.info.State).toEqual("7c36afed-4941-4e83-b9a5-3a0bcfe44f10");
+  });
+
+  iit('should get a list of states from endpoint', function() {
+    expect(dScope.states).toEqual(mockStates.states);
   });
 });
