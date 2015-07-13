cf9090b3303820cc1d9ab03f7c088e777b91994e
John Daley <John.Daley@NextGearCapital.com>
2015-07-13 14:53:25 -0400

    Enable debug info for dev mode

---
+++ app/scripts/dev/debugInfo.js
@@ -0,0 +1,17 @@
+(function () {
+
+  'use strict';
+
+  // app/scripts/config/nxgDebugData.js disabled debug info so enable it here
+  // for dev.
+  // https://code.angularjs.org/1.3.16/docs/guide/production
+  angular.module('nextgearWebApp')
+    .config(enableDebugMessage);
+
+  enableDebugMessage.$inject = ['$compileProvider'];
+
+  function enableDebugMessage($compileProvider) {
+    $compileProvider.debugInfoEnabled(true);
+  }
+
+})();
