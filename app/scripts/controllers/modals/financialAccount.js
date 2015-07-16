2d57aea5ddc1e62bdd15200b7f14077d14c017c4
George Ruan <george.ruan@nextgearcapital.com>
2015-07-16 15:10:51 -0400

    MNGW-5686: Display Account Number as read-only and update user input real-time.

--- app/scripts/controllers/modals/financialAccount.js
+++ app/scripts/controllers/modals/financialAccount.js
@@ -12,7 +12,7 @@
     $scope.defaultForDisbursement = options.defaultForDisbursement;
 
     var accountNumber = $scope.account.AccountNumber;
-    $scope.accountNumberDisplay = accountNumber ? '******' + accountNumber.substr(accountNumber.length - 4) : '';
+    $scope.accountNumberDisplay = accountNumber ? '******' + lastFour(accountNumber) : '';
     $scope.routingNumberDisplay = $scope.account.RoutingNumber;
 
     $scope.confirmAccountNumberValid = true;
@@ -24,6 +24,19 @@
 
     $scope.confirmRequest = confirmRequest;
     $scope.close = closeDialog;
+
+    function lastFour(str, prefix) {
+      prefix = prefix || '';
+      if(!str) {
+        return '';
+      }
+      else if(str.length < 4) {
+        return prefix + str;
+      }
+      else {
+        return prefix + str.substr(str.length - 4);
+      }
+    }
 
     // User cannot have an account that is not active and set as either a default disbursement or default payment.
     function activeValid () {
@@ -40,12 +53,14 @@
       $scope.confirmAccountNumberValid = action === 'edit' || confirmAccountNumberValid();
 
       if ($scope.validity.$valid && $scope.activeValid && $scope.confirmAccountNumberValid) {
+        $scope.q = 'q';
         if(action === 'edit') {
           AccountManagement.updateBankAccount($scope.account).then(function () {
             dialog.close($scope.account);
           });
         }
         if(action === 'add') {
+          $scope.account.AccountName = $scope.accountNameDisplay;
           AccountManagement.addBankAccount($scope.account).then(function (bankAccountId) {
             $scope.account.AccountId = bankAccountId;
             dialog.close($scope.account);
@@ -57,6 +72,17 @@
     function closeDialog() {
       dialog.close();
     }
+
+    $scope.$watch(
+      function() {
+        return $scope.account.BankName + $scope.account.AccountNumber;
+      },
+      function() {
+        var bankName = $scope.account.BankName;
+        var accNumber = $scope.account.AccountNumber;
+        $scope.accountNameDisplay = (bankName && accNumber) ? bankName + lastFour(accNumber, ' - ') : '';
+      }
+    );
   }
 
 })();
