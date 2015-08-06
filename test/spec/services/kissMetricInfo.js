01f1581cef80fbd1f27634f121e5f01d0cdc8f2c
NGC\warren.barnes <warren.barnes@nextgearcapital.com>
2015-08-06 11:34:04 -0400

    Fixed unit tests and added an unit test for the new KissMetricInfo service

---
+++ test/spec/services/kissMetricInfo.js
@@ -0,0 +1,40 @@
+'use strict';
+
+describe('Service: kissMetricInfo', function () {
+
+  // load the controller's module
+  beforeEach(module('nextgearWebApp'));
+
+  var $window, kissMetricInfo, BusinessHours, $q,$rootScope;
+
+  beforeEach(inject(function (_kissMetricInfo_,_$window_, _BusinessHours_, _$q_, _$rootScope_){
+
+    kissMetricInfo = _kissMetricInfo_;
+    $window = _$window_;
+    BusinessHours =_BusinessHours_;
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+
+
+    spyOn(BusinessHours,'insideBusinessHours').andReturn($q.when(true));
+  }));
+
+  it('should return true for biz hours if within biz hours and return non-null properties for kissMetric', function() {
+
+    var result;
+
+    kissMetricInfo.getKissMetricInfo().then(function(retVal) {
+      result = retVal;
+    });
+
+    $rootScope.$apply();
+
+    expect(result.isBusinessHours).toBe(true);
+    expect(result.height).not.toBeNull();
+    expect(result.width).not.toBeNull();
+    expect(result.vendor).not.toBeNull();
+    expect(result.version).not.toBeNull();
+
+  });
+
+});
