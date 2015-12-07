242da0597fdc66b75449c507623f704b3abc2291
Ashley Stove <ashley.stove@nextgearcapital.com>
2015-12-07 13:28:26 -0500

    feature flag addition

--- app/scripts/controllers/modals/floorCarMessage.js
+++ app/scripts/controllers/modals/floorCarMessage.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('nextgearWebApp')
-  .controller('FloorCarMessageCtrl', function ($scope, dialog, floorSuccess, uploadSuccess) {
+  .controller('FloorCarMessageCtrl', function ($scope, dialog, canAttachDocuments, floorSuccess, uploadSuccess) {
     // access to all the data the user entered in the form (a copy)
     $scope.floorSuccess = floorSuccess;
     $scope.uploadSuccess = uploadSuccess;
+    $scope.canAttachDocuments = canAttachDocuments;
 
     $scope.close = function () {
       dialog.close();
