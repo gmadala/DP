d707410c1706db9a256bf16d9bb443c6c392e44a
Gayathri Madala <gayathri.madala@nextgearcapital.com>
2016-02-19 12:21:36 -0500

    MNGW-6236 fixed some QA bug MNGW-6468.

--- app/scripts/controllers/auctionSettings.js
+++ app/scripts/controllers/auctionSettings.js
@@ -4,5 +4,5 @@ angular.module('nextgearWebApp')
   .controller('AuctionSettingsCtrl', function($scope, $sce) {
     // Parent Scope for Profile Settings and Account Management
     // for when showing Auction user settings page
-    $scope.passwordTooltip = $sce.trustAsHtml('<h5>Password Reset</h5><p>Passwords must consist of a minimum of 8 characters.</p><p>Passwords must contain 3 of the 4 following types of characters: uppercase, lowercase, number, or special characters.</p>')
+    $scope.passwordTooltip = $sce.trustAsHtml('<h5>Password Reset</h5><p>Passwords must consist of a minimum of 8 characters.</p><p>Passwords must contain 3 of the 4 following types of characters: uppercase, lowercase, number, or special characters.</p>');
   });
