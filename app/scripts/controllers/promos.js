7663444b98f3c9b86f097c80076801149f0995fb
Ashley Stove <ashley.stove@nextgearcapital.com>
2016-02-22 14:31:22 -0500

    sort and display based on date, not just terminated

--- app/scripts/controllers/promos.js
+++ app/scripts/controllers/promos.js
@@ -8,7 +8,19 @@ function PromosCtrl($scope, $filter, api) {
 
     api.request('GET',api.ngenContentLink('/eventsales'),null,null,true, api.ngenSuccessHandler).then(
       function (result) {
-        $scope.promos = $filter('orderBy')(result, 'salesDate', true);
+        var now = new Date();
+        var today = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
+        var promos = $filter('orderBy')(result, 'salesDate', true);
+        $scope.promosCurrent = promos.filter(function(promo){
+          if(promo.salesDate >= today && !promo.terminated){
+            return promo;
+          }
+        });
+        $scope.promosPast = promos.filter(function(promo){
+          if(promo.salesDate < today && !promo.terminated){
+            return promo;
+          }
+        });
       }
     );
   };
