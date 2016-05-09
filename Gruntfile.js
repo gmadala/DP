1099c6f4ab3673188fe2931c013f4c4b50db14a6
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-05-09 09:48:54 -0400

    fixed live reloading

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,26 +48,29 @@ module.exports = function(grunt) {
         tasks: ['compass']
       },
       livereload: {
+        options: {
+            livereload: true
+        },
         files: [
-          '<%= yeoman.app %>/**/*.html',
-          '!<%= yeoman.app %>/index.html',
-          '.tmp/index.html',
-          '{.tmp,<%= yeoman.app %>}/styles/**/*.css',
-          '{.tmp,<%= yeoman.app %>}/client/**/*.js',
-          '{.tmp,<%= yeoman.app %>}/private-components/**/*.js',
-          '<%= yeoman.app %>/styles/img/**/*.{png,jpg,jpeg,gif,webp,svg}'
-        ],
-        tasks: ['livereload']
+        '<%= yeoman.app %>/**/*.html',
+        '!<%= yeoman.app %>/index.html',
+        '.tmp/index.html',
+        '{.tmp,<%= yeoman.app %>}/styles/**/*.css',
+        '{.tmp,<%= yeoman.app %>}/client/**/*.js',
+        '{.tmp,<%= yeoman.app %>}/private-components/**/*.js',
+        '<%= yeoman.app %>/styles/img/**/*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
       },
       html: {
-        files: ['{.tmp,<%= yeoman.app %>}/index.html'],
+        files: ['<%= yeoman.app %>/index.html'],
         tasks: ['processhtml:server']
       }
     },
     connect: {
       options: {
         port: 9000,
-        hostname: '0.0.0.0'
+        hostname: 'localhost',
+        livereload: true
       },
       livereload: {
         options: {
@@ -575,8 +578,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.renameTask('regarde', 'watch');
-
   grunt.registerTask('dev-setup', [
     'gitinfo',
     'env:dev',
@@ -590,7 +591,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('server', [
     'dev-setup',
-    'livereload-start',
     'connect:livereload',
     'shell:chrome',
     'watch'
@@ -598,7 +598,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('server-ie', [
     'dev-setup',
-    'livereload-start',
     'connect:livereload',
     'shell:ie',
     'watch'
@@ -618,7 +617,6 @@ module.exports = function(grunt) {
   grunt.registerTask('test:e2e', [
     'dev-setup',
     'shell:webdriverUpdate',
-    'livereload-start',
     'connect:livereload',
     'protractor:run'
   ]);
