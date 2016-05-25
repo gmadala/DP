8e92b14a9e125d384fb5c0fea0eb5c92a04f47fc
Calderon <Javier.Calderon@nextgearcapital.com>
2016-05-25 09:30:34 -0400

    took out comments in it blocks

--- e2e/tests/login/resources-suite.js
+++ e2e/tests/login/resources-suite.js
@@ -24,52 +24,42 @@ describe("Log In Suite  \n ", function () {
     resources.checkNewTab('https://test.nextgearcapital.com/MobileService/api/dealer/feeschedule/FeeSchedule?AuthToken');
   });
   it("2. As a dealer I want to view welcome packet", function () {
-    //Login and go to request credit increase
     resources.clickWelcomePacket();
     resources.checkNewTab('http://www.nextgearcapital.com/welcome-packet/');
   });
   it("3. As a dealer I want to view dealer funding checklist", function () {
-    //Login and go to request credit increase
     resources.clickDealerFunding();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Dealer%20Funding%20Checklist.pdf');
   });
   it("4. As a dealer I want to view Title Management Frequently Asked Questions", function () {
-    //Login and go to request credit increase
     resources.clickTitleManagement();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Records%20Title%20FAQ.pdf');
   });
   it("5. As a dealer I want to view instructions for buyers", function () {
-    //Login and go to request credit increase
     resources.clickInstructionsForBuyers();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/NextGear%20Capital%20Website%20Guide%20-%20Buyers.pdf');
   });
   it("6. As a dealer I want to view welcome letter", function () {
-    //Login and go to request credit increase
     resources.clickWelcomeLetter();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Welcome%20Letter.pdf');
   });
   it("7. As a dealer I want to view guidelines", function () {
-    //Login and go to request credit increase
     resources.clickGuidelines();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Insurance%20Guidelines.pdf');
   });
   it("8. As a dealer I want to view information sheet", function () {
-    //Login and go to request credit increase
     resources.clickInformationSheet();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Information%20Sheet.pdf');
   });
   it("9. As a dealer I want to view claim form", function () {
-    //Login and go to request credit increase
     resources.clickClaimForm();
     resources.checkNewTab('https://test.nextgearcapital.com/test/documents/Claim%20Form.pdf');
   });
   it("10. As a dealer I want to download IOS app", function () {
-    //Login and go to request credit increase
     resources.clickIosApp();
     resources.checkNewTab('https://itunes.apple.com/us/app/nextgear-capital/id748609885?mt=8');
   });
   it("11. As a dealer I want to download android app", function () {
-    //Login and go to request credit increase
     resources.clickAndroid();
     resources.checkNewTab('https://play.google.com/store/apps/details?id=com.nextgear.mobile');
   });
