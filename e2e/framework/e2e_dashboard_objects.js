1aa55790e311c95e3c10378d40de5c052ef70b39
Richard Lopez <richard.lopez@nextgearcapital.com>
2016-06-28 09:52:33 -0400

    e2e failure

--- e2e/framework/e2e_dashboard_objects.js
+++ e2e/framework/e2e_dashboard_objects.js
@@ -3,7 +3,7 @@
 function DashboardObjects() {
 
   var delay = 500;
-  var longDelay = 2000;
+  //var longDelay = 2000;
 
   //Locators
   this.elReceiptsLink = browser.element(by.css('a[ng-href="#/receipts"]'));
@@ -11,37 +11,37 @@ function DashboardObjects() {
   this.elRequestCreditIncrease = browser.element(by.id('requestCreditButton'));
 
   //Doers
-  this.doResources = function () {
+  this.doResources = function() {
     browser.sleep(browser.sleep(browser.params.longDelay));
     return this.elResourcesLink.click();
   };
-  this.doReceipts = function () {
+  this.doReceipts = function() {
     browser.sleep(browser.sleep(browser.params.longDelay));
     return this.elReceiptsLink.click();
   };
-  this.doRequestCreditIncrease = function () {
+  this.doRequestCreditIncrease = function() {
     browser.sleep(delay);
     this.elRequestCreditIncrease.click();
   };
 
   //Getters
-  this. getPasswordErrorTextPhoneNumber = function () {
+  this.getPasswordErrorTextPhoneNumber = function() {
     return this.passwordErrorPhoneNumbers().get(2).getText();
   };
 
   //Setters
-  this.enterQuestion9 = function (param) {
+  this.enterQuestion9 = function(param) {
     return this.securityQuestion9().clear().sendKeys(param);
   };
 
   //Count
-  this.disabledCount = function () {
+  this.disabledCount = function() {
     return this.disabledFields().count();
   };
   //LAST ONE
-  this.placeholder = function (index) {
-    return  this._thumbnail(index).click();
-  }
+  this.placeholder = function(index) {
+    return this._thumbnail(index).click();
+  };
 
 }
 module.exports.dashboardObjects = DashboardObjects;
