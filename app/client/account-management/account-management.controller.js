583dc9299d40122879023d2d5a475b5ea1d80b06
Warren Barnes <warren.barnes@nextgearcapital.com>
2016-07-21 14:37:28 -0400

    Made the rest of the account management page responsive

--- app/client/account-management/account-management.controller.js
+++ app/client/account-management/account-management.controller.js
@@ -47,7 +47,8 @@
     $scope.contactInfoEnabled = User.getFeatures().hasOwnProperty('contactInfo') ? User.getFeatures().contactInfo.enabled : false;
     $scope.addBankAccountEnabled = User.getFeatures().hasOwnProperty('addBankAccount') ? User.getFeatures().addBankAccount.enabled : false;
     $scope.editBankAccountEnabled = User.getFeatures().hasOwnProperty('editBankAccount') ? User.getFeatures().editBankAccount.enabled : false;
-    $scope.editDefaultAccount = false;
+    $scope.editDefaultDAccount = false;
+    $scope.editDefaultPAccount = false;
     $scope.refreshActiveAchAccounts = refreshActiveAchAccounts;
 
 
@@ -117,7 +118,8 @@
       getData = AccountManagement.get();
     }
 
-    $scope.editDefaultAccount = false;
+    $scope.editDefaultDAccount = false;
+    $scope.editDefaultPAccount = false;
 
     function refreshActiveAchAccounts() {
       AccountManagement.getFinancialAccountData()
@@ -288,7 +290,7 @@
             },
             isEditable: function () {
               return $scope.brand.editable && $scope.business.data.isStakeholder &&
-                $scope.business.data.isStakeholderActive;
+               $scope.business.data.isStakeholderActive;
             },
             isDisplayed: function () {
               return angular.isDefined(results.AutoPayEnabled) && $scope.isDealer && $scope.isUnitedStates &&
@@ -317,15 +319,22 @@
           },
           dirtyData: null, // a copy of the data for editing (lazily built)
           editable: false,
-          edit: function () {
-            $scope.editDefaultAccount = true;
+          editD: function () {
+            $scope.editDefaultDAccount = true;
+            $scope.editDefaultPAccount = false;
             dirtyPayment = $scope.defaultPayment;
             dirtyDeposit = $scope.defaultDeposit;
           },
-          cancel: function () {
+          editP: function () {
+            $scope.editDefaultDAccount = false;
+            $scope.editDefaultPAccount = true;
+            dirtyPayment = $scope.defaultPayment;
+            dirtyDeposit = $scope.defaultDeposit;
+          },          cancel: function () {
             $scope.defaultPayment = dirtyPayment;
             $scope.defaultDeposit = dirtyDeposit;
-            $scope.editDefaultAccount = false;
+            $scope.editDefaultDAccount = false;
+            $scope.editDefaultPAccount = false;
           },
           save: function () {
             if ($scope.defaultDeposit.BankAccountId === $scope.defaultPayment.BankAccountId) {
@@ -340,7 +349,8 @@
                   return AccountManagement.updateBankAccount(account);
                 })
                 .then(function () {
-                  $scope.editDefaultAccount = false;
+                  $scope.editDefaultDAccount = false;
+                  $scope.editDefaultPAccount = false;
                   $scope.financial.data.billingAccount = account;
                   $scope.financial.data.disbursementAccount = account;
                 });
@@ -360,7 +370,8 @@
                   return AccountManagement.updateBankAccount(accounts[1]);
                 })
                 .then(function () {
-                  $scope.editDefaultAccount = false;
+                  $scope.editDefaultDAccount = false;
+                  $scope.editDefaultPAccount = false;
                   $scope.financial.data.disbursementAccount = accounts[0];
                   $scope.financial.data.billingAccount = accounts[1];
                 });
