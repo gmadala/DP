1c643e369048b0518687870ff705c37d2ec3e0da
John Rigsby <john.rigsby@nextgearcapital.com>
2016-07-25 14:06:23 -0400

    removing console.log

--- app/client/shared/directives/waybill/nxg-download-waybill.spec.js
+++ app/client/shared/directives/waybill/nxg-download-waybill.spec.js
@@ -21,7 +21,6 @@ describe("Directive: nxgDownloadWaybill", function () {
       var expectedByteArray = new Uint8Array([84, 101, 115, 116, 83, 116, 114, 105, 110, 103]);
 
       scope.base64ToBlob(base64String, contentType, 512, function (byteArray) {
-        console.log(byteArray);
         resultByteArray = byteArray.slice(0);
       });
 
