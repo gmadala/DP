397d66c5396c6dbb77d6eb642db3a9dffcf9cfe7
Win Ribeka <nyoman.ribeka@gmail.com>
2016-08-05 10:31:31 -0400

    MNGW-6974: Adding basic navigation in the wizard.

--- app/client/floor-vehicle/wizard/wizard-floor.controller.js
+++ app/client/floor-vehicle/wizard/wizard-floor.controller.js
@@ -5,11 +5,50 @@
     .module('nextgearWebApp')
     .controller('WizardFloorCtrl', WizardFloorCtrl);
 
-  WizardFloorCtrl.$inject = [];
+  WizardFloorCtrl.$inject = ['$state'];
 
-  function WizardFloorCtrl() {
+  function WizardFloorCtrl($state) {
     var vm = this;
-    vm.sample = 'This wizard info is coming from the controller';
+    vm.counter = 1;
+    vm.validForm = true;
+
+    vm.pageCount = 3;
+
+    vm.nextAvailable = function() {
+      return vm.counter < vm.pageCount;
+    };
+
+    vm.next = function() {
+      if (vm.nextAvailable()) {
+        vm.counter++;
+        switchState();
+      }
+    };
+
+    vm.previousAvailable = function() {
+      return vm.counter > 1;
+    };
+
+    vm.previous = function() {
+      if (vm.previousAvailable()) {
+        vm.counter--;
+        switchState();
+      }
+    };
+
+    function switchState() {
+      switch (vm.counter) {
+        case 1:
+          $state.go('flooringWizard.car');
+          break;
+        case 2:
+          $state.go('flooringWizard.sales');
+          break;
+        case 3:
+          $state.go('flooringWizard.document');
+          break;
+      }
+    }
   }
 
 })();
