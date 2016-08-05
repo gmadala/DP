64409953d2d33cd046ddf128df1af41530fa2cdf
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-08-05 15:07:18 -0400

    MNGW-7002 -- car info page

--- app/client/floor-vehicle/wizard/car-info.controller.js
+++ app/client/floor-vehicle/wizard/car-info.controller.js
@@ -5,11 +5,54 @@
     .module('nextgearWebApp')
     .controller('CarInfoCtrl', CarInfoCtrl);
 
-  CarInfoCtrl.$inject = [];
+  CarInfoCtrl.$inject = [
+    '$scope'
+  ];
 
-  function CarInfoCtrl() {
+  function CarInfoCtrl(
+    $scope
+  ) {
     var vm = this;
+
     vm.sample = 'This car info is coming from the controller';
+
+    vm.data = null;
+
+    vm.vinDetailsErrorFlag = false;
+
+    vm.defaultData = {
+      PhysicalInventoryAddressId: null, // Location object locally, flatten to string for API tx
+      UnitColorId: null, // Color object locally, flatten to string to API tx
+      UnitMake: null, // string
+      UnitMileage: null, // string
+      UnitModel: null, // string
+      UnitPurchaseDate: $scope.$parent.wizardFloor.today, // Date locally, format to string for API transmission, default is today
+      UnitPurchasePrice: null, // string
+      UnitStyle: null, // string
+      UnitVin: null, // string
+      VinAckLookupFailure: false, // Boolean (whether vehicle data came from VIN or manual attribute entry)
+      UnitYear: null, // int
+      TitleLocationId: null, // TitleLocationOption object locally, flatten to int for API tx
+      TitleTypeId: null // null locally, int extracted from TitleLocationOption object above for API tx
+    };
+
+    vm.setDefaults = function() {
+      _.assign($scope.$parent.wizardFloor.data, vm.defaultData);
+    }
+
+    // User.getStatics().then(function(res) {
+    //   vm.options = res;
+    // });
+    //
+    // User.getInfo().then(function(res) {
+    //   console.log(res);
+    //   vm.options = angular.extend({}, res);
+    // });
+
+    // vm.validate = function(form) {
+    //   $scope.validity = angular.copy(form);
+    //   $scope.$parent.wizardFloor.carInfoValid = form.$valid ? true : false;
+    // };
   }
 
 })();
--- app/client/floor-vehicle/wizard/wizard-floor.controller.js
+++ app/client/floor-vehicle/wizard/wizard-floor.controller.js
@@ -5,11 +5,45 @@
     .module('nextgearWebApp')
     .controller('WizardFloorCtrl', WizardFloorCtrl);
 
-  WizardFloorCtrl.$inject = [];
+  WizardFloorCtrl.$inject = [
+    'moment',
+    '$scope',
+    '$state',
+    '$q',
+    'User',
+    'Addresses'
+  ];
 
-  function WizardFloorCtrl() {
+  function WizardFloorCtrl(
+    moment,
+    $scope,
+    $state,
+    $q,
+    User,
+    Addresses
+  ) {
     var vm = this;
     vm.sample = 'This wizard info is coming from the controller';
+
+    var today = new Date();
+    vm.today = moment([today.getFullYear(), today.getMonth(), today.getDate()]).toDate();
+
+    vm.data = null;
+
+    $q.all([User.getStatics(), User.getInfo()]).then(function(res) {
+      vm.options = angular.extend({}, res[0], res[1]);
+
+      vm.options.locations = Addresses.getActivePhysical();
+    });
+
+    vm.next = function(form, nextState) {
+      vm.validity = angular.copy(form);
+
+      if (form.$valid) {
+        $state.go(nextState);
+      }
+    };
+
   }
 
 })();
