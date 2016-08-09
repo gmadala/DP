43d25c74ec319cf8b5adb2ac60836c8c7d39250b
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-08-09 14:54:59 -0400

    MNGW-6974 -- lots of changes for salesInfo

--- app/client/floor-vehicle/wizard/sales-info.controller.js
+++ app/client/floor-vehicle/wizard/sales-info.controller.js
@@ -5,16 +5,63 @@
     .module('nextgearWebApp')
     .controller('SalesInfoCtrl', SalesInfoCtrl);
 
-  SalesInfoCtrl.$inject = ['$scope'];
+  SalesInfoCtrl.$inject = ['$scope', 'gettext', 'gettextCatalog', 'moment', 'User'];
 
-  function SalesInfoCtrl($scope) {
+  function SalesInfoCtrl($scope, gettext, gettextCatalog, moment, User) {
     var vm = this;
+    console.log($scope);
+    // init a special version of today's date for our datepicker which only works right with dates @ midnight
+    var today = new Date();
+    today = moment([today.getFullYear(), today.getMonth(), today.getDate()]).toDate();
+
+    var purchaseDateDisclaimer = gettext("To floor plan purchases older than 365 days, please contact NextGear Capital" +
+      " Floorplan Services at fundingservices@nextgearcapital.com, attach the Bill of Sale," +
+      " and provide the customer's NextGear Dealer Number.");
+    vm.purchaseDateDisclaimer = gettextCatalog.getString(purchaseDateDisclaimer);
+
+    var aucPurchaseDateDisclaimer = gettext("To floor plan purchases older than 7 days, please contact NextGear Capital " +
+      "Floor plan Services at fundingservices@nextgearcapital.com, attach the Bill of Sale, " +
+      "and provide the customer's NextGear Dealer Number.");
+    vm.aucPurchaseDateDisclaimer = gettextCatalog.getString(aucPurchaseDateDisclaimer);
+
     vm.sample = 'This sales info is coming from the controller';
 
+    vm.dealerMinDate = moment().subtract(364, 'days');
+    vm.auctionMinDate = moment().subtract(6, 'days');
+    vm.maxDate = moment();
+
+    vm.datePicker = {
+      opened: false
+    };
+
+    vm.openDatePicker = function() {
+      vm.datePicker.opened = true;
+    };
+
+    vm.dateFormat = 'MM/dd/yyyy';
+
     $scope.$parent.wizardFloor.transitionValidation = function() {
-      $scope.$parent.wizardFloor.formParts.two = true;
+      console.log("2. transition", $scope);
+      $scope.form.$submitted = true;
+      $scope.$parent.wizardFloor.validity = angular.copy($scope.form);
+      $scope.$parent.wizardFloor.formParts.two = $scope.form.$valid;
       return true;
     };
+
+    // vm.defaultData = {
+    //   FloorplanSourceId: User.isDealer() ? 6 : 7, // 6 for dealer in web app, 7 for auction user
+    //   SaleTradeIn: false, // Boolean, default is no (only dealers that can be paid directly may change this to true)
+    //   BankAccountId: null, // BankAccount object locally, flatten to string for API tx
+    //   UnitPurchaseDate: today, // Date locally, format to string for API transmission, default is today
+    //   UnitPurchasePrice: null, // string
+    //   LineOfCreditId: null, // LineOfCredit object locally, flatten to string for API tx
+    //   PaySeller: null, // Boolean, default is false if user is dealer and buyer payment is possible, otherwise true
+    //   LotNumber: null, // string (AUCTION ONLY)
+    //   // transient local values
+    //   $selectedVehicle: null, // Object returned from VIN lookup, populates BlackBookGroupNumber & BlackBookUvc on tx
+    //   $blackbookMileage: null // cache most recent mileage value used to get updated blackbook data
+    // };
+
 
   }
 
--- app/client/floor-vehicle/wizard/wizard-floor.controller.js
+++ app/client/floor-vehicle/wizard/wizard-floor.controller.js
@@ -5,10 +5,40 @@
     .module('nextgearWebApp')
     .controller('WizardFloorCtrl', WizardFloorCtrl);
 
-  WizardFloorCtrl.$inject = ['$state', '$scope', '$q', 'User', 'AccountManagement', 'Addresses'];
+  WizardFloorCtrl.$inject = [
+    '$state',
+    '$scope',
+    '$q',
+    'User',
+    'AccountManagement',
+    'Addresses',
+    'OptionDefaultHelper',
+    'kissMetricInfo',
+    'segmentio',
+    'metric',
+    'moment'
+  ];
 
-  function WizardFloorCtrl($state, $scope, $q, User, AccountManagement, Addresses) {
+  function WizardFloorCtrl(
+    $state,
+    $scope,
+    $q,
+    User,
+    AccountManagement,
+    Addresses,
+    OptionDefaultHelper,
+    kissMetricInfo,
+    segmentio,
+    metric,
+    moment
+  ) {
     var vm = this;
+    var isDealer = User.isDealer();
+
+    // init a special version of today's date for our datepicker which only works right with dates @ midnight
+    var today = new Date();
+    today = moment([today.getFullYear(), today.getMonth(), today.getDate()]).toDate();
+
     vm.counter = 1;
     vm.validForm = true;
     vm.data = null;
@@ -25,9 +55,87 @@
     $q.all([User.getStatics(), User.getInfo(), AccountManagement.getDealerSummary()]).then(function(result) {
       vm.options = angular.extend({}, result[0], result[1]);
 
+      var activeBankAccounts = _.filter(result[2].BankAccounts, function(bankAccount) {
+        return bankAccount.IsActive === true;
+      });
+
+      vm.options.BankAccounts = _.sortBy(activeBankAccounts, 'AchBankName');
+
       vm.options.locations = Addresses.getActivePhysical();
+
+      var optionListsToDefault = [
+        {
+          scopeSrc: 'wizardFloor.options.BankAccounts',
+          modelDest: 'BankAccountId'
+        },
+        {
+          scopeSrc: 'wizardFloor.paySellerOptions',
+          modelDest: 'PaySeller',
+          useFirst: true
+        }
+      ];
+
+      if (isDealer) {
+        optionListsToDefault.push({
+          scopeSrc: 'wizardFloor.options.locations',
+          modelDest: 'PhysicalInventoryAddressId'
+        }, {
+          scopeSrc: 'wizardFloor.options.LinesOfCredit',
+          modelDest: 'LineOfCreditId'
+        });
+      }
+
+      vm.optionsHelper = OptionDefaultHelper.create(optionListsToDefault);
+      vm.reset();
     });
 
+    User.getPaySellerOptions().then(function(opts) {
+      vm.paySellerOptions = opts;
+    });
+
+    User.canPayBuyer().then(function(canPay) {
+      vm.canPayBuyer = canPay;
+    });
+
+    // form data model template w/ default values for a new blank form - should be considered read-only
+    vm.defaultData = {
+      FloorplanSourceId: User.isDealer() ? 6 : 7, // 6 for dealer in web app, 7 for auction user
+      BankAccountId: null, // BankAccount object locally, flatten to string for API tx
+      LineOfCreditId: null, // LineOfCredit object locally, flatten to string for API tx
+      PaySeller: null, // Boolean, default is false if user is dealer and buyer payment is possible, otherwise true
+      PhysicalInventoryAddressId: null, // Location object locally, flatten to string for API tx
+      SaleTradeIn: false, // Boolean, default is no (only dealers that can be paid directly may change this to true)
+      BusinessId: null, // business search result object locally, flatten to string for API tx
+      UnitColorId: null, // Color object locally, flatten to string to API tx
+      UnitMake: null, // string
+      UnitMileage: null, // string
+      UnitModel: null, // string
+      UnitPurchaseDate: today, // Date locally, format to string for API transmission, default is today
+      UnitPurchasePrice: null, // string
+      UnitStyle: null, // string
+      UnitVin: null, // string
+      VinAckLookupFailure: false, // Boolean (whether vehicle data came from VIN or manual attribute entry)
+      UnitYear: null, // int
+      TitleLocationId: null, // TitleLocationOption object locally, flatten to int for API tx
+      TitleTypeId: null, // null locally, int extracted from TitleLocationOption object above for API tx
+      ConsignerTicketNumber: null, // string (AUCTION ONLY)
+      LotNumber: null, // string (AUCTION ONLY)
+      // transient local values
+      $selectedVehicle: null, // Object returned from VIN lookup, populates BlackBookGroupNumber & BlackBookUvc on tx
+      $blackbookMileage: null // cache most recent mileage value used to get updated blackbook data
+    };
+
+    vm.reset = function () {
+      vm.data = angular.copy(vm.defaultData);
+      vm.files = [];
+      vm.invalidFiles = [];
+      vm.comment = '';
+      vm.optionsHelper.applyDefaults($scope, vm.data);
+      vm.validity = undefined;
+      $scope.$broadcast('reset');
+    };
+
+    // Wizard Nav functions ---------------------------------------------------
     vm.tabClick = function(count) {
       if (canTransition(count)) {
         vm.counter = count;
