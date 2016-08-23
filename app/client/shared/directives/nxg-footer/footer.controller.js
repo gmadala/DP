78d45dce8b26377bf76044569ecc16dbe704a8c7
Jarrod <jarrod.mayer@nextgearcapital.com>
2016-08-23 12:00:17 -0400

    MNGNF-277: Fixed footer to show correct link if user is not logged in.

--- app/client/shared/directives/nxg-footer/footer.controller.js
+++ app/client/shared/directives/nxg-footer/footer.controller.js
@@ -18,41 +18,51 @@
 
     // bind functions for use in spec
     $scope.updateContactLink = updateContactLink;
+    $scope.updateCurrentLang = updateCurrentLang;
 
     // initialize contact link
-    $scope.updateContactLink().then(function () {
+    $scope.updateContactLink();
 
-      $scope.$on('event:userAuthenticated', function () {
-        $scope.updateContactLink();
-      });
-
+    $scope.$on('event:userAuthenticated', function () {
+      $scope.updateContactLink();
     });
 
     // update contact link based on user country / language
     function updateContactLink() {
-      return User.getInfo().then(function () {
-        var currentLang = gettextCatalog.currentLanguage;
 
-        // redirect to 
-        if (!User.isUnitedStates()) {
-          $scope.contactLink = 'https://canada.nextgearcapital.com/';
-        } else {
-          $scope.contactLink = 'https://www.nextgearcapital.com/';
-        }
+      if (User.isLoggedIn()) {
 
-        switch (currentLang) {
-          // fr_CA is hard-coded in language selector, possibly should update to 'fr' since not everyone likes maple syrup eh
-          case 'fr_CA':
-            $scope.contactLink += 'nous-contacter/?lang=fr';
-            break;
-          case 'es':
-            $scope.contactLink += 'contact-us/?lang=es';
-            break;
-          default:
-            $scope.contactLink += 'contact-us';
-            break;
-        }
-      });
+        return User.getInfo().then(function () {
+
+          if (!User.isUnitedStates()) {
+            $scope.contactLink = 'https://canada.nextgearcapital.com/';
+          } else {
+            $scope.contactLink = 'https://www.nextgearcapital.com/';
+          }
+
+          $scope.updateCurrentLang();
+        });
+      } else {
+        $scope.contactLink = 'https://www.nextgearcapital.com/';
+        $scope.updateCurrentLang();
+      }
+
     }
+
+    function updateCurrentLang() {
+      switch (gettextCatalog.currentLanguage) {
+        // fr_CA is hard-coded in language selector, possibly should update to 'fr' since not everyone likes maple syrup eh
+        case 'fr_CA':
+          $scope.contactLink += 'nous-contacter/?lang=fr';
+          break;
+        case 'es':
+          $scope.contactLink += 'contact-us/?lang=es';
+          break;
+        default:
+          $scope.contactLink += 'contact-us';
+          break;
+      }
+    }
+
   }
 })();
