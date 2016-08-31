2921ea46c9c95f8ebf5461fe5e731d9cef7e6410
Win Ribeka <nyoman.ribeka@gmail.com>
2016-08-31 15:03:21 -0400

    MNGW-6983: Initial stab on the directive to display value lookup.

--- app/client/shared/directives/nxg-value-lookup/nxg-value-lookup.directive.js
+++ app/client/shared/directives/nxg-value-lookup/nxg-value-lookup.directive.js
@@ -1,3 +1,71 @@
-/**
- * Created by win.ribeka on 8/31/16.
- */
+(function() {
+
+  'use strict';
+
+  angular
+    .module('nextgearWebApp')
+    .directive('nxgValueLookup', nxgCreditAvailability);
+
+  nxgCreditAvailability.$inject = [];
+
+  function nxgCreditAvailability() {
+
+    return {
+      templateUrl: 'client/shared/directives/nxg-value-lookup/nxg-value-lookup.template.html',
+      restrict: 'E',
+      scope: {
+        vin: '=',
+        odometer: '=',
+        zipCode: '='
+      },
+      replace: 'true',
+      link: linkFn
+    };
+
+    function linkFn(scope) {
+      var options = {
+
+      };
+      element.find('.nxg-value-lookup').highcharts(options);
+
+      scope.$watch('vin', function(newValue, oldValue) {
+        // skip doing anything when the value is not changing
+        if (oldValue === newValue) {
+          return;
+        }
+
+        // update blackbook and mmr values
+        if (scope.odometer) {
+          // only update the kbb if the zipCode is set
+          if (scope.zipCode) {
+          }
+        }
+      });
+
+      scope.$watch('odometer', function(newValue, oldValue) {
+        // skip doing anything when the value is not changing
+        if (oldValue === newValue) {
+          return;
+        }
+
+        // update blackbook and mmr values
+        if (scope.vin) {
+          // only update the kbb if the zipCode is set
+          if (scope.zipCode) {
+          }
+        }
+      });
+
+      scope.$watch('zipCode', function(newValue, oldValue) {
+        // skip doing anything when the value is not changing
+        if (oldValue === newValue) {
+          return;
+        }
+
+        // update the kbb value only
+        if (scope.vin && scope.odometer) {
+        }
+      });
+    }
+  }
+})();
