44acdbb425b0d20add3e1993e8002d11c3defb30
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-09-15 16:03:24 -0400

    MNGW-6975 -- Fix business v2 modal so you cannot close by clicking the backdrop

--- app/client/floor-vehicle/nxg-business-field-v2/nxg-business-field.controller.js
+++ app/client/floor-vehicle/nxg-business-field-v2/nxg-business-field.controller.js
@@ -38,9 +38,9 @@
       if (!searchOpen) {
         var dialogOptions = {
           dialogClass: 'modal modal-x-large',
-          backdrop: true,
+          backdrop: 'static',
           keyboard: true,
-          backdropClick: true,
+          backdropClick: false,
           templateUrl: 'client/dealer-search/business-search-modal/business-search.template.html',
           controller: 'BusinessSearchCtrl',
           resolve: {
