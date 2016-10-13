6e3111b1c05e758887bbd3be60a0109d7bd8a307
Jarrod <jarrod.mayer@nextgearcapital.com>
2016-10-13 12:04:04 -0400

    ngReact: Updated to fix build error

--- app/client/resources/auction/auction-resources.controller.js
+++ app/client/resources/auction/auction-resources.controller.js
@@ -1,18 +1,18 @@
-(function() {
-    function AuctionDocumentsCtrl($scope, gettextCatalog, User, segmentio, metric, kissMetricInfo) {
+( function() {
+  function AuctionDocumentsCtrl( $scope, gettextCatalog, User, segmentio, metric, kissMetricInfo ) {
 
-        // passing things to react aww yeah
-        $scope.reactProps = {
-            isUnitedStates: User.isUnitedStates(),
-            language: gettextCatalog.currentLanguage,
-            segmentio,
-            kissMetricInfo
-        }
+    // passing things to react aww yeah
+    $scope.reactProps = {
+      isUnitedStates: User.isUnitedStates(),
+      language: gettextCatalog.currentLanguage,
+      segmentio: segmentio,
+      kissMetricInfo: kissMetricInfo
     }
+  }
 
-    angular
-        .module('nextgearWebApp')
-        .controller('AuctionDocumentsCtrl', AuctionDocumentsCtrl);
+  angular
+    .module( 'nextgearWebApp' )
+    .controller( 'AuctionDocumentsCtrl', AuctionDocumentsCtrl );
 
-    AuctionDocumentsCtrl.$inject = ['$scope', 'gettextCatalog', 'User', 'segmentio', 'metric', 'kissMetricInfo'];
-})();
+  AuctionDocumentsCtrl.$inject = [ '$scope', 'gettextCatalog', 'User', 'segmentio', 'metric', 'kissMetricInfo' ];
+} )();
--- app/client/resources/dealer/resources.controller.js
+++ app/client/resources/dealer/resources.controller.js
@@ -1,19 +1,19 @@
-(function() {
-    function DocumentsCtrl($scope, api, metric, gettextCatalog, User, kissMetricInfo, segmentio) {
+( function() {
+  function DocumentsCtrl( $scope, api, metric, gettextCatalog, User, kissMetricInfo, segmentio ) {
 
-        // passing things to react aww yeah
-        $scope.reactProps = {
-            isUnitedStates: User.isUnitedStates(),
-            language: gettextCatalog.currentLanguage,
-            segmentio,
-            kissMetricInfo,
-            api
-        }
+    // passing things to react aww yeah
+    $scope.reactProps = {
+      isUnitedStates: User.isUnitedStates(),
+      language: gettextCatalog.currentLanguage,
+      segmentio: segmentio,
+      kissMetricInfo: kissMetricInfo,
+      api: api
     }
+  }
 
-    angular
-        .module('nextgearWebApp')
-        .controller('DocumentsCtrl', DocumentsCtrl);
+  angular
+    .module( 'nextgearWebApp' )
+    .controller( 'DocumentsCtrl', DocumentsCtrl );
 
-    DocumentsCtrl.$inject = ['$scope', 'api', 'metric', 'gettextCatalog', 'User', 'kissMetricInfo', 'segmentio'];
-})();
+  DocumentsCtrl.$inject = [ '$scope', 'api', 'metric', 'gettextCatalog', 'User', 'kissMetricInfo', 'segmentio' ];
+} )();
