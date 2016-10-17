d1cfeac7080bcb5939e738aec6e9e5e9c89896b7
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-10-17 13:35:39 -0400

    MNGW-7156 -- attempt rev fix 5ish or something

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,7 +242,7 @@ module.exports = function(grunt) {
       options: {
         assetsDirs: ['<%= yeoman.dist %>'],
         patterns: {
-          html: [[ /<img[^\>]+src=['"]([^"']+)["']/gm, 'Update the HTML with the new img filenames']],
+          html: [[ /<img[^\>]+src=['"]([^"']+)["']/gm, 'Update the HTML with the new revved images']],
           js: [[/(img\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']],
           css: [[/(img\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the CSS to reference our revved images']]
         }
