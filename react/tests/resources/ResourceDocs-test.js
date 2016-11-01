eba050603c61c27a21a4a779cd0e5568b4bc3ca8
Jarrod <jarrod.mayer@nextgearcapital.com>
2016-11-01 13:50:43 -0400

    MNGNF-470: Updated resourceDocs test to add new variable, updated package script call

--- react/tests/resources/ResourceDocs-test.js
+++ react/tests/resources/ResourceDocs-test.js
@@ -5,16 +5,16 @@ import ListItemLink from '../../app/shared/ListItemLink';
 
 describe( 'ResourceDocs', () => {
   const onClick = sinon.spy();
+  const titleKey = 'resources.resourceDocs.documents';
   const docs = [ { name: 'test', url: 'https://www.test.com', metric: 'test', id: '1' } ];
 
   it( 'renders a ListItemLink for docs when passed in', () => {
-    const wrapper = shallow( <ResourceDocs docs={ docs } handleClick={ onClick } /> );
-
+    const wrapper = shallow( <ResourceDocs docs={ docs } handleClick={ onClick } titleKey={titleKey} />);
     expect( wrapper.find(ListItemLink) ).to.have.length( 1 );
   } );
 
   it( 'renders a ListItemLink for collat docs when passed in', () => {
-    const wrapper = shallow( <ResourceDocs docs={ [] } collateralDocs={ docs } handleClick={ onClick } /> );
+    const wrapper = shallow( <ResourceDocs docs={ [] } collateralDocs={ docs } handleClick={ onClick } titleKey={titleKey} /> );
 
     expect( wrapper.find(ListItemLink) ).to.have.length( 1 );
   } );
