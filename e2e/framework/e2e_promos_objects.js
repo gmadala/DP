0633a97b2f8f99bbfa4caec91c5bab5061508757
bala.krishnamoorthy <bala.nithiya@nextgearcapital.com>
2016-11-10 09:59:19 -0500

    changed conf.js file for running all scripts MNGNF-497_bala_refractor

--- e2e/framework/e2e_promos_objects.js
+++ e2e/framework/e2e_promos_objects.js
@@ -13,7 +13,6 @@ function Promos() {
   this.elTitle = element(by.css('h6.info-block-text'));
   this.elHeader = element(by.css('h2.well-title'));
   this.elPromos = element(by.id('showorhide'));
-  // this.elPromotionsDetails = element(by.css('div.promo-item.inactive.row.empty-promos'));
   this.elPromotionsDetails = element(by.css('div.promo-container'));
 
   //Doers
