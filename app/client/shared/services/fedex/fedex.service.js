000500bdbe1ae9eb7abea46fd2d71217867103a1
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-11-11 15:36:15 -0500

    MNGW-7249 -- MNGW-7249 -- refactored kissMetric code for fedex part 2

--- app/client/shared/services/fedex/fedex.service.js
+++ app/client/shared/services/fedex/fedex.service.js
@@ -21,13 +21,20 @@
         return false;
       },
       getWaybill: function (businessId, wizardStatus) {
+
+        console.log('Entered get way bill function');
+
         return api.request('GET', api.ngenContentLink('/fedex/waybill/' + businessId), null, null, true).then(function (response) {
+
+          console.log('in success handler');
 
           kissMetricInfo.getKissMetricInfo().then(function (result) {
             result.FedExTrackingNumber = response.data.trackingNumber;
             result.fromWizard = wizardStatus === null ? false : wizardStatus;
             segmentio.track(metric.WAYBILL_PRINTED, result);
           });
+
+          console.log('pass kiss metric call');
 
           return {
             waybill: response.data.labelImage,
