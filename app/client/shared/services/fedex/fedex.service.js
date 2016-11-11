9f9c9e650ecbbaa0d63b5641a3655da624f0fc7c
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-11-11 15:28:43 -0500

    MNGW-7249 -- MNGW-7249 -- refactored kissMetric code for fedex

--- app/client/shared/services/fedex/fedex.service.js
+++ app/client/shared/services/fedex/fedex.service.js
@@ -23,29 +23,20 @@
       getWaybill: function (businessId, wizardStatus) {
         return api.request('GET', api.ngenContentLink('/fedex/waybill/' + businessId), null, null, true).then(function (response) {
 
-          handleNgenSucessRequest(response, wizardStatus, function() {
-            return {
-              waybill: response.data.labelImage,
-              trackingNumber: response.data.trackingNumber,
-              wizardStatus: wizardStatus
-            };
+          kissMetricInfo.getKissMetricInfo().then(function (result) {
+            result.FedExTrackingNumber = response.data.trackingNumber;
+            result.fromWizard = wizardStatus === null ? false : wizardStatus;
+            segmentio.track(metric.WAYBILL_PRINTED, result);
           });
+
+          return {
+            waybill: response.data.labelImage,
+            trackingNumber: response.data.trackingNumber,
+            wizardStatus: wizardStatus
+          };
+
         });
       }
     };
-
-    function handleNgenSucessRequest(response, wizardStatus, callback) {
-      api.resetSessionTimeout();
-      console.log('wizardStatus: ' + wizardStatus);
-      console.log(response);
-
-      kissMetricInfo.getKissMetricInfo().then(function (result) {
-        result.FedExTrackingNumber = response.data.trackingNumber;
-        result.fromWizard = wizardStatus === null ? false : wizardStatus;
-        segmentio.track(metric.WAYBILL_PRINTED, result);
-      });
-
-      return callback;
-    }
   }
 })();
