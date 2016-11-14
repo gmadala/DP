f138de1ebf2b16a5b815a63dd5d04ff9847df65d
Win Ribeka <nyoman.ribeka@gmail.com>
2016-11-14 12:54:21 -0500

    Running all e2e tests.

--- e2e/conf.js
+++ e2e/conf.js
@@ -36,7 +36,7 @@ exports.config = {
   },
 
   //Spec patterns are relative to the current working directly when protractor is called.
-  specs: ['tests/e2e_spec/e2e_resources_spec.js'],
+  specs: ['tests/e2e_spec/*_spec.js'],
 
   //More miscellaneous configuration options
   directConnect: false,
