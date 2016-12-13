c05b90b330769780125fde2df649cb580054ff26
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2016-12-13 13:32:20 -0500

    MNGW-7266 -- MNGW-7266 -- removed only from describe

--- react/tests/dashboard/ribbon-test.js
+++ react/tests/dashboard/ribbon-test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import Ribbon from '../../app/dashboard/Ribbon'
 import RibbonItem from '../../app/dashboard/RibbonItem';
 
-describe.only('Ribbon', ( ) => {
+describe('Ribbon', ( ) => {
     const navfloorplan = sinon.spy( );
     const floorplanshow = true;
     const floorplancount = 12;
