c2bda18501c65bb0a50cfb4ad620c0dff5f1f606
Richard Lopez <richard.lopez@nextgearcapital.com>
2016-12-19 10:12:02 -0500

    MNGW-7294 Move rename file to wizard controller

--- app/client/floor-vehicle/wizard/document-info.controller.js
+++ app/client/floor-vehicle/wizard/document-info.controller.js
@@ -13,26 +13,6 @@
 
     $scope.$parent.wizardFloor.stateChangeCounterFix(4);
 
-    $scope.$parent.wizardFloor.renameFile = function (file, index) {
-      var filename = "";
-      var dotPos = 0;
-      // Get all files before the current file
-      var firstXFiles = _.first($scope.$parent.wizardFloor.data.files, index);
-      // Get all files that have same name as file
-      var fileList = _.map(_.where(firstXFiles, {'name': file}), 'name');
-      // If there are other files with the same name need to add index to file name
-      var fileIndex = fileList.length;
-
-      if (fileIndex > 0) {
-        dotPos = file.lastIndexOf(".");
-        filename = file.substring(0, dotPos) + fileIndex + file.substring(dotPos);
-        return filename;
-      }
-      else {
-        return file;
-      }
-    };
-
     $scope.$watch('$scope.$parent.wizardFloor.data.files', function (newValue, oldValue) {
       if (newValue && oldValue) {
         if (newValue.length !== oldValue.length) {
