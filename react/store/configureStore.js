46bfbc08155e8c1ea904c6141a060321428dd4f6
Mayer, Jarrod <Jarrod.Mayer@nextgearcapital.com>
2017-01-04 13:00:39 -0500

    redux-framework - missed file in last commit

--- react/store/configureStore.js
+++ react/store/configureStore.js
@@ -1,17 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
-
 import logger from 'redux-logger';
-import axiosMiddleware from 'redux-axios-middleware';
+import reduxThunk from 'redux-thunk';
 
 import reducers from '../reducers';
-import { initApi } from '../config/apiConfig';
-
-const client = initApi();
 
 const middleware = applyMiddleware(
-    axiosMiddleware(client),
+    reduxThunk,
     logger()
-);
+)(createStore);
 
 export default function configureStore(initialState) {
     return createStore(reducers, initialState, middleware);
