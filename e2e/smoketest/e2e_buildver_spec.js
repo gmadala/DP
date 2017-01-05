9a1c438adb2711405a9171714bb5cb05650fe39d
Bobby Washington <bobwashington@hotmail.com>
2017-01-05 09:31:22 -0500

    Removed commented out text

--- e2e/smoketest/e2e_buildver_spec.js
+++ e2e/smoketest/e2e_buildver_spec.js
@@ -2,7 +2,7 @@
 
 var bambooCommitHash = process.env.bamboo_repository_revision_number.substring(0, 7);
 var objHash = browser.element(by.css('[style=\'word-wrap: break-word; white-space: pre-wrap;\']'));
-var versiontxt = "version.txt";
+var appendurl = "version.txt";
 
 describe('Build Verification', function () {
 
@@ -13,13 +13,10 @@ describe('Build Verification', function () {
 
   it("1. Build Version  - Validating the current build version has been deployed", function () {
 
-    //console.log("\n Substring of baseUrl is " + browser.baseUrl.split('#')[0] + versiontxt);
-
-    browser.get(browser.baseUrl.split('#')[0] + versiontxt);
+    browser.get(browser.baseUrl.split('#')[0] + appendurl);
 
     console.log('\n The commit hash from bamboo environment variable "bamboo_repository_revision_number" is: ' + bambooCommitHash);
-
-
+      
     browser.getCurrentUrl().then(function (url) {
           console.log("\n The url being used is " + url)
       });
@@ -30,6 +27,3 @@ describe('Build Verification', function () {
     });
   });
 });
-
-
-//https://customer.nextgearcapital.com/version.txt
