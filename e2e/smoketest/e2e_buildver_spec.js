1bf96c3191d4825b0bcd58c58c9e9531b9b3f074
Bobby Washington <bobwashington@hotmail.com>
2017-01-10 12:31:15 -0500

    Testing out bamboo variable

--- e2e/smoketest/e2e_buildver_spec.js
+++ e2e/smoketest/e2e_buildver_spec.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var bambooCommitHash = process.env.bamboo_repository_revision_number.substring(0, 7);
+//var bambooCommitHash = process.env.bamboo_repository_revision_number.substring(0, 7);
+
+var bambooCommitHash = process.env.bamboo.planRepository.revision.substring(0, 7);
+
+console.log("The value for bamboo_repository_revision_number is " + process.env.bamboo.planRepository.revision);
+
 var objHash = browser.element(by.css('[style=\'word-wrap: break-word; white-space: pre-wrap;\']'));
 var appendurl = "version.txt";
 
@@ -16,7 +21,7 @@ describe('Build Verification', function () {
     browser.get(browser.baseUrl.split('#')[0] + appendurl);
 
     console.log('\n The commit hash from bamboo environment variable "bamboo_repository_revision_number" is: ' + bambooCommitHash);
-      
+
     browser.getCurrentUrl().then(function (url) {
           console.log("\n The url being used is " + url)
       });
