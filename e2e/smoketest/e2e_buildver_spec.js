afccc32424704b16a576095f568a80651ab9a4a9
Bobby Washington <bobwashington@hotmail.com>
2017-01-11 11:09:12 -0500

    Added additional console.log linee

--- e2e/smoketest/e2e_buildver_spec.js
+++ e2e/smoketest/e2e_buildver_spec.js
@@ -1,34 +1,37 @@
 'use strict';
 
-//var bambooCommitHash = process.env.bamboo_repository_revision_number.substring(0, 7);
-
-var bambooCommitHash = process.env.bamboo.planRepository.revision.substring(0, 7);
-
-console.log("The value for bamboo_repository_revision_number is " + process.env.bamboo.planRepository.revision);
-
+var bambooCommitHash = process.env.bamboo_repository_revision_number.substring(0, 7);
 var objHash = browser.element(by.css('[style=\'word-wrap: break-word; white-space: pre-wrap;\']'));
 var appendurl = "version.txt";
 
 describe('Build Verification', function () {
 
-  beforeEach(function () {
-    browser.sleep(browser.params.shortDelay);
-    browser.ignoreSynchronization = true;
-  });
-
-  it("1. Build Version  - Validating the current build version has been deployed", function () {
-
-    browser.get(browser.baseUrl.split('#')[0] + appendurl);
-
-    console.log('\n The commit hash from bamboo environment variable "bamboo_repository_revision_number" is: ' + bambooCommitHash);
-
-    browser.getCurrentUrl().then(function (url) {
-          console.log("\n The url being used is " + url)
-      });
-
-    objHash.getText().then(function (string) {
-      var substr = string.substring(12, 19);
-      expect(substr).toEqual(bambooCommitHash);
+    beforeEach(function () {
+        browser.sleep(browser.params.shortDelay);
+        browser.ignoreSynchronization = true;
     });
-  });
+
+    it("1. Build Version  - Validating the current build version has been deployed", function () {
+
+        browser.get(browser.baseUrl.split('#')[0] + appendurl);
+
+        browser.sleep('3000');
+
+        //Console logs for info...
+        browser.getCurrentUrl().then(function (url) {
+            console.log("\n The current url being used is " + url)
+        });
+
+        console.log('\n The commit hash from bamboo environment variable "bamboo_repository_revision_number" is: ' + bambooCommitHash);
+
+        objHash.getText().then(function (string) {
+            console.log("\n The commit hash from the current url is: " + string.substring(12, 19));
+        });
+
+
+        objHash.getText().then(function (string) {
+            var substr = string.substring(12, 19);
+            expect(substr).toEqual(bambooCommitHash);
+        });
+    });
 });
