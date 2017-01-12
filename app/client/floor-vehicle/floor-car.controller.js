5bbea7cbf558e6c6f9002c111e6853dcecc47b3c
Win Ribeka <nyoman.ribeka@gmail.com>
2017-01-12 15:39:22 -0500

    MNGW-7332 - Fixing floorplan on old screen.

--- app/client/floor-vehicle/floor-car.controller.js
+++ app/client/floor-vehicle/floor-car.controller.js
@@ -240,15 +240,12 @@
       }
 
       $scope.vinDetailsErrorFlag = true;
-      if (!$scope.form.$valid) {
-        if (isDealer && $scope.canAttachDocuments()) {
-          $scope.missingDocuments = $scope.files.length < 1;
-        }
-        return false;
-      }
 
       if (isDealer && $scope.canAttachDocuments()) {
         $scope.missingDocuments = $scope.files.length < 1;
+      }
+
+      if (!$scope.form.$valid || $scope.missingDocuments) {
         return false;
       }
 
