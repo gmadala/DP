adafec2e95378bac8171da2e72b5f0991dc4c437
Jarrod Mayer <jarrod.mayer@gmail.com>
2017-02-22 15:04:17 -0500

    MNGW-7348 - removed extra HTTP call

--- react/app/sideMenu/SideMenu.jsx
+++ react/app/sideMenu/SideMenu.jsx
@@ -69,15 +69,15 @@ class SideMenu extends Component {
         const supportList = this.props.menuList.find(x => x.id === 'support');
         if (typeof(supportList.subMenu) === 'undefined' && typeof(supportList.set) === 'undefined' && this.props.nxgConfig) {
             supportList.set = true
-            this.buildSupportMenu()
+            if (this.props.userInfo) { this.buildSupportMenu() }
         }
     }
 
     buildTimeline = () => {
         const t1 = TweenMax.fromTo(this.container, 0, { autoAlpha: 0 }, { autoAlpha: 1 })
         const t2 = TweenMax.fromTo(this.menuContainer, 0, { autoAlpha: 0 }, { autoAlpha: 1 })
-        const t3 = TweenMax.fromTo(this.overlay, 0.5, { autoAlpha: 0, ease: Power0.easeOut }, { autoAlpha: 0.7, ease: Power0.easeIn })
-        const t4 = TweenMax.fromTo(this.menu, 0.5, { autoAlpha: 0, left: '-100%', ease: Power0.easeOut }, { autoAlpha: 1, left: '0', ease: Power0.easeIn })
+        const t3 = TweenMax.fromTo(this.overlay, 0.2, { autoAlpha: 0, ease: Power0.easeOut }, { autoAlpha: 0.7, ease: Power0.easeIn })
+        const t4 = TweenMax.fromTo(this.menu, 0.2, { autoAlpha: 0, left: '-100%', ease: Power0.easeOut }, { autoAlpha: 1, left: '0', ease: Power0.easeIn })
 
         this.state.tl.add([t1, t2, t3, t4], '+=0', 'start')
     }
@@ -96,46 +96,42 @@ class SideMenu extends Component {
         // check user type, dealers and auctions will have different subdomains to go to
         const forumId = this.props.user.isDealer ? config.dealerForumId : config.auctionForumId
         const customTemplateId = this.props.user.isDealer ? config.dealerCustomTemplateId : config.auctionCustomTemplateId;
-        this.props.user.getInfo( ).then((info) => {
-            if (info) {
-                const menu = [
-                    {
-                        title: 'sideMenu.supportMenu.chat',
-                        metric: metrics.MOBILE_CLICK_CHAT_NOW_LINK,
-                        href: 'https://home-c4.incontact.com/inContact/ChatClient/ChatClient.aspx?poc=0a63c698-c417-4ade-8e0d-55cccf2b0d85&bu=4592556',
-                        target: '_blank',
-                        active: false,
-                        permission: permissions.DEALER,
-                    }, {
-                        title: 'sideMenu.supportMenu.callAccountExecutive',
-                        metric: metrics.MOBILE_CLICK_CALL_ACCOUNT_EXECUTIVE,
-                        href: `tel:+${ info.MarketPhoneNumber }`,
-                        active: false,
-                        permission: permissions.DEALER,
-                    }, {
-                        title: 'sideMenu.supportMenu.callCustomerService',
-                        metric: metrics.MOBILE_CLICK_CALL_CUSTOMER_SERVICE,
-                        href: `tel:+${ info.CSCPhoneNumber }`,
-                        active: false,
-                        permission: permissions.ALL,
-                    }, {
-                        title: 'sideMenu.supportMenu.sendEmail',
-                        metric: metrics.MOBILE_CLICK_SEND_EMAIL,
-                        href: `mailto:${ this.props.user.isDealer( ) ? info.MarketEMail : 'auctionservices@nextgearcapital.com' }`,
-                        active: false,
-                        permission: permissions.ALL,
-                    }, {
-                        title: 'sideMenu.supportMenu.feedback',
-                        metric: metrics.MOBILE_CLICK_FEEDBACK,
-                        href: `https://nextgearcapital.uservoice.com/clients/widgets/classic_widget?contact_enabled=true&custom_template_id=${customTemplateId}&default_mode=support&embed_type=lightbox&feedback_enabled=true&forum_id=${forumId}&link_color=1864A1&mobile=true&mode=full&primary_color=135889&referrer=${encodeURI(window.location.href)}&smartvote=true&strings=e30%3D&support_tab_name=Technical Support&trigger_background_color=135889&trigger_method=pin`, // eslint-disable-line
-                        target: '_blank',
-                        active: false,
-                        permission: permissions.ALL,
-                    },
-                ]
-                this.props.updateSubMenuItems('support', menu)
-            }
-        })
+        const menu = [
+            {
+                title: 'sideMenu.supportMenu.chat',
+                metric: metrics.MOBILE_CLICK_CHAT_NOW_LINK,
+                href: 'https://home-c4.incontact.com/inContact/ChatClient/ChatClient.aspx?poc=0a63c698-c417-4ade-8e0d-55cccf2b0d85&bu=4592556',
+                target: '_blank',
+                active: false,
+                permission: permissions.DEALER,
+            }, {
+                title: 'sideMenu.supportMenu.callAccountExecutive',
+                metric: metrics.MOBILE_CLICK_CALL_ACCOUNT_EXECUTIVE,
+                href: `tel:+${ this.props.userInfo.MarketPhoneNumber }`,
+                active: false,
+                permission: permissions.DEALER,
+            }, {
+                title: 'sideMenu.supportMenu.callCustomerService',
+                metric: metrics.MOBILE_CLICK_CALL_CUSTOMER_SERVICE,
+                href: `tel:+${ this.props.userInfo.CSCPhoneNumber }`,
+                active: false,
+                permission: permissions.ALL,
+            }, {
+                title: 'sideMenu.supportMenu.sendEmail',
+                metric: metrics.MOBILE_CLICK_SEND_EMAIL,
+                href: `mailto:${ this.props.user.isDealer( ) ? this.props.userInfo.MarketEMail : 'auctionservices@nextgearcapital.com' }`,
+                active: false,
+                permission: permissions.ALL,
+            }, {
+                title: 'sideMenu.supportMenu.feedback',
+                metric: metrics.MOBILE_CLICK_FEEDBACK,
+                href: `https://nextgearcapital.uservoice.com/clients/widgets/classic_widget?contact_enabled=true&custom_template_id=${customTemplateId}&default_mode=support&embed_type=lightbox&feedback_enabled=true&forum_id=${forumId}&link_color=1864A1&mobile=true&mode=full&primary_color=135889&referrer=${encodeURI(window.location.href)}&smartvote=true&strings=e30%3D&support_tab_name=Technical Support&trigger_background_color=135889&trigger_method=pin`, // eslint-disable-line
+                target: '_blank',
+                active: false,
+                permission: permissions.ALL,
+            },
+        ]
+        this.props.updateSubMenuItems('support', menu)
     }
     updateLogoutLink = () => {
         this.props.addTopLevelLinkFunc('signOut', () => { this.props.$rootScope.$emit( 'event:userRequestedLogout' ) })
