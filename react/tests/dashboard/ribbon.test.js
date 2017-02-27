318f8ef22968784acce13fc4d00762d0efd8345c
Myles Hiatt <myles.hiatt@nextgearcapital.com>
2017-02-27 10:34:58 -0500

    MNGW-7257 -- removed commented out code from ribbon.test.js

--- react/tests/dashboard/ribbon.test.js
+++ react/tests/dashboard/ribbon.test.js
@@ -4,9 +4,6 @@ import { shallow } from 'enzyme';
 import Ribbon from '../../app/ribbon/Ribbon';
 import RibbonItem from '../../app/ribbon/RibbonItem';
 
-// import PendingUnitsAccordion from '../../app/ribbon/PendingUnitsAccordion';
-// import PendingUnitPanel from '../../app/ribbon/PendingUnitPanel';
-
 describe('Ribbon', ( ) => {
     const navfloorplan = sinon.spy( );
     const floorplanshow = true;
@@ -53,66 +50,4 @@ describe('Ribbon', ( ) => {
 
         expect(wrapper.find(RibbonItem)).to.have.length(2);
     });
-
-    // it('test to make sure acccordion is there when pending floorplan is equal 0', () => {
-    //     floorplancount = 0;
-    //
-    //     const wrapper = shallow(
-    //         <Ribbon
-    //             navfloorplan={navfloorplan}
-    //             floorplanshow={floorplanshow}
-    //             floorplancount={floorplancount}
-    //             pendingunitsdata={pendingunitsdata}
-    //             openauditsshow={openauditsshow}
-    //             openauditscount={openauditscount}
-    //             navaudit={navaudit}
-    //             language={language}
-    //         />
-    //     );
-    //
-    //     expect(wrapper.find(PendingUnitsAccordion)).to.have.length(0);
-    // });
-    //
-    // it('test to make sure acccordion is there and panel length match pendingunitsdata length when pending floorplan is greater than 0', () => {
-    //     floorplancount = 3;
-    //     pendingunitsdata = [
-    //         {
-    //             StockNumber: 123,
-    //             Description: 'Some Car Description',
-    //             UnitVIN: '1231k2l3j12l',
-    //             FlooringDate: '2019 12 12',
-    //             FloorplanId: '45645645645645'
-    //         },
-    //         {
-    //             StockNumber: 123,
-    //             Description: 'Some Car Description',
-    //             UnitVIN: '1231k2l3j12l',
-    //             FlooringDate: '2019 12 12',
-    //             FloorplanId: '34234243234234'
-    //         },
-    //         {
-    //             StockNumber: 123,
-    //             Description: 'Some Car Description',
-    //             UnitVIN: '1231k2l3j12l',
-    //             FlooringDate: '2019 12 12',
-    //             FloorplanId: '65756756757675'
-    //         }
-    //     ];
-    //
-    //     const wrapper = shallow(
-    //         <Ribbon
-    //             navfloorplan={navfloorplan}
-    //             floorplanshow={floorplanshow}
-    //             floorplancount={floorplancount}
-    //             pendingunitsdata={pendingunitsdata}
-    //             openauditsshow={openauditsshow}
-    //             openauditscount={openauditscount}
-    //             navaudit={navaudit}
-    //             language={language}
-    //         />
-    //     );
-    //
-    //     expect(wrapper.find(PendingUnitsAccordion)).to.have.length(1);
-    //     expect(wrapper.find(PendingUnitPanel)).to.have.length(3);
-    // });
 });
