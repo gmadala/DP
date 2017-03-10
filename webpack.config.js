866a53f20766ab8a090e8cbc99481070603b7003
Jarrod Mayer <jarrod.mayer@gmail.com>
2017-03-10 13:52:51 -0500

    MTD-88 - Added production build setup, moved eslint to it's respective folders

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
         : [
             new webpack.optimize.DedupePlugin( ),
             new webpack.optimize.OccurenceOrderPlugin( ),
-            new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+            new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true }),
             new webpack.DefinePlugin({
                 "process.env": {
                     NODE_ENV: JSON.stringify( "production" )
