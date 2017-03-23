e20e3696c02c0f75c8616eb75184d0f6f2df73ba
Jarrod Mayer <jarrod.mayer@gmail.com>
2017-03-23 10:48:52 -0400

    MTD-88 - turned off sourcemap for prod

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
         : [
             new webpack.optimize.DedupePlugin( ),
             new webpack.optimize.OccurenceOrderPlugin( ),
-            new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: true }),
+            new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
             new webpack.DefinePlugin({
                 "process.env": {
                     NODE_ENV: JSON.stringify( "production" )
